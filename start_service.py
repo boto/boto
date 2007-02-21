dbd75ed69a403fda0ee09db565a1e1b5a1caa785
Mitch.Garnaat <Mitch.Garnaat@604d75c7-a419-0410-a38f-bde1a0bd1dbf>
2007-02-21 01:17:28 +0000

    Fixing some bugs.

--- start_service.py
+++ start_service.py
@@ -9,7 +9,6 @@ def find_class(module, class_name):
     modules = module.split('.')
     path = None
     for module_name in modules:
-        print 'loading %s' % module_name
         fp, pathname, description = imp.find_module(module_name, path)
         module = imp.load_module(module_name, fp, pathname, description)
         if hasattr(module, '__path__'):
@@ -18,40 +17,34 @@ def find_class(module, class_name):
   
 def main():
     try:
-        opts, args = getopt.getopt(sys.argv[1:], 'hcmw:i:o:',
-                                   ['help', 'class', 'module', 'working_dir=',
-                                    'inputqueue', 'outputqueue'])
+        opts, args = getopt.getopt(sys.argv[1:], 'c:hi:m:o:w:',
+                                   ['class', 'help', 'inputqueue',
+                                    'outputqueue', 'module', 'working_dir='])
     except:
         usage()
     input_queue = None
     output_queue = None
     wdir = 'work'
-    module = None
+    mod_name = None
     class_name = None
     for o, a in opts:
         if o in ('-h', '--help'):
             usage()
-            sys.exit()
         if o in ('-c', '--class'):
             class_name = a
         if o in ('-m', '--module'):
-            module = a
+            mod_name = a
         if o in ('-w', '--working_dir'):
             wdir = a
         if o in ('-i', '--inputqueue'):
             input_queue = a
         if o in ('-o', '--outputqueue'):
             output_queue = a
-    print 'module=%s' % module
-    print 'class_name=%s' % classname
-    print 'input_queue=%s' % input_queue
-    print 'output-queue=%s' % output_queue
-    print 'wdir=%s' % wdir
-    if not class_name or not module:
+    if not class_name or not mod_name:
         usage()
-    cls = find_class(module, class_name)
+    cls = find_class(mod_name, class_name)
     s = cls(working_dir=wdir, input_queue_name=input_queue,
-            output_queue_name=output-queue)
+            output_queue_name=output_queue)
     s.run(notify)
 
 if __name__ == "__main__":
