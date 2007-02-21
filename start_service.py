394e3ed399de368f444dc4041bd915186f6edea2
Mitch.Garnaat <Mitch.Garnaat@604d75c7-a419-0410-a38f-bde1a0bd1dbf>
2007-02-21 00:59:16 +0000

    A command-line script to start up a service.

---
+++ start_service.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+import getopt, sys, imp
+from smartdoc.xip import Xip
+
+def usage():
+    print 'start_service.py -m module -c class_name [-w working_dir] [-i input_queue_name] [-o output_queue_name]'
+
+def find_class(module, class_name):
+    modules = module.split('.')
+    path = None
+    for module_name in modules:
+        print 'loading %s' % module_name
+        fp, pathname, description = imp.find_module(module_name, path)
+        module = imp.load_module(module_name, fp, pathname, description)
+        if hasattr(module, '__path__'):
+            path = module.__path__
+    return getattr(module, class_name)
+  
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'hcmw:i:o:',
+                                   ['help', 'class', 'module', 'working_dir=',
+                                    'inputqueue', 'outputqueue'])
+    except:
+        usage()
+        sys.exit(2)
+    input_queue = None
+    output_queue = None
+    wdir = 'work'
+    module = None
+    class_name = None
+    for o, a in opts:
+        if o in ('-h', '--help'):
+            usage()
+            sys.exit()
+        if o in ('-c', '--class'):
+            class_name = a
+        if o in ('-m', '--module'):
+            module = a
+        if o in ('-w', '--working_dir'):
+            wdir = a
+        if o in ('-i', '--inputqueue'):
+            input_queue = a
+        if o in ('-o', '--outputqueue'):
+            output_queue = a
+    if not class_name or not module:
+        usage()
+        exit(2)
+    cls = find_class(module, class_name)
+    s = cls(working_dir=wdir, input_queue_name=input_queue,
+            output_queue_name=output-queue)
+    s.run(notify)
+
+if __name__ == "__main__":
+    main()
+
