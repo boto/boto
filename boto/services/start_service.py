0a3047bc81cdd0211ce7b85cc9767596ef1806aa
Mitch.Garnaat <Mitch.Garnaat@604d75c7-a419-0410-a38f-bde1a0bd1dbf>
2007-02-21 16:24:45 +0000

    Try to read module_name and class_name from instance userdata.

--- boto/services/start_service.py
+++ boto/services/start_service.py
@@ -1,9 +1,25 @@
 #!/usr/bin/env python
 import getopt, sys, imp
+from boto.utils import get_instance_userdata
 
 def usage():
     print 'start_service.py -m module -c class_name [-w working_dir] [-i input_queue_name] [-o output_queue_name]'
     sys.exit()
+
+def get_userdata():
+    module_name = None
+    class_name = None
+    s = get_instance_userdata()
+    if s:
+        l = s.split('|')
+        for nvpair in l:
+            t = nvpair.split('=')
+            if t[0].strip() == 'module_name':
+                module_name = t[1].strip()
+            elif t[0].strip() == 'class_name':
+                class_name = t[1].strip()
+    return (module_name, class_name)
+    
 
 def find_class(module, class_name):
     modules = module.split('.')
@@ -19,13 +35,13 @@ def main():
     try:
         opts, args = getopt.getopt(sys.argv[1:], 'c:hi:m:o:w:',
                                    ['class', 'help', 'inputqueue',
-                                    'outputqueue', 'module', 'working_dir='])
+                                    'outputqueue', 'module', 'working_dir'])
     except:
         usage()
     input_queue = None
     output_queue = None
     wdir = 'work'
-    mod_name = None
+    module_name = None
     class_name = None
     for o, a in opts:
         if o in ('-h', '--help'):
@@ -33,16 +49,18 @@ def main():
         if o in ('-c', '--class'):
             class_name = a
         if o in ('-m', '--module'):
-            mod_name = a
+            module_name = a
         if o in ('-w', '--working_dir'):
             wdir = a
         if o in ('-i', '--inputqueue'):
             input_queue = a
         if o in ('-o', '--outputqueue'):
             output_queue = a
-    if not class_name or not mod_name:
+    if not class_name or not module_name:
+        module_name, class_name = get_userdata()
+    if not class_name or not module_name:
         usage()
-    cls = find_class(mod_name, class_name)
+    cls = find_class(module_name, class_name)
     s = cls(working_dir=wdir, input_queue_name=input_queue,
             output_queue_name=output_queue)
     s.run()
