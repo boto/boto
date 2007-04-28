f50fd1f8a801bb1d9b2b389bc0ba0ef716f89186
Mitch.Garnaat <Mitch.Garnaat@604d75c7-a419-0410-a38f-bde1a0bd1dbf>
2007-04-28 02:41:27 +0000

    Adding support for https_connection_factory.  Fixes Issue-57.

--- boto/sqs/connection.py
+++ boto/sqs/connection.py
@@ -34,10 +34,11 @@ class SQSConnection(AWSAuthConnection):
     
     def __init__(self, aws_access_key_id=None, aws_secret_access_key=None,
                  is_secure=False, port=None, proxy=None, proxy_port=None,
-                 host=DefaultHost, debug=0):
+                 host=DefaultHost, debug=0, https_connection_factory=None):
         AWSAuthConnection.__init__(self, host,
                                    aws_access_key_id, aws_secret_access_key,
-                                   is_secure, port, proxy, proxy_port, debug)
+                                   is_secure, port, proxy, proxy_port, debug,
+                                   https_connection_factory)
 
     def make_request(self, method, path, headers=None, data=''):
         # add auth header
