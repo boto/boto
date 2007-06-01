3c476ba81bdc43aefd6a89edce1f42c323709d7a
Mitch.Garnaat <Mitch.Garnaat@604d75c7-a419-0410-a38f-bde1a0bd1dbf>
2007-06-01 13:51:33 +0000

    Added sorting of log messages

--- boto/services/get_log.py
+++ boto/services/get_log.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 import getopt, sys, imp
 import boto
+from boto.sqs.message import MHMessage
 
 def usage():
     print 'SYNOPSIS'
     print '\tget_log.py -l log_queue_name -o log_file_name'
     sys.exit()
+
+def compare_timestamps(x, y):
+    return cmp(x['Timestamp'], y['Timestamp'])
 
 def main():
     try:
@@ -24,7 +28,19 @@ def main():
             log_file_name = a
     c = boto.connect_sqs()
     q = c.get_queue(log_queue_name)
-    q.save(log_file_name)
+    q.set_message_class(MHMessage)
+    msgs = []
+    msg = q.read()
+    while msg:
+        msgs.append(msg)
+        q.delete_message(msg)
+        msg = q.read()
+    msgs.sort(compare_timestamps)
+    fp = open(log_file_name, 'w')
+    for msg in msgs:
+        fp.write(msg.get_body())
+        fp.write('\n-----------------------\n')
+    fp.close()
 
 if __name__ == "__main__":
     main()
