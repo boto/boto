3b0f33548de3600c421a7884721371f632da5fb5
Mitch.Garnaat <Mitch.Garnaat@604d75c7-a419-0410-a38f-bde1a0bd1dbf>
2007-09-25 02:23:55 +0000

    Cleaning up broken references to get_user_data

--- boto/pyami/startup.py
+++ boto/pyami/startup.py
@@ -45,19 +45,19 @@ class Startup:
         return val
 
     def get_script(self):
-        script_name = self.get_user_data['script_name']
+        script_name = self.get_user_data('script_name')
         if script_name:
             c = boto.connect_s3(self.get_user_data('aws_access_key_id'),
                                 self.get_user_data('aws_secret_access_key'))
             script_name = script + '.py'
-            bucket = c.get_bucket(self.get_user_data['bucket_name'])
+            bucket = c.get_bucket(self.get_user_data('bucket_name'))
             script = bucket.get_key(script_name)
             print 'Fetching %s.%s' % (bucket.name, script.name)
             script_path = os.path.join(self.working_dir, script_name)
             script.get_contents_to_filename(script_path)
             self.module_name = script_name
         else:
-            self.module_name = self.get_user_data['module_name']
+            self.module_name = self.get_user_data('module_name')
 
     def run_script(self):
         if self.module_name:
