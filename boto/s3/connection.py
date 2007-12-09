f58af8f48d88367775356265c8a2e89962b6e648
Mitch.Garnaat <Mitch.Garnaat@604d75c7-a419-0410-a38f-bde1a0bd1dbf>
2007-12-09 15:49:33 +0000

    Adding convenience methods to S3 module to make it easier to add grants to buckets and keys.  Also added a few more docstrings.

--- boto/s3/connection.py
+++ boto/s3/connection.py
@@ -78,6 +78,19 @@ class S3Connection(AWSAuthConnection):
         xml.sax.parseString(body, h)
         return rs
 
+    def get_canonical_user_id(self):
+        """
+        Convenience method that returns the "CanonicalUserID" of the user who's credentials
+        are associated with the connection.  The only way to get this value is to do a GET
+        request on the service which returns all buckets associated with the account.  As part
+        of that response, the canonical userid is returned.  This method simply does all of
+        that and then returns just the user id.
+        Returns:
+            A string containing the canonical user id.
+        """
+        rs = self.get_all_buckets()
+        return rs.ID
+
     def get_bucket(self, bucket_name):
         bucket = Bucket(self, bucket_name)
         rs = bucket.get_all_keys(None, maxkeys=0)
