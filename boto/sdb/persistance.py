1bec655efb894e80db6d41cc6688eb28614e870c
Mitch.Garnaat <Mitch.Garnaat@604d75c7-a419-0410-a38f-bde1a0bd1dbf>
2008-01-10 04:21:44 +0000

    A better version of persistance.  Still not complete or finished but closer.

--- boto/sdb/persistance.py
+++ boto/sdb/persistance.py
@@ -23,6 +23,7 @@ import boto
 from datetime import datetime
 from boto.exception import SDBPersistanceError
 from boto.utils import find_class
+import uuid
 
 ISO8601 = '%Y-%m-%dT%H:%M:%SZ'
 
@@ -50,34 +51,51 @@ class Persistance:
 
 def object_lister(cls, query_lister):
     for item in query_lister:
-        class_name, item_name = SDBObject.split_name(item.name)
-        yield cls(item_name)
+        yield cls(item.name)
                   
 class SDBObject(object):
 
     @classmethod
-    def join_name(cls, name):
-        return '%s:%s' % (cls.__name__, name)
-
-    @classmethod
-    def split_name(cls, name):
-        return name.split(':')
-
-    @classmethod
-    def __exists(cls, name):
+    def get(cls, id=None, **params):
         domain = Persistance.get_domain()
-        a = domain.get_attributes(cls.join_name(name), ['__name__'])
-        if a.has_key('__name__'):
-            return True
+        if id:
+            a = domain.get_attributes(id, ['__type__'])
+            if a.has_key('__type__'):
+                return cls(id)
+            else:
+                raise SDBPersistanceError('%s object with id=%s does not exist' % (cls.__name__, id))
         else:
-            return False
+            rs = cls.find(**params)
+            try:
+                id = rs.next()
+            except StopIteration:
+                raise SDBPersistanceError('%s object matching query does not exist' % cls.__name__)
+            try:
+                rs.next()
+            except StopIteration:
+                return cls(id)
+            raise SDBPersistanceError('Query matched more than 1 item')
 
     @classmethod
-    def find(cls, name):
-        if cls.__exists(name):
-            return cls(name, check=False)
-        else:
-            return None
+    def find(cls, **params):
+        keys = params.keys()
+        if len(keys) > 5:
+            raise SDBPersistanceError('Too many fields, max is 5')
+        parts = []
+        for key in keys:
+            if cls.__dict__.has_key(key):
+                if isinstance(cls.__dict__[key], ScalarProperty):
+                    checker = cls.__dict__[key].checker
+                    parts.append("['%s' = '%s']" % (key, checker.to_string(params[key])))
+                else:
+                    raise SDBPersistanceError('%s is not a searchable field' % key)
+            else:
+                raise SDBPersistanceError('%s is not a valid field' % key)
+        query = ' intersection '.join(parts)
+        print query
+        domain = Persistance.get_domain()
+        rs = domain.query(query)
+        return object_lister(cls, rs)
 
     @classmethod
     def list(cls):
@@ -85,76 +103,108 @@ class SDBObject(object):
         rs = domain.query("['__type__' = '%s']" % cls.__name__)
         return object_lister(cls, rs)
 
-    def __init__(self, name, check=True):
-        self.name = name
-        if check and not self.__exists(name):
-            domain = Persistance.get_domain()
-            domain.put_attributes(self, {'__type__' : self.__class__.__name__,
-                                         '__name__' : self.name})
+    def __init__(self, id=None, name=None):
+        domain = Persistance.get_domain()
+        if id:
+            self.id = id
+            attrs = domain.get_attributes(self.id, ['__name__'])
+            if attrs.has_key('__name__'):
+                self.name = attrs['__name__']
+        else:
+            attrs = {}
+            self.id = str(uuid.uuid4())
+            self.name = name
+            if self.name:
+                attrs['__name__'] = self.name
+            attrs['__type__'] = self.__class__.__name__
+            attrs['__module__'] = self.__class__.__module__
+            domain.put_attributes(self.id, attrs, replace=True)
 
     def __repr__(self):
-        return self.join_name(self.name)
+        return '%s<%s>' % (self.__class__.__name__, self.id)
 
     def get_handle(self):
         return '%s:%s' % (self.__class__.__module__, self)
 
-class StringValue:
+class ValueChecker:
+
+    def check(self, value):
+        """
+        Checks a value to see if it is of the right type.
+
+        Should raise a TypeError exception if an in appropriate value is passed in.
+        """
+        raise TypeError
+
+    def from_string(self, str_value):
+        """
+        Takes a string as input and returns the type-specific value represented by that string.
+
+        Should raise a ValueError if the value cannot be converted to the appropriate type.
+        """
+        raise ValueError
+
+    def to_string(self, value):
+        """
+        Convert a value to it's string representation.
+
+        Should raise a ValueError if the value cannot be converted to a string representation.
+        """
+        raise ValueError
+    
+class StringChecker(ValueChecker):
 
     def __init__(self, **params):
-        if params.has_key('max_length'):
-            self.max_length = params['max_length']
+        if params.has_key('maxlength'):
+            self.maxlength = params['maxlength']
         else:
-            self.max_length = 1024
+            self.maxlength = 1024
         if params.has_key('default'):
-            self.set(params['default'])
+            self.check(params['default'])
+            self.default = params['default']
         else:
-            self.set('')
+            self.default = ''
 
-    def set(self, value):
+    def check(self, value):
         if isinstance(value, str) or isinstance(value, unicode):
-            if len(value) > self.max_length:
-                raise SDBPersistanceError('Max size of %d characters exceeded' % self.max_length)
-            self.value = value
+            if len(value) > self.maxlength:
+                raise ValueError
         else:
-            raise SDBPersistanceError('Value must be of type str or unicode')
+            raise TypeError
 
-    def set_from_string(self, str_value):
-        self.set(str_value)
+    def from_string(self, str_value):
+        return str_value
 
-    def get(self):
-        return self.value
+    def to_string(self, value):
+        self.check(value)
+        return value
 
-    def get_as_string(self):
-        return self.get()
-    
-class PositiveIntegerValue:
+class PositiveIntegerChecker(ValueChecker):
 
     def __init__(self, **params):
-        if params.has_key('max_length'):
-            self.max_length = params['max_length']
+        if params.has_key('maxlength'):
+            self.maxlength = params['maxlength']
         else:
-            self.max_length = 10
+            self.maxlength = 10
         if params.has_key('default'):
-            self.set(params['default'])
+            self.check(params['default'])
+            self.default = params['default']
         else:
-            self.set(0)
-        self.format_string = '%%0%dd' % self.max_length
+            self.default = 0
+        self.format_string = '%%0%dd' % self.maxlength
 
-    def set(self, value):
+    def check(self, value):
         if not isinstance(value, int):
-            raise SDBPersistanceError('Value must be of type int')
-        self.value = value
+            raise TypeError
 
-    def set_from_string(self, str_value):
-        self.set(int(str_value))
+    def from_string(self, str_value):
+        return int(str_value)
 
-    def get(self):
-        return self.value
-
-    def get_as_string(self):
-        return self.format_string % self.get()
+    def to_string(self, value):
+        self.check(value)
+        return self.format_string % value
     
-class BooleanValue:
+class BooleanChecker(ValueChecker):
 
     def __init__(self, **params):
         if params.has_key('default'):
@@ -162,93 +212,83 @@ class BooleanValue:
         else:
             self.set(False)
 
-    def set(self, value):
+    def check(self, value):
         if not isinstance(value, bool):
-            raise SDBPersistanceError('Value must be of type bool')
-        self.value = value
+            raise TypeError
 
-    def set_from_string(self, str_value):
+    def from_string(self, str_value):
         if str_value.lower() == 'true':
-            self.set(True)
+            return True
         else:
-            self.set(False)
-
-    def get(self):
-        return self.value
-
-    def get_as_string(self):
-        if self.get():
+            return False
+        
+    def to_string(self, value):
+        self.check(value)
+        if value == True:
             return 'true'
         else:
             return 'false'
     
-class DateTimeValue:
+class DateTimeChecker(ValueChecker):
 
     def __init__(self, **params):
-        if params.has_key('max_length'):
-            self.max_length = params['max_length']
+        if params.has_key('maxlength'):
+            self.maxlength = params['maxlength']
         else:
-            self.max_length = 1024
+            self.maxlength = 1024
         if params.has_key('default'):
             self.set(params['default'])
         else:
             self.set(datetime.now())
 
-    def set(self, value):
+    def check(self, value):
         if not isinstance(value, datetime):
-            raise SDBPersistanceError('Value must be of type datetime')
-        self.value = value
+            raise TypeError
 
-    def set_from_string(self, str_value):
+    def from_string(self, str_value):
         try:
-            ts = datetime.strptime(str_value, ISO8601)
-            self.set(ts)
+            return datetime.strptime(str_value, ISO8601)
         except:
-            raise SDBPersistanceError('Date String is not in ISO8601 format')
+            raise ValueError
 
-    def get(self):
-        return self.value
-
-    def get_as_string(self):
-        return self.value.strftime(ISO8601)
+    def to_string(self, value):
+        self.check(value)
+        return value.strftime(ISO8601)
     
-class ObjectValue:
+class ObjectChecker(ValueChecker):
 
     def __init__(self, **params):
-        self.set(None)
+        self.default = None
         if params.has_key('ref_class'):
             self.ref_class = params['ref_class']
         else:
             self.ref_class = SDBObject
 
-    def set(self, value):
+    def check(self, value):
         if value == None:
             return
         if not isinstance(value, SDBObject):
-            raise SDBPersistanceError('Value must be a subclass of SDBObject')
+            raise TypeError
         if not isinstance(value, self.ref_class):
-            raise SDBPersistanceError('Value must be an instance of class %s' % self.ref_class)
-        self.value = value
+            raise TypeError
 
-    def set_from_string(self, str_value):
+    def from_string(self, str_value):
         try:
             module_name, class_name, obj_name = str_value.split(':')
             cls = find_class(module_name, class_name)
-            self.set(cls(obj_name))
+            return cls(obj_name)
         except:
-            raise SDBPersistanceError('Object Reference is not in correct format')
+            raise ValueError
 
-    def get(self):
-        return self.value
-
-    def get_as_string(self):
-        return self.value.get_handle()
+    def to_string(self, value):
+        self.check(value)
+        return value.id
 
 class ScalarProperty(object):
 
-    def __init__(self, name, value_class, **params):
+    def __init__(self, name, checker_class, **params):
         self.name = name
-        self.value = value_class(**params)
+        self.checker = checker_class(**params)
         self.slot_name = '__' + name
 
     def __get__(self, obj, objtype):
@@ -257,54 +297,52 @@ class ScalarProperty(object):
                 value = getattr(obj, self.slot_name)
             except AttributeError:
                 domain = Persistance.get_domain()
-                a = domain.get_attributes(obj, [self.name])
+                a = domain.get_attributes(obj.id, [self.name])
                 if self.name in a:
-                    self.value.set_from_string(a[self.name])
-                    value = self.value.get()
+                    value = self.checker.from_string(a[self.name])
                     setattr(obj, self.slot_name, value)
                 else:
-                    self.__set__(obj, self.value.get())
+                    self.__set__(obj, self.checker.get())
         return value
 
     def __set__(self, obj, value):
         domain = Persistance.get_domain()
-        self.value.set(value)
-        setattr(obj, self.slot_name, self.value.get())
         try:
-            domain.put_attributes(obj, {self.name : self.value.get_as_string()}, replace=True)
+            domain.put_attributes(obj.id, {self.name : self.checker.to_string(value)}, replace=True)
         except:
             print 'Problem setting value: %s' % value
+        setattr(obj, self.slot_name, value)
 
 class StringProperty(ScalarProperty):
 
     def __init__(self, name, **params):
-        ScalarProperty.__init__(self, name, StringValue, **params)
+        ScalarProperty.__init__(self, name, StringChecker, **params)
 
 class PositiveIntegerProperty(ScalarProperty):
 
     def __init__(self, name, **params):
-        ScalarProperty.__init__(self, name, PositiveIntegerValue, **params)
+        ScalarProperty.__init__(self, name, PositiveIntegerChecker, **params)
 
 class BooleanProperty(ScalarProperty):
 
     def __init__(self, name, **params):
-        ScalarProperty.__init__(self, name, BooleanValue, **params)
+        ScalarProperty.__init__(self, name, BooleanChecker, **params)
 
 class DateTimeProperty(ScalarProperty):
 
     def __init__(self, name, **params):
-        ScalarProperty.__init__(self, name, DateTimeValue, **params)
+        ScalarProperty.__init__(self, name, DateTimeChecker, **params)
 
 class ObjectProperty(ScalarProperty):
 
     def __init__(self, name, **params):
-        ScalarProperty.__init__(self, name, ObjectValue, **params)
+        ScalarProperty.__init__(self, name, ObjectChecker, **params)
         
 class MultiValueProperty(object):
 
-    def __init__(self, name, value_class, **params):
+    def __init__(self, name, checker_class, **params):
         self.name = name
-        self.value = value_class(**params)
+        self.checker = checker_class(**params)
         self._list = None
 
     def __repr__(self):
@@ -314,11 +352,11 @@ class MultiValueProperty(object):
             return repr(self._list)
 
     def append(self, value):
-        self.value.set(value)
-        self._list.append(self.value.get())
+        self.checker.check(value)
+        self._list.append(value)
         domain = Persistance.get_domain()
         try:
-            domain.put_attributes(self.object, {self.name : self.value.get_as_string()}, replace=False)
+            domain.put_attributes(self.object.id, {self.name : self.checker.to_string(value)}, replace=False)
         except:
             print 'problem appending %s' % value
 
@@ -328,14 +366,14 @@ class MultiValueProperty(object):
             if self._list == None:
                 self._list = []
                 domain = Persistance.get_domain()
-                a = domain.get_attributes(obj, [self.name])
+                a = domain.get_attributes(obj.id, [self.name])
                 if self.name in a:
                     lst = a[self.name]
                     if not isinstance(lst, list):
                         lst = [lst]
                     for value in lst:
-                        self.value.set_from_string(value)
-                        self._list.append(self.value.get())
+                        value = self.checker.from_string(value)
+                        self._list.append(value)
         return self
 
     def __set__(self, obj, value):
@@ -344,11 +382,10 @@ class MultiValueProperty(object):
         self._list = value
         str_list = []
         for value in self._list:
-            self.value.set(value)
-            str_list.append(self.value.get_as_string())
+            str_list.append(self.checker.to_string(value))
         domain = Persistance.get_domain()
         try:
-            domain.put_attributes(obj, {self.name : str_list}, replace=True)
+            domain.put_attributes(obj.id, {self.name : str_list}, replace=True)
         except:
             print 'problem setting value: %s' % value
 
@@ -359,20 +396,20 @@ class MultiValueProperty(object):
 class StringListProperty(MultiValueProperty):
 
     def __init__(self, name, **params):
-        MultiValueProperty.__init__(self, name, StringValue, **params)
+        MultiValueProperty.__init__(self, name, StringChecker, **params)
 
 class PositiveIntegerListProperty(MultiValueProperty):
 
     def __init__(self, name, **params):
-        MultiValueProperty.__init__(self, name, PositiveIntegerValue, **params)
+        MultiValueProperty.__init__(self, name, PositiveIntegerChecker, **params)
 
 class BooleanListProperty(MultiValueProperty):
 
     def __init__(self, name, **params):
-        MultiValueProperty.__init__(self, name, BooleanValue, **params)
+        MultiValueProperty.__init__(self, name, BooleanChecker, **params)
 
 class ObjectListProperty(MultiValueProperty):
 
     def __init__(self, name, **params):
-        MultiValueProperty.__init__(self, name, ObjectValue, **params)
+        MultiValueProperty.__init__(self, name, ObjectChecker, **params)
         
