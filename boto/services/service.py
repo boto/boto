bc00017fd46af815974239f0e8a9113de2a2ec13
Mitch.Garnaat <Mitch.Garnaat@604d75c7-a419-0410-a38f-bde1a0bd1dbf>
2008-03-26 03:53:11 +0000

    Lots of clean up to work in new pyami structure, etc.

--- boto/services/service.py
+++ boto/services/service.py
@@ -34,12 +34,6 @@ import mimetypes
 
 class Service(ScriptBase):
 
-    # Number of times to retry failed requests to S3 or SQS
-    RetryCount = 5
-
-    # Number of seconds to wait between Retries
-    RetryDelay = 5
-
     # Number of times to retry queue read when no messages are available
     MainLoopRetryCount = 5
     
@@ -49,9 +43,9 @@ class Service(ScriptBase):
     # Time required to process a transaction
     ProcessingTime = 60
 
-
     def __init__(self):
         self.name = self.__class__.__name__
+        self.working_dir = boto.config.get('Pyami', 'working_dir')
         self.queue_cache = {}
         self.bucket_cache = {}
         self.create_connections()
@@ -59,7 +53,7 @@ class Service(ScriptBase):
 #            mimetypes.init(mimetype_files)
 
     def create_connections(self):
-        self.sqs_conn = SQSConnection()
+        self.sqs_conn = boto.connect_sqs()
         queue_name = boto.config.get(self.name, 'input_queue_name', None)
         if queue_name:
             self.input_queue = self.get_queue(queue_name)
@@ -70,7 +64,7 @@ class Service(ScriptBase):
             self.output_queue = self.get_queue(queue_name)
         else:
             self.output_queue = None
-        self.s3_conn = S3Connection()
+        self.s3_conn = boto.connect_s3()
 
     def split_key(key):
         if key.find(';') < 0:
@@ -118,54 +112,37 @@ class Service(ScriptBase):
         return m
 
     def read_message(self):
-        self.log(method='read_message')
+        boto.log.info('read_message')
         message = self.input_queue.read(self.ProcessingTime)
         if message:
-            print message.get_body()
+            boto.log.info(message.get_body())
             key = 'Service-Read'
             message[key] = get_ts()
         return message
 
     # retrieve the source file from S3
-    def get_file(self, bucket_name, key_name, file_name):
-        self.log(method='get_file', bucket_name=bucket_name,
-                 key=key_name, file_name=file_name)
-        print 'getting file %s.%s' % (bucket_name, key_name)
-        bucket = self.get_bucket(bucket_name)
-        key = Key(bucket)
-        key.name = key_name
-        key.get_contents_to_filename(file_name)
-
-    # retrieve the source file from S3
     def get_file(self, message):
         bucket_name = message['Bucket']
         key_name = message['InputKey']
-        working_dir = boto.config.get('Pyami', 'working_dir')
-        file_name = os.path.join(working_dir, message.get('OriginalFileName', 'in_file'))
-        self.log(method='get_file', bucket_name=bucket_name,
-                 key=key_name, file_name=file_name)
-        print 'getting file %s.%s' % (bucket_name, key_name)
+        file_name = os.path.join(self.working_dir, message.get('OriginalFileName', 'in_file'))
+        boto.log.info('get_file: %s/%s to %s' % (bucket_name, key_name, file_name))
         bucket = self.get_bucket(bucket_name)
         key = Key(bucket)
         key.name = key_name
-        key.get_contents_to_filename(os.path.join(working_dir, file_name))
+        key.get_contents_to_filename(os.path.join(self.working_dir, file_name))
         return file_name
 
     # process source file, return list of output files
     def process_file(self, in_file_name, msg):
         return []
 
-    def determine_key_name(self, m, mimetype):
-        preserve = boto.config.getbool(self.name, 'preserve_filename')
-        key_name = None
-        if m.has_key('PreserveFileName'):
-            if m['PreserveFileName'].lower() == 'true':
-                preserve = True
-        if preserve:
-            if m.has_key('OriginalFileName'):
-                file_name, ext = os.path.splitext(m['OriginalFileName'])
-                key_name = file_name + mimetypes.guess_extension(mimetype)
-        return key_name
+    # store result file in S3
+    def put_file(self, bucket_name, file_path, key_name=None):
+        boto.log.info('putting file %s as %s.%s' % (file_path, bucket_name, key_name))
+        bucket = self.get_bucket(bucket_name)
+        key = bucket.new_key(key_name)
+        key.set_contents_from_filename(file_path)
+        return key
 
     def save_results(self, results, input_message, output_message):
         output_keys = []
@@ -174,35 +151,15 @@ class Service(ScriptBase):
                 output_bucket = input_message['OutputBucket']
             else:
                 output_bucket = input_message['Bucket']
-            key_name = self.determine_key_name(input_message, type)
+            key_name = os.path.split(file)[1]
             key = self.put_file(output_bucket, file, key_name)
             output_keys.append('%s;type=%s' % (key.name, type))
         output_message['OutputKey'] = ','.join(output_keys)
             
-    # store result file in S3
-    def put_file(self, bucket_name, file_path, key_name=None):
-        self.log(method='put_file', bucket_name=bucket_name, file_path=file_path,
-                 key_name=key_name)
-        successful = False
-        num_tries = 0
-        while not successful and num_tries < self.RetryCount:
-            try:
-                num_tries += 1
-                bucket = self.get_bucket(bucket_name)
-                key = bucket.new_key(key_name)
-                key.set_contents_from_filename(file_path)
-                print 'putting file %s as %s.%s' % (file_path, bucket_name, key.name)
-                successful = True
-            except S3ResponseError, e:
-                print 'caught S3Error'
-                print e
-                time.sleep(self.RetryDelay)
-        return key
-
     # write message to each output queue
     def write_message(self, message):
         if self.output_queue:
-            self.log(method='write_message')
+            boto.log.info('Writing message to %s' % self.output_queue.id)
             message['Service-Write'] = time.strftime("%a, %d %b %Y %H:%M:%S GMT",
                                                      time.gmtime())
             message['Server'] = self.name
@@ -210,13 +167,11 @@ class Service(ScriptBase):
                 message['Host'] = os.environ['HOSTNAME']
             else:
                 message['Host'] = 'unknown'
-            print 'writing message to %s' % self.output_queue.id
             self.output_queue.write(message)
 
     # delete message from input queue
     def delete_message(self, message):
-        self.log(method='delete_message')
-        print 'deleting message from %s' % self.input_queue.id
+        boto.log.info('deleting message from %s' % self.input_queue.id)
         self.input_queue.delete_message(message)
 
     # to clean up any files, etc. after each iteration
@@ -224,16 +179,16 @@ class Service(ScriptBase):
         pass
 
     def shutdown(self):
-        on_completion = boto.config.get(self.name, 'on_completion', 'shutdown')
+        on_completion = boto.config.get(self.name, 'on_completion', 'stay_alive')
         if on_completion == 'shutdown':
             instance_id = boto.config.get('Instance', 'instance-id')
             if instance_id:
                 time.sleep(60)
-                c = EC2Connection()
+                c = boto.connect_ec2()
                 c.terminate_instances([instance_id])
 
-    def run(self, notify=False):
-        self.notify('Service Starting')
+    def main(self, notify=False):
+        self.notify('Service: %s Starting' % self.name)
         empty_reads = 0
         while empty_reads < self.MainLoopRetryCount:
             try:
@@ -242,7 +197,7 @@ class Service(ScriptBase):
                     empty_reads = 0
                     output_message = ServiceMessage(None, input_message.get_body())
                     input_file = self.get_file(input_message)
-                    results = self.process_file(os.path.join(wd, in_file_name), output_message)
+                    results = self.process_file(input_file, output_message)
                     self.save_results(results, input_message, output_message)
                     self.write_message(output_message)
                     self.delete_message(input_message)
@@ -251,12 +206,9 @@ class Service(ScriptBase):
                     empty_reads += 1
                     time.sleep(self.MainLoopDelay)
             except Exception, e:
+                boto.log.exception('Service Failed')
                 empty_reads += 1
-                fp = StringIO.StringIO()
-                traceback.print_exc(None, fp)
-                s = fp.getvalue()
-                self.notify('Service failed\n%s' % s)
                 self.create_connections()
-        self.notify('Service Shutting Down')
+        self.notify('Service: %s Shutting Down' % self.name)
         self.shutdown()
 
