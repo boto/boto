75bb7b9f6c3a90c09b6b2706fd572d46e4a90b09
Mitch.Garnaat <Mitch.Garnaat@604d75c7-a419-0410-a38f-bde1a0bd1dbf>
2008-03-26 03:51:36 +0000

    Some tweaks to scriptbase to better handle status from shell commands and to pass error output to logger.

--- boto/pyami/scriptbase.py
+++ boto/pyami/scriptbase.py
@@ -9,21 +9,21 @@ class ScriptBase:
         self.instance_id = boto.config.get_instance('instance-id', 'default')
         self.ts = get_ts()
 
-    def notify(self, subject):
+    def notify(self, subject, body=''):
         to_string = boto.config.get_value('Notification', 'smtp_to', None)
         if to_string:
             try:
                 from_string = boto.config.get_value('Notification', 'smtp_from', 'boto')
-                body = "From: %s\n" % from_string
-                body += "To: %s\n" % to_string
-                body += "Subject: %s\n\n" % subject
-                #body += self.log_fp.getvalue()
+                msg = "From: %s\n" % from_string
+                msg += "To: %s\n" % to_string
+                msg += "Subject: %s\n\n" % subject
+                msg += body
                 smtp_host = boto.config.get_value('Notification', 'smtp_host', 'localhost')
                 server = smtplib.SMTP(smtp_host)
                 smtp_user = boto.config.get_value('Notification', 'smtp_user', '')
                 smtp_pass = boto.config.get_value('Notification', 'smtp_pass', '')
                 server.login(smtp_user, smtp_pass)
-                server.sendmail(from_string, to_string, body)
+                server.sendmail(from_string, to_string, msg)
                 server.quit()
             except:
                 boto.log.error('notify failed')
@@ -42,10 +42,12 @@ class ScriptBase:
     def run(self, command, notify=False, exit_on_error=False):
         self.last_command = ShellCommand(command)
         if self.last_command.status != 0:
+            boto.log.error(self.last_command.output)
             if notify:
-                self.notify('Error encountered')
+                self.notify('Error encountered', self.last_command.output)
             if exit_on_error:
                 sys.exit(-1)
+        return self.last_command.status
 
     def main(self):
         pass
