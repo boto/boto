99152048fd51794c1995c23742f789da4d8de5f0
Mitch.Garnaat <Mitch.Garnaat@604d75c7-a419-0410-a38f-bde1a0bd1dbf>
2008-05-27 18:52:49 +0000

    Removing reference to SDBPersistance typo that crept in during update merge.

--- boto/sdb/persist/property.py
+++ boto/sdb/persist/property.py
@@ -19,7 +19,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 # IN THE SOFTWARE.
 
-from boto.exception import SDBPersistanceError
+from boto.exception import SDBPersistenceError
 from boto.sdb.persist.checker import *
 from boto.utils import Password
 
