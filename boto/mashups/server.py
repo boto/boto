dc444c07978628103947eaaa2a61e09aecef6a77
kopertop <kopertop@604d75c7-a419-0410-a38f-bde1a0bd1dbf>
2008-05-27 18:44:42 +0000

    Fixed the grabbing of the instance_id after the server has started

--- boto/mashups/server.py
+++ boto/mashups/server.py
@@ -200,7 +200,7 @@ class Server(SDBObject):
                     instance_type = self.instance_type,
                     placement = self.zone,
                     user_data = self._config)
-        i = r.next()
+        i = r.instances.next()
         self.instance_id = i.id
         if self.elastic_ip:
             ec2.associate_address(self.instance_id, self.elastic_ip)
