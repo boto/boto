2618bc3588172a2c317a0d8c17b1dce2a681f7a4
Mitch.Garnaat <Mitch.Garnaat@604d75c7-a419-0410-a38f-bde1a0bd1dbf>
2009-01-13 21:04:15 +0000

    Allow proxy_user and proxy_pass to be set for S3Connection.  Fixes issue 197.

--- boto/s3/connection.py
+++ boto/s3/connection.py
@@ -93,13 +93,14 @@ class S3Connection(AWSAuthConnection):
 
     def __init__(self, aws_access_key_id=None, aws_secret_access_key=None,
                  is_secure=True, port=None, proxy=None, proxy_port=None,
+                 proxy_user=None, proxy_pass=None,
                  host=DefaultHost, debug=0, https_connection_factory=None,
                  calling_format=SubdomainCallingFormat()):
         self.calling_format = calling_format
         AWSAuthConnection.__init__(self, host,
                 aws_access_key_id, aws_secret_access_key,
-                is_secure, port, proxy, proxy_port, debug=debug,
-                https_connection_factory=https_connection_factory)
+                is_secure, port, proxy, proxy_port, proxy_user, proxy_pass,
+                debug=debug, https_connection_factory=https_connection_factory)
 
     def __iter__(self):
         return self.get_all_buckets()
