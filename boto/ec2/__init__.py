45fa15d8b051a86f04a3d188fda8db42eea4233d
Mitch.Garnaat <Mitch.Garnaat@604d75c7-a419-0410-a38f-bde1a0bd1dbf>
2009-05-06 14:47:16 +0000

    Allow keyword params to be passed to connect_to_region method.  Addresses issue 232.

--- boto/ec2/__init__.py
+++ boto/ec2/__init__.py
@@ -38,9 +38,9 @@ def regions(**kw_params):
     c = EC2Connection(**kw_params)
     return c.get_all_regions()
 
-def connect_to_region(region_name):
+def connect_to_region(region_name, **kw_params):
     for region in regions():
         if region.name == region_name:
-            return region.connect()
+            return region.connect(**kw_params)
     return None
     
