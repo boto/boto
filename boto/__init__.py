166bbe57361786920cda7e2704a77312d56c5138
Mitch.Garnaat <Mitch.Garnaat@604d75c7-a419-0410-a38f-bde1a0bd1dbf>
2009-05-22 03:10:00 +0000

    Adding connect_cloudwatch method.

--- boto/__init__.py
+++ boto/__init__.py
@@ -108,6 +108,20 @@ def connect_elb(aws_access_key_id=None, aws_secret_access_key=None, **kwargs):
     from boto.ec2.elb import ELBConnection
     return ELBConnection(aws_access_key_id, aws_secret_access_key, **kwargs)
 
+def connect_cloudwatch(aws_access_key_id=None, aws_secret_access_key=None, **kwargs):
+    """
+    @type aws_access_key_id: string
+    @param aws_access_key_id: Your AWS Access Key ID
+    
+    @type aws_secret_access_key: string
+    @param aws_secret_access_key: Your AWS Secret Access Key
+    
+    @rtype: L{CloudWatchConnection<boto.ec2.cloudwatch.CloudWatchConnection>}
+    @return: A connection to Amazon's EC2 Monitoring service
+    """
+    from boto.ec2.cloudwatch import CloudWatchConnection
+    return CloudWatchConnection(aws_access_key_id, aws_secret_access_key, **kwargs)
+
 def connect_sdb(aws_access_key_id=None, aws_secret_access_key=None, **kwargs):
     """
     @type aws_access_key_id: string
