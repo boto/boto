55731bfed1775c74825a4c6cd2d860ffedf812cf
Mitch.Garnaat <Mitch.Garnaat@604d75c7-a419-0410-a38f-bde1a0bd1dbf>
2009-09-30 22:39:09 +0000

    Including forgotten regioninfo.py from last commit.

---
+++ boto/sdb/regioninfo.py
@@ -0,0 +1,40 @@
+# Copyright (c) 2006-2009 Mitch Garnaat http://garnaat.org/
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish, dis-
+# tribute, sublicense, and/or sell copies of the Software, and to permit
+# persons to whom the Software is furnished to do so, subject to the fol-
+# lowing conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABIL-
+# ITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+# SHALL THE AUTHOR BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+#
+
+from boto.ec2.regioninfo import RegionInfo
+
+class SDBRegionInfo(RegionInfo):
+
+    def connect(self, **kw_params):
+        """
+        Connect to this Region's endpoint. Returns an SDBConnection
+        object pointing to the endpoint associated with this region.
+        You may pass any of the arguments accepted by the SDBConnection
+        object's constructor as keyword arguments and they will be
+        passed along to the SDBConnection object.
+        
+        :rtype: :class:`boto.sdb.connection.SDBConnection`
+        :return: The connection to this regions endpoint
+        """
+        from boto.sdb.connection import SDBConnection
+        return SDBConnection(region=self, **kw_params)
+
