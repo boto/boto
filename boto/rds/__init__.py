f85adfa58199a0a311f4d97681b11bb0fc9b0555
Mitch.Garnaat <Mitch.Garnaat@604d75c7-a419-0410-a38f-bde1a0bd1dbf>
2009-10-30 22:27:10 +0000

    Fixing a typo bug in RDSConnection contructor.

--- boto/rds/__init__.py
+++ boto/rds/__init__.py
@@ -43,7 +43,7 @@ class RDSConnection(AWSQueryConnection):
 
     def __init__(self, aws_access_key_id=None, aws_secret_access_key=None,
                  is_secure=True, port=None, proxy=None, proxy_port=None,
-                 proxy_user=None, proxy_pass=None, host=DefaultHost, debug,
+                 proxy_user=None, proxy_pass=None, host=DefaultHost, debug=0,
                  https_connection_factory=None, path='/'):
         AWSQueryConnection.__init__(self, aws_access_key_id, aws_secret_access_key,
                                     is_secure, port, proxy, proxy_port, proxy_user,
