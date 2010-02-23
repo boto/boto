22384be318b4b026415d8f22b31bb75fdefd4d6e
paltman <paltman@604d75c7-a419-0410-a38f-bde1a0bd1dbf>
2010-02-23 05:10:46 +0000

    attempt using the hudson environemnt variable to get revision in doc build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,12 +9,10 @@ master_doc = 'index'
 project = u'boto'
 copyright = u'2009,2010, Mitch Garnaat'
 version = '1.9'
-release = "HEAD" #'1.8d'
 exclude_trees = []
 pygments_style = 'sphinx'
 html_theme = 'boto_theme'
 html_theme_path = ["."]
-html_title = "boto v%s (r%s)" % (version, release)
 html_static_path = ['_static']
 htmlhelp_basename = 'botodoc'
 latex_documents = [
@@ -24,12 +22,9 @@ latex_documents = [
 intersphinx_mapping = {'http://docs.python.org/': None}
 
 try:
-    import subprocess, os
-    print os.environ
-    release = os.environ['SVN_REVISION']
+    release = os.environ.get('SVN_REVISION', 'HEAD')
     print release
-    # p = subprocess.Popen(["svn info ../boto | grep Revision | awk '{print $2}'"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-    # release = p.stdout.read().strip()
-    # print p.stderr.read()
 except Exception, e:
     print e
+
+html_title = "boto v%s (r%s)" % (version, release)
