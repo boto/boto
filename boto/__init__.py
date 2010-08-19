7bb3a030da0b45d5197d6a9c2a3025aa8deb10b5
Mitch Garnaat <mitch@cloudright.com>
2010-08-19 12:35:59 -0400

    Bumping version number prior to release.

--- boto/__init__.py
+++ boto/__init__.py
@@ -28,7 +28,7 @@ import logging
 import logging.config
 from boto.exception import InvalidUriError
 
-__version__ = '2.0b1'
+__version__ = '2.0b2'
 Version = __version__ # for backware compatibility
 
 UserAgent = 'Boto/%s (%s)' % (__version__, sys.platform)
