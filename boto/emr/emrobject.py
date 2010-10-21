c6e78d2d57f180eb7c5bc388c2214fc891cdd570
Jeremy Thurgood <firxen@gmail.com>
2010-10-21 16:11:30 +0200

    Fixed EMR handling of new DescribeJobFlows response format and added a couple of unit tests for this.

--- boto/emr/emrobject.py
+++ boto/emr/emrobject.py
@@ -1,4 +1,5 @@
 # Copyright (c) 2010 Spotify AB
+# Copyright (c) 2010 Jeremy Thurgood <firxen+boto@gmail.com>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the
@@ -25,6 +26,7 @@ This module contains EMR response objects
 
 from boto.resultset import ResultSet
 
+
 class EmrObject(object):
     Fields = set()
 
@@ -38,8 +40,10 @@ class EmrObject(object):
         if name in self.Fields:
             setattr(self, name.lower(), value)
 
+
 class RunJobFlowResponse(EmrObject):
     Fields = set(['JobFlowId'])
+
 
 class Arg(EmrObject):
     def __init__(self, connection=None):
@@ -57,16 +61,33 @@ class Step(EmrObject):
                   'EndDateTime',
                   'LastStateChangeReason',
                   'State'])
-    
+
     def __init__(self, connection=None):
         self.connection = connection
-        self.args = None 
+        self.args = None
 
     def startElement(self, name, attrs, connection):
         if name == 'Args':
             self.args = ResultSet([('member', Arg)])
             return self.args
- 
+
+
+class InstanceGroup(EmrObject):
+    Fields = set(['Name',
+                  'CreationDateTime',
+                  'InstanceRunningCount',
+                  'StartDateTime',
+                  'ReadyDateTime',
+                  'State',
+                  'EndDateTime',
+                  'InstanceRequestCount',
+                  'InstanceType',
+                  'Market',
+                  'LastStateChangeReason',
+                  'InstanceRole',
+                  'InstanceGroupId'])
+
+
 class JobFlow(EmrObject):
     Fields = set(['CreationDateTime',
                   'StartDateTime',
@@ -77,7 +98,7 @@ class JobFlow(EmrObject):
                   'JobFlowId',
                   'KeepJobAliveWhenNoSteps',
                   'LogUri',
-                  'MasterPublicDnsName', 
+                  'MasterPublicDnsName',
                   'MasterInstanceId',
                   'Name',
                   'Placement',
@@ -94,11 +115,15 @@ class JobFlow(EmrObject):
     def __init__(self, connection=None):
         self.connection = connection
         self.steps = None
+        self.instancegroups = None
 
     def startElement(self, name, attrs, connection):
         if name == 'Steps':
             self.steps = ResultSet([('member', Step)])
             return self.steps
+        elif name == 'InstanceGroups':
+            self.instancegroups = ResultSet([('member', InstanceGroup)])
+            return self.instancegroups
         else:
             return None
 
