88ba7e63f1a11a7f173d2b5b46b422bcadc707ce
Brad Greenlee <brad@daylife.com>
2010-11-09 17:49:53 -0800

    Add option to boto.cfg to enable sandbox mode in mturk.
    If the following is in boto.cfg:
    
    [MTurk]
    sandbox = True
    
    An MTurkConnection created without specifying a host will default to the Mechanical Turk sandbox (mechanicalturk.sandbox.amazonaws.com).

--- boto/mturk/connection.py
+++ boto/mturk/connection.py
@@ -24,6 +24,7 @@ import datetime
 import itertools
 
 from boto import handler
+from boto import config
 from boto.mturk.price import Price
 import boto.mturk.notification
 from boto.connection import AWSQueryConnection
@@ -43,8 +44,14 @@ class MTurkConnection(AWSQueryConnection):
     def __init__(self, aws_access_key_id=None, aws_secret_access_key=None,
                  is_secure=False, port=None, proxy=None, proxy_port=None,
                  proxy_user=None, proxy_pass=None,
-                 host='mechanicalturk.amazonaws.com', debug=0,
+                 host=None, debug=0,
                  https_connection_factory=None):
+        if not host:
+            if config.has_option('MTurk', 'sandbox') and config.get('MTurk', 'sandbox') == 'True':
+                host = 'mechanicalturk.sandbox.amazonaws.com'
+            else:
+                host = 'mechanicalturk.amazonaws.com'
+
         AWSQueryConnection.__init__(self, aws_access_key_id,
                                     aws_secret_access_key,
                                     is_secure, port, proxy, proxy_port,
