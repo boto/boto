1b3ca0e5fe5974886577909b872ae3aabe3e346a
Nick Tarleton <nick@quixey.com>
2011-04-01 22:08:49 -0700

    Fix relative imports

--- boto/ses/__init__.py
+++ boto/ses/__init__.py
@@ -20,5 +20,5 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 # IN THE SOFTWARE.
 
-from connection import SESConnection
+from boto.ses.connection import SESConnection
 
