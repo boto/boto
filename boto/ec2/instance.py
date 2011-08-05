35bccedc73d007f9aeaaddec0ca27f3e8ffc463a
Mitch Garnaat <mitch@garnaat.com>
2011-08-05 09:16:40 -0400

    Fix parsing of DescribeInstanceAttributes response.  Closes #291.

--- boto/ec2/instance.py
+++ boto/ec2/instance.py
@@ -379,17 +379,35 @@ class ConsoleOutput:
 
 class InstanceAttribute(dict):
 
+    ValidValues = ['instanceType', 'kernel', 'ramdisk', 'userData',
+                   'disableApiTermination', 'instanceInitiatedShutdownBehavior',
+                   'rootDeviceName', 'blockDeviceMapping', 'sourceDestCheck',
+                   'groupSet']
+
     def __init__(self, parent=None):
         dict.__init__(self)
+        self.instance_id = None
+        self.request_id = None
         self._current_value = None
 
     def startElement(self, name, attrs, connection):
-        return None
+        if name == 'blockDeviceMapping':
+            self[name] = BlockDeviceMapping()
+            return self[name]
+        elif name == 'groupSet':
+            self[name] = ResultSet([('item', Group)])
+            return self[name]
+        else:
+            return None
 
     def endElement(self, name, value, connection):
-        if name == 'value':
+        if name == 'instanceId':
+            self.instance_id = value
+        elif name == 'requestId':
+            self.request_id = value
+        elif name == 'value':
             self._current_value = value
-        else:
+        elif name in self.ValidValues:
             self[name] = self._current_value
 
 class StateReason(dict):
