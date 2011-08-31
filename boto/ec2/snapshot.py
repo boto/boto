8909fb81ee226e18d8f2fa2d3e0527a92f1b8279
Mitch Garnaat <mitch@garnaat.com>
2011-08-31 19:11:18 -0400

    Fixing docstring.  Also a couple of PEP8 cleanups.  Closes #319.

--- boto/ec2/snapshot.py
+++ boto/ec2/snapshot.py
@@ -21,11 +21,13 @@
 # IN THE SOFTWARE.
 
 """
-Represents an EC2 Elastic IP Snapshot
+Represents an EC2 Elastic Block Store Snapshot
 """
 from boto.ec2.ec2object import TaggedEC2Object
 
 class Snapshot(TaggedEC2Object):
+    
+    AttrName = 'createVolumePermission'
     
     def __init__(self, connection=None):
         TaggedEC2Object.__init__(self, connection)
@@ -88,26 +90,26 @@ class Snapshot(TaggedEC2Object):
         return self.connection.delete_snapshot(self.id)
 
     def get_permissions(self):
-        attrs = self.connection.get_snapshot_attribute(self.id,
-                                                       attribute='createVolumePermission')
+        attrs = self.connection.get_snapshot_attribute(self.id, self.AttrName)
         return attrs.attrs
 
     def share(self, user_ids=None, groups=None):
         return self.connection.modify_snapshot_attribute(self.id,
-                                                         'createVolumePermission',
+                                                         self.AttrName,
                                                          'add',
                                                          user_ids,
                                                          groups)
 
     def unshare(self, user_ids=None, groups=None):
         return self.connection.modify_snapshot_attribute(self.id,
-                                                         'createVolumePermission',
+                                                         self.AttrName,
                                                          'remove',
                                                          user_ids,
                                                          groups)
 
     def reset_permissions(self):
-        return self.connection.reset_snapshot_attribute(self.id, 'createVolumePermission')
+        return self.connection.reset_snapshot_attribute(self.id,
+                                                        self.AttrName)
 
 class SnapshotAttribute:
 
