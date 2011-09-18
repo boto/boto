0a37a8787ca204d8bb945dc27ec2f846dc9e57a0
Samuel Lucidi <samuel@exemplar.(none)>
2011-09-17 21:34:35 -0400

    Added delete_attributes to sdb.db.model.Model

--- boto/sdb/db/model.py
+++ boto/sdb/db/model.py
@@ -219,6 +219,19 @@ class Model(object):
         self.reload()
         return self
 
+    def delete_attributes(self, attrs):
+        """Delete just these attributes,
+        not the whole object.
+        :param attrs: Attributes to save, as a list of string names
+        :type attrs: list
+        :return: self
+        :rtype: :class:boto.sdb.db.model.Model
+        """
+        assert(isinstance(attrs, list)), "Argument must be a list of names of keys to delete."
+        self._manager.domain.delete_attributes(self.id, attrs)
+        self.reload()
+        return self
+
     save_attributes = put_attributes
         
     def delete(self):
