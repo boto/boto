38485af50be478db9cc17dbe4b48304283945ab3
Mike Milner <milner@blissisland.ca>
2011-10-03 23:35:23 -0300

    Added cloudfront tests to unittest suite.

--- tests/test.py
+++ tests/test.py
@@ -37,6 +37,7 @@ from s3.test_https_cert_validation import CertValidationTest
 from ec2.test_connection import EC2ConnectionTest
 from autoscale.test_connection import AutoscaleConnectionTest
 from sdb.test_connection import SDBConnectionTest
+from cloudfront.test_signed_urls import CloudfrontSignedUrlsTest
 
 def usage():
     print "test.py  [-t testsuite] [-v verbosity]"
@@ -80,6 +81,7 @@ def suite(testsuite="all"):
         tests.addTest(unittest.makeSuite(EC2ConnectionTest))
         tests.addTest(unittest.makeSuite(SDBConnectionTest))
         tests.addTest(unittest.makeSuite(AutoscaleConnectionTest))
+        tests.addTest(unittest.makeSuite(CloudfrontSignedUrlsTest))
     elif testsuite == "s3":
         tests.addTest(unittest.makeSuite(S3ConnectionTest))
         tests.addTest(unittest.makeSuite(S3VersionTest))
@@ -99,6 +101,8 @@ def suite(testsuite="all"):
         tests.addTest(unittest.makeSuite(AutoscaleConnectionTest))
     elif testsuite == "sdb":
         tests.addTest(unittest.makeSuite(SDBConnectionTest))
+    elif testsuite == "cloudfront":
+        tests.addTest(unittest.makeSuite(CloudfrontSignedUrlsTest))
     else:
         raise ValueError("Invalid choice.")
     return tests
