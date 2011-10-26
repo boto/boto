547d45997c6674a408ca732c1a5816596c352cb6
Mitch Garnaat <mitch@garnaat.com>
2011-10-26 14:26:23 -0400

    Change default to use HTTPS rather than HTTP connection for MTurk.  Closes #381.

--- boto/mturk/connection.py
+++ boto/mturk/connection.py
@@ -41,7 +41,7 @@ class MTurkConnection(AWSQueryConnection):
     APIVersion = '2008-08-02'
     
     def __init__(self, aws_access_key_id=None, aws_secret_access_key=None,
-                 is_secure=False, port=None, proxy=None, proxy_port=None,
+                 is_secure=True, port=None, proxy=None, proxy_port=None,
                  proxy_user=None, proxy_pass=None,
                  host=None, debug=0,
                  https_connection_factory=None):
