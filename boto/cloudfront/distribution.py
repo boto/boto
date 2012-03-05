ec344c4e6cbd96e9554b36060525d76da5c522de
James Brown <jbrown@yelp.com>
2012-03-05 13:29:09 -0800

    restore Python 2.5.x compatibility

--- boto/cloudfront/distribution.py
+++ boto/cloudfront/distribution.py
@@ -21,7 +21,10 @@
 
 import uuid
 import base64
-import json
+try:
+    import simplejson as json
+except ImportError:
+    import json
 from boto.cloudfront.identity import OriginAccessIdentity
 from boto.cloudfront.object import Object, StreamingObject
 from boto.cloudfront.signers import ActiveTrustedSigners, TrustedSigners
