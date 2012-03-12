c90a2fef7fa1a3004b0cfb1d6c21b954df181c92
blinsay <blinsay@gmail.com>
2012-03-11 18:30:48 -0700

    Removed default Hadoop version. Updated docstring.

--- boto/emr/connection.py
+++ boto/emr/connection.py
@@ -245,9 +245,8 @@ class EmrConnection(AWSQueryConnection):
             should be enabled.
 
         :type hadoop_version: str
-        :param hadoop_version: Version of Hadoop to use. If ami_version
-            is not set, defaults to '0.20' for backwards compatibility
-            with older versions of boto.
+        :param hadoop_version: Version of Hadoop to use. This no longer
+        defaults to '0.20' and now uses the AMI default. 
 
         :type steps: list(boto.emr.Step)
         :param steps: List of steps to add with the job
@@ -280,11 +279,6 @@ class EmrConnection(AWSQueryConnection):
         :rtype: str
         :return: The jobflow id
         """
-        # hadoop_version used to default to '0.20', but this won't work
-        # on later AMI versions, so only default if it ami_version isn't set.
-        if not (hadoop_version or ami_version):
-            hadoop_version = '0.20'
-
         params = {}
         if action_on_failure:
             params['ActionOnFailure'] = action_on_failure
