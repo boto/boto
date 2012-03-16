9c5afadaaedf620890e51b7925b7ae0736ca3a3d
Andy O'Neill <oneill@energyhub.net>
2012-03-16 16:33:48 -0400

    Remove redundant build_list_params call

--- boto/ec2/elb/__init__.py
+++ boto/ec2/elb/__init__.py
@@ -459,7 +459,6 @@ class ELBConnection(AWSQueryConnection):
             self.build_list_params(params, policies, 'PolicyNames.member.%d')
         else:
             params["PolicyNames"] = ""
-        self.build_list_params(params, policies, 'PolicyNames.member.%d')
         return self.get_status('SetLoadBalancerPoliciesOfListener', params)
 
     def apply_security_groups_to_lb(self, name, security_groups):
