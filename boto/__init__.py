94c739193a12fb2fca1062af3b2031fd6a3c3344
Mitch Garnaat <mitch@garnaat.com>
2012-03-18 14:23:38 -0700

    Changing version tag in master to denote it is now 2.3.0-dev.

--- boto/__init__.py
+++ boto/__init__.py
@@ -31,7 +31,7 @@ import logging.config
 import urlparse
 from boto.exception import InvalidUriError
 
-__version__ = '2.2.2-dev'
+__version__ = '2.3.0-dev'
 Version = __version__ # for backware compatibility
 
 UserAgent = 'Boto/%s (%s)' % (__version__, sys.platform)
