ae3d1add4b725ca2654e9aa6e1f1ab92db69e407
Stanislav Ievlev <stanislav.ievlev@gmail.com>
2012-04-26 16:58:26 +0400

    drop unused parameters in stop_instances() method

--- boto/ec2/connection.py
+++ boto/ec2/connection.py
@@ -716,7 +716,7 @@ class EC2Connection(AWSQueryConnection):
         return self.get_list('TerminateInstances', params,
                              [('item', Instance)], verb='POST')
 
-    def stop_instances(self, instance_ids=None, force=False, suspend=False):
+    def stop_instances(self, instance_ids=None, force=False):
         """
         Stop the instances specified
 
@@ -726,17 +726,12 @@ class EC2Connection(AWSQueryConnection):
         :type force: bool
         :param force: Forces the instance to stop
 
-        :type suspend: bool
-        :param suspend: Suspend instances instead of stopping
-
         :rtype: list
         :return: A list of the instances stopped
         """
         params = {}
         if force:
             params['Force'] = 'true'
-        if suspend:
-            params['Suspend'] = 'true'
         if instance_ids:
             self.build_list_params(params, instance_ids, 'InstanceId')
         return self.get_list('StopInstances', params,
