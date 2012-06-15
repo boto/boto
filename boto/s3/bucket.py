c9b69e0c592777677255509f9c54f7df0cc29eb1
Mike Schwartz <mfschwartz@google.com>
2012-06-15 11:11:34 -0700

    Change to UTF-8 encoding for ACL content

--- boto/s3/bucket.py
+++ boto/s3/bucket.py
@@ -749,7 +749,7 @@ class Bucket(object):
         if version_id:
             query_args += '&versionId=%s' % version_id
         response = self.connection.make_request('PUT', self.name, key_name,
-                                                data=acl_str.encode('ISO-8859-1'),
+                                                data=acl_str.encode('UTF-8'),
                                                 query_args=query_args,
                                                 headers=headers)
         body = response.read()
