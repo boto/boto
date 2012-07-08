da9778ef339cecc256897bb784fb37963025175e
Zachary Huff <zach.huff.386@gmail.com>
2012-07-08 14:11:06 -0400

    Updated ec2 api version from 2012-06-01 to 2012-06-15

--- boto/ec2/connection.py
+++ boto/ec2/connection.py
@@ -61,7 +61,7 @@ from boto.exception import EC2ResponseError
 
 class EC2Connection(AWSQueryConnection):
 
-    APIVersion = boto.config.get('Boto', 'ec2_version', '2012-06-01')
+    APIVersion = boto.config.get('Boto', 'ec2_version', '2012-06-15')
     DefaultRegionName = boto.config.get('Boto', 'ec2_region_name', 'us-east-1')
     DefaultRegionEndpoint = boto.config.get('Boto', 'ec2_region_endpoint',
                                             'ec2.us-east-1.amazonaws.com')
