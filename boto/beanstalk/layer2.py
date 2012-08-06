3274d850786c2cc7554e40d235387567b984cb07
James Saryerwinnie <jls.npi@gmail.com>
2012-08-06 11:19:33 -0700

    Minor modifications to beanstalk pull request
    * Use getattr instead of getattribute
    * Layer2 will create an instance of layer1, similar
    to how dynamodb's layer2 module works.
    * Reworked some of the unittests so they can be run independently,
      also after running the tests there were several resources that
      weren't deleted.
    * Moved the tests over to integration, since they interact
      with the service.

--- boto/beanstalk/layer2.py
+++ boto/beanstalk/layer2.py
@@ -8,44 +8,28 @@ from boto.exception import BotoServerError
 import boto.beanstalk.exception as exception
 
 
-class Layer2(Layer1):
-
-    PRIVATE_METHODS = re.compile('^_')
-
-    def __init__(self, *args, **kargs):
-        super(Layer2, self).__init__(*args, **kargs)
-
-        # used to store real method called so we can wrap around it
-        self._method_called = None
-
-    def __getattribute__(self, name):
-        # if it's a function defined in Layer1
-        if name in Layer1.__dict__ and hasattr(Layer1.__dict__[name], '__call__'):
-        # and not private
-            if not Layer2.PRIVATE_METHODS.match(name):
-        # then wrap the call
-                self._method_called = name
-                return self._wrapper
-        # otherwise do normal thing
-        return super(Layer2, self).__getattribute__(name)
-
-    # wrapper for layer1 api calls
-    def _wrapper(self, *args, **kargs):
-        # layer1 api call
-        name = self._method_called
-
+def beanstalk_wrapper(func, name):
+    def _wrapped_low_level_api(*args, **kwargs):
         try:
-            # get super's response
-            response = getattr(super(Layer2, self), name)(*args, **kargs)
-
-        except BotoServerError as e:
-            # throw a more descriptive error
+            response = func(*args, **kwargs)
+        except BotoServerError, e:
             raise exception.simple(e)
-
         # turn 'this_is_a_function_name' into 'ThisIsAFunctionNameResponse'
-        cls_name = ''.join( [ part.capitalize() for part in name.split('_') ] ) + 'Response'
+        cls_name = ''.join([part.capitalize() for part in name.split('_')]) + 'Response'
         # get the class object from boto.beanstalk.response
-        Class = getattr(Wrapper, cls_name)
+        cls = getattr(Wrapper, cls_name)
 
         # return class instead
-        return Class(response)
+        return cls(response)
+    return _wrapped_low_level_api
+
+
+class Layer2(object):
+    def __init__(self, *args, **kwargs):
+        self.api = Layer1(*args, **kwargs)
+
+    def __getattr__(self, name):
+        try:
+            return beanstalk_wrapper(getattr(self.api, name), name)
+        except AttributeError, e:
+            raise AttributeError("%s has no attribute %r" % (self, name))
