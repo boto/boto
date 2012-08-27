9d6b913497902d2b4890d4c03782dfcef0b9be9a
Mitch Garnaat <mitch@garnaat.com>
2012-08-27 15:08:33 -0700

    Make sure headers is a dict.  Fixes #914.

--- boto/s3/key.py
+++ boto/s3/key.py
@@ -1164,6 +1164,8 @@ class Key(object):
             with the stored object in the response.  See
             http://goo.gl/EWOPb for details.
         """
+        if headers is None:
+            headers = {}
         save_debug = self.bucket.connection.debug
         if self.bucket.connection.debug == 1:
             self.bucket.connection.debug = 0
