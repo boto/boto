a01a5d1a1e88f79ed5db52639d3674d9eb5e45dc
Mitch Garnaat <mitch@garnaat.com>
2012-08-31 08:59:26 -0700

    Fixed a long-standing issue in encoding query-string authentication URL's.  Fixes #916.

--- boto/s3/connection.py
+++ boto/s3/connection.py
@@ -333,7 +333,7 @@ class S3Connection(AWSAuthConnection):
         c_string = boto.utils.canonical_string(method, auth_path, headers,
                                                expires, self.provider)
         b64_hmac = self._auth_handler.sign_string(c_string)
-        encoded_canonical = urllib.quote_plus(b64_hmac)
+        encoded_canonical = urllib.quote(b64_hmac, safe='')
         self.calling_format.build_path_base(bucket, key)
         if query_auth:
             query_part = '?' + self.QueryString % (encoded_canonical, expires,
