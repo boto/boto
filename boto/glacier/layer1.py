boto/boto

commit 05ff4258541684c4c6aa36ddcdd112a6f17c68d2

Fixing unit tests to work again.

boto/glacier/layer1.py
@@ -83,8 +83,8 @@ class Layer1(AWSAuthConnection):
             if response.getheader('Content-Type') == 'application/json':
                 body = json.loads(response.read())
             else:
-                body = {'Response': response.read()}
-            body['RequestId'] = response.getheader('x-amzn-requestid')
+                body = {u'Response': response.read()}
+            body[u'RequestId'] = response.getheader('x-amzn-requestid')
             if response_headers:
                 for header_name, item_name in response_headers:
                     body[item_name] = response.getheader(header_name)
@@ -177,7 +177,7 @@ class Layer1(AWSAuthConnection):
         """
         uri = 'vaults/%s' % vault_name
         return self.make_request('PUT', uri, ok_responses=(201,),
-                                 response_headers=[('Location', 'Location')])
+                                 response_headers=[('uLocation', 'uLocation')])
 
     def delete_vault(self, vault_name):
         """
@@ -346,8 +346,8 @@ class Layer1(AWSAuthConnection):
               archive-retrieval|inventory-retrieval
         """
         uri = 'vaults/%s/jobs' % vault_name
-        response_headers = [('x-amz-job-id', 'JobId'),
-                            ('Location', 'Location')]
+        response_headers = [('x-amz-job-id', u'JobId'),
+                            ('Location', u'Location')]
         json_job_data = json.dumps(job_data)
         return self.make_request('POST', uri, data=json_job_data,
                                  ok_responses=(202,),
@@ -378,9 +378,9 @@ class Layer1(AWSAuthConnection):
         :param range: A tuple of integers specifying the slice (in bytes)
             of the archive you want to receive
         """
-        response_headers = [('x-amz-sha256-tree-hash', 'TreeHash'),
-                            ('Content-Range', 'ContentRange'),
-                            ('Content-Type', 'ContentType')]
+        response_headers = [('x-amz-sha256-tree-hash', u'TreeHash'),
+                            ('Content-Range', u'ContentRange'),
+                            ('Content-Type', u'ContentType')]
         headers = None
         if byte_range:
             headers = {'Range': 'bytes=%d-%d' % (byte_range[0],
@@ -465,8 +465,8 @@ class Layer1(AWSAuthConnection):
             a power of 2.  The minimum allowable part size is 1MB and the
             maximum is 4GB.
         """
-        response_headers = [('x-amz-multipart-upload-id', 'UploadId'),
-                            ('Location', 'Location')]
+        response_headers = [('x-amz-multipart-upload-id', u'UploadId'),
+                            ('Location', u'Location')]
         headers = {'x-amz-part-size': str(part_size)}
         if description:
             headers['x-amz-archive-description'] = description
@@ -503,8 +503,8 @@ class Layer1(AWSAuthConnection):
             archive. This value should be the sum of all the sizes of
             the individual parts that you uploaded.
         """
-        response_headers = [('x-amz-archive-id', 'ArchiveId'),
-                            ('Location', 'Location')]
+        response_headers = [('x-amz-archive-id', u'ArchiveId'),
+                            ('Location', u'Location')]
         headers = {'x-amz-sha256-tree-hash': sha256_treehash,
                    'x-amz-archive-size': str(archive_size)}
         uri = 'vaults/%s/multipart-uploads/%s' % (vault_name, upload_id)
@@ -619,7 +619,7 @@ class Layer1(AWSAuthConnection):
                    'x-amz-sha256-tree-hash': tree_hash,
                    'Content-Range': 'bytes=%d-%d' % (byte_range[0],
                                                      byte_range[1])}
-        response_headers = [('x-amz-sha256-tree-hash', 'TreeHash')]
+        response_headers = [('x-amz-sha256-tree-hash', u'TreeHash')]
         uri = 'vaults/%s/multipart-uploads/%s' % (vault_name, upload_id)
         return self.make_request('PUT', uri, headers=headers,
                                  data=part_data, ok_responses=(204,),

tests/unit/glacier/test_layer1.py
@@ -1,6 +1,8 @@
 from tests.unit import AWSMockServiceTestCase
 from boto.glacier.layer1 import Layer1
 import json
+import copy
+
 
 class GlacierLayer1ConnectionBase(AWSMockServiceTestCase):
     connection_class = Layer1
@@ -11,12 +13,13 @@ class GlacierLayer1ConnectionBase(AWSMockServiceTestCase):
         self.vault_name = u'examplevault'
         self.vault_arn = 'arn:aws:glacier:us-east-1:012345678901:vaults/' + \
                           self.vault_name
-        self.vault_info = {'CreationDate':'2012-03-16T22:22:47.214Z',
-                           'LastInventoryDate': '2012-03-21T22:06:51.218Z',
-                           'NumberOfArchives': 2,
-                           'SizeInBytes': 12334,
-                           'VaultARN': self.vault_arn,
-                           'VaultName': self.vault_name }
+        self.vault_info = {u'CreationDate': u'2012-03-16T22:22:47.214Z',
+                           u'LastInventoryDate': u'2012-03-21T22:06:51.218Z',
+                           u'NumberOfArchives': 2,
+                           u'SizeInBytes': 12334,
+                           u'VaultARN': self.vault_arn,
+                           u'VaultName': self.vault_name}
+
 
 class GlacierVaultsOperations(GlacierLayer1ConnectionBase):
 
@@ -25,16 +28,18 @@ class GlacierVaultsOperations(GlacierLayer1ConnectionBase):
         self.service_connection.create_vault(self.vault_name)
 
     def test_list_vaults(self):
-        content = {'Marker': None, 
-                   'VaultList': [self.vault_info]}
-        self.set_http_response(status_code=200, header=self.json_header, 
+        content = {u'Marker': None,
+                   u'RequestId': None,
+                   u'VaultList': [self.vault_info]}
+        self.set_http_response(status_code=200, header=self.json_header,
                                body=json.dumps(content))
         api_response = self.service_connection.list_vaults()
         self.assertDictEqual(content, api_response)
 
     def test_describe_vaults(self):
-        content = self.vault_info
-        self.set_http_response(status_code=200, header=self.json_header, 
+        content = copy.copy(self.vault_info)
+        content[u'RequestId'] = None
+        self.set_http_response(status_code=200, header=self.json_header,
                                body=json.dumps(content))
         api_response = self.service_connection.describe_vault(self.vault_name)
         self.assertDictEqual(content, api_response)
@@ -42,29 +47,32 @@ class GlacierVaultsOperations(GlacierLayer1ConnectionBase):
     def test_delete_vault(self):
         self.set_http_response(status_code=204)
         self.service_connection.delete_vault(self.vault_name)
-    
+
+
 class GlacierJobOperations(GlacierLayer1ConnectionBase):
 
     def setUp(self):
         super(GlacierJobOperations, self).setUp()
-        self.job_content = 'abc'*1024
+        self.job_content = 'abc' * 1024
 
     def test_initiate_archive_job(self):
-        content = {'Type': 'archive-retrieval',
-                   'ArchiveId': 'AAABZpJrTyioDC_HsOmHae8EZp_uBSJr6cnGOLKp_XJCl-Q',
-                   'Description': 'Test Archive',
-                   'SNSTopic': 'Topic'}
+        content = {u'Type': u'archive-retrieval',
+                   u'ArchiveId': u'AAABZpJrTyioDC_HsOmHae8EZp_uBSJr6cnGOLKp_XJCl-Q',
+                   u'Description': u'Test Archive',
+                   u'SNSTopic': u'Topic',
+                   u'JobId': None,
+                   u'Location': None,
+                   u'RequestId': None}
         self.set_http_response(status_code=202, header=self.json_header,
                                body=json.dumps(content))
         api_response = self.service_connection.initiate_job(self.vault_name,
                                                             self.job_content)
         self.assertDictEqual(content, api_response)
-            
+
     def test_get_archive_output(self):
         header = [('Content-Type', 'application/octet-stream')]
         self.set_http_response(status_code=200, header=header,
                                body=self.job_content)
-        _, data = self.service_connection.get_job_output(self.vault_name,
+        response = self.service_connection.get_job_output(self.vault_name,
                                                          'example-job-id')
-        self.assertEqual(self.job_content, data)
-    
+        self.assertEqual(self.job_content, response['Response'])

commit 1f282e06288cc479009f71e51545d00013b00eda

Fix small typo introduced in 05ff4258

boto/glacier/layer1.py
@@ -177,7 +177,7 @@ class Layer1(AWSAuthConnection):
         """
         uri = 'vaults/%s' % vault_name
         return self.make_request('PUT', uri, ok_responses=(201,),
-                                 response_headers=[('uLocation', 'uLocation')])
+                                 response_headers=[(u'Location', u'Location')])
 
     def delete_vault(self, vault_name):
         """