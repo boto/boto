788ad932dd030c3468f4ec111277b75c57457b6a
James Saryerwinnie <js@jamesls.com>
2012-09-06 14:37:27 -0700

    Pep8 cleanup to glacer l2 unit tests
    Also switched the assert statements to use
    assertEqual for better error diagnostics.

--- tests/unit/glacier/test_layer2.py
+++ tests/unit/glacier/test_layer2.py
@@ -21,11 +21,7 @@
 # IN THE SOFTWARE.
 #
 
-try:
-    import unittest2 as unittest
-except ImportError:
-    import unittest
-import httplib
+from tests.unit import unittest
 
 from mock import Mock
 
@@ -46,30 +42,36 @@ FIXTURE_VAULT = {
 
 FIXTURE_ARCHIVE_JOB = {
   "Action": "ArchiveRetrieval",
-  "ArchiveId": "NkbByEejwEggmBz2fTHgJrg0XBoDfjP4q6iu87-TjhqG6eGoOY9Z8i1_AUyUsuhPAdTqLHy8pTl5nfCFJmDl2yEZONi5L26Omw12vcs01MNGntHEQL8MBfGlqrEXAMPLEArchiveId",
+  "ArchiveId": ("NkbByEejwEggmBz2fTHgJrg0XBoDfjP4q6iu87-TjhqG6eGoOY9Z8i1_AUyUs"
+                "uhPAdTqLHy8pTl5nfCFJmDl2yEZONi5L26Omw12vcs01MNGntHEQL8MBfGlqr"
+                "EXAMPLEArchiveId"),
   "ArchiveSizeInBytes": 16777216,
   "Completed": False,
   "CreationDate": "2012-05-15T17:21:39.339Z",
   "CompletionDate": "2012-05-15T17:21:43.561Z",
   "InventorySizeInBytes": None,
   "JobDescription": "My ArchiveRetrieval Job",
-  "JobId": "HkF9p6o7yjhFx-K3CGl6fuSm6VzW9T7esGQfco8nUXVYwS0jlb5gq1JZ55yHgt5vP54ZShjoQzQVVh7vEXAMPLEjobID",
-  "SHA256TreeHash": "beb0fe31a1c7ca8c6c04d574ea906e3f97b31fdca7571defb5b44dca89b5af60",
+  "JobId": ("HkF9p6o7yjhFx-K3CGl6fuSm6VzW9T7esGQfco8nUXVYwS0jlb5gq1JZ55yHgt5v"
+            "P54ZShjoQzQVVh7vEXAMPLEjobID"),
+  "SHA256TreeHash": ("beb0fe31a1c7ca8c6c04d574ea906e3f97b31fdca7571defb5b44dc"
+                     "a89b5af60"),
   "SNSTopic": "arn:aws:sns:us-east-1:012345678901:mytopic",
   "StatusCode": "InProgress",
   "StatusMessage": "Operation in progress.",
   "VaultARN": "arn:aws:glacier:us-east-1:012345678901:vaults/examplevault"
-} 
+}
+
 
 class GlacierLayer2Base(unittest.TestCase):
     def setUp(self):
         self.mock_layer1 = Mock(spec=Layer1)
 
+
 class TestGlacierLayer2Connection(GlacierLayer2Base):
     def setUp(self):
         GlacierLayer2Base.setUp(self)
         self.layer2 = Layer2(layer1=self.mock_layer1)
-                           
+
     def test_create_vault(self):
         self.mock_layer1.describe_vault.return_value = FIXTURE_VAULT
         self.layer2.create_vault("My Vault")
@@ -78,15 +80,16 @@ class TestGlacierLayer2Connection(GlacierLayer2Base):
     def test_get_vault(self):
         self.mock_layer1.describe_vault.return_value = FIXTURE_VAULT
         vault = self.layer2.get_vault("examplevault")
-        assert vault.layer1 == self.mock_layer1
-        assert vault.name == "examplevault"
-        assert vault.size == 78088912
-        assert vault.number_of_archives == 192
+        self.assertEqual(vault.layer1, self.mock_layer1)
+        self.assertEqual(vault.name, "examplevault")
+        self.assertEqual(vault.size, 78088912)
+        self.assertEqual(vault.number_of_archives, 192)
 
     def list_vaults(self):
         self.mock_layer1.list_vaults.return_value = [FIXTURE_VAULT]
         vaults = self.layer2.list_vaults()
-        assert vaults[0].name == "examplevault"
+        self.assertEqual(vaults[0].name, "examplevault")
+
 
 class TestVault(GlacierLayer2Base):
     def setUp(self):
@@ -96,27 +99,39 @@ class TestVault(GlacierLayer2Base):
     # TODO: Tests for the other methods of uploading
 
     def test_create_archive_writer(self):
-        self.mock_layer1.initiate_multipart_upload.return_value = {"UploadId": "UPLOADID"}
+        self.mock_layer1.initiate_multipart_upload.return_value = {
+            "UploadId": "UPLOADID"}
         writer = self.vault.create_archive_writer(description="stuff")
-        self.mock_layer1.initiate_multipart_upload.assert_called_with("examplevault", self.vault.DefaultPartSize, "stuff")
-        assert writer.vault == self.vault
-        assert writer.upload_id == "UPLOADID"
+        self.mock_layer1.initiate_multipart_upload.assert_called_with(
+            "examplevault", self.vault.DefaultPartSize, "stuff")
+        self.assertEqual(writer.vault, self.vault)
+        self.assertEqual(writer.upload_id, "UPLOADID")
 
     def test_delete_vault(self):
         self.vault.delete_archive("archive")
-        self.mock_layer1.delete_archive.assert_called_with("examplevault","archive")
-        
+        self.mock_layer1.delete_archive.assert_called_with("examplevault",
+                                                           "archive")
+
     def test_get_job(self):
         self.mock_layer1.describe_job.return_value = FIXTURE_ARCHIVE_JOB
-        job = self.vault.get_job("NkbByEejwEggmBz2fTHgJrg0XBoDfjP4q6iu87-TjhqG6eGoOY9Z8i1_AUyUsuhPAdTqLHy8pTl5nfCFJmDl2yEZONi5L26Omw12vcs01MNGntHEQL8MBfGlqrEXAMPLEArchiveId")
-        assert job.action == "ArchiveRetrieval"
-        
+        job = self.vault.get_job(
+            "NkbByEejwEggmBz2fTHgJrg0XBoDfjP4q6iu87-TjhqG6eGoOY9Z8i1_AUyUsuhPA"
+            "dTqLHy8pTl5nfCFJmDl2yEZONi5L26Omw12vcs01MNGntHEQL8MBfGlqrEXAMPLEA"
+            "rchiveId")
+        self.assertEqual(job.action, "ArchiveRetrieval")
+
     def test_list_jobs(self):
-        self.mock_layer1.list_jobs.return_value = {"JobList": [FIXTURE_ARCHIVE_JOB]}
+        self.mock_layer1.list_jobs.return_value = {
+            "JobList": [FIXTURE_ARCHIVE_JOB]}
         jobs = self.vault.list_jobs(False, "InProgress")
-        self.mock_layer1.list_jobs.assert_called_with("examplevault", False, "InProgress")
-        assert jobs[0].archive_id == "NkbByEejwEggmBz2fTHgJrg0XBoDfjP4q6iu87-TjhqG6eGoOY9Z8i1_AUyUsuhPAdTqLHy8pTl5nfCFJmDl2yEZONi5L26Omw12vcs01MNGntHEQL8MBfGlqrEXAMPLEArchiveId"
-        
+        self.mock_layer1.list_jobs.assert_called_with("examplevault",
+                                                      False, "InProgress")
+        self.assertEqual(jobs[0].archive_id,
+                         "NkbByEejwEggmBz2fTHgJrg0XBoDfjP4q6iu87-TjhqG6eGoOY9Z"
+                         "8i1_AUyUsuhPAdTqLHy8pTl5nfCFJmDl2yEZONi5L26Omw12vcs0"
+                         "1MNGntHEQL8MBfGlqrEXAMPLEArchiveId")
+
+
 class TestJob(GlacierLayer2Base):
     def setUp(self):
         GlacierLayer2Base.setUp(self)
@@ -126,5 +141,7 @@ class TestJob(GlacierLayer2Base):
     def test_get_job_output(self):
         self.mock_layer1.get_job_output.return_value = "TEST_OUTPUT"
         self.job.get_output((0,100))
-        self.mock_layer1.get_job_output.assert_called_with("examplevault", "HkF9p6o7yjhFx-K3CGl6fuSm6VzW9T7esGQfco8nUXVYwS0jlb5gq1JZ55yHgt5vP54ZShjoQzQVVh7vEXAMPLEjobID", (0,100))
-        
+        self.mock_layer1.get_job_output.assert_called_with(
+            "examplevault",
+            "HkF9p6o7yjhFx-K3CGl6fuSm6VzW9T7esGQfco8nUXVYwS0jlb5gq1JZ55yHgt5vP"
+            "54ZShjoQzQVVh7vEXAMPLEjobID", (0,100))
