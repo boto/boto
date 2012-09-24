3955cf336efe92444621eef2619ca84eb1c2d81a
George Sequeira <george.m.sequeira@gmail.com>
2012-09-24 16:22:24 -0400

    fixed an import thing for qualfix

--- boto/mturk/connection.py
+++ boto/mturk/connection.py
@@ -30,6 +30,7 @@ import boto.mturk.notification
 from boto.connection import AWSQueryConnection
 from boto.exception import EC2ResponseError
 from boto.resultset import ResultSet
+from boto.mturk.question import QuestionForm, ExternalQuestion
 
 class MTurkRequestError(EC2ResponseError):
     "Error for MTurk Requests"
