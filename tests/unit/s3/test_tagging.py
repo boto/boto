028085f5bf0987fdac83860eca2282567590d073
James Saryerwinnie <js@jamesls.com>
2012-09-28 10:16:47 -0700

    Add a unittest for s3 tag equality (#1008)

--- tests/unit/s3/test_tagging.py
+++ tests/unit/s3/test_tagging.py
@@ -2,6 +2,7 @@ from tests.unit import AWSMockServiceTestCase
 
 from boto.s3.connection import S3Connection
 from boto.s3.bucket import Bucket
+from boto.s3.tagging import Tag
 
 
 class TestS3Tagging(AWSMockServiceTestCase):
@@ -35,3 +36,12 @@ class TestS3Tagging(AWSMockServiceTestCase):
         self.assertEqual(api_response[0][0].value, 'Project One')
         self.assertEqual(api_response[0][1].key, 'User')
         self.assertEqual(api_response[0][1].value, 'jsmith')
+
+    def test_tag_equality(self):
+        t1 = Tag('foo', 'bar')
+        t2 = Tag('foo', 'bar')
+        t3 = Tag('foo', 'baz')
+        t4 = Tag('baz', 'bar')
+        self.assertEqual(t1, t2)
+        self.assertNotEqual(t1, t3)
+        self.assertNotEqual(t1, t4)
