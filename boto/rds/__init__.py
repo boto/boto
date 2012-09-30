86c84f295fe72a00e284190e1be0f880117f1272
Mark Nunnikhoven <me@markn.ca>
2012-09-29 20:26:08 -0400

    Updated RDS API version to 2012-09-17

--- boto/rds/__init__.py
+++ boto/rds/__init__.py
@@ -81,7 +81,7 @@ class RDSConnection(AWSQueryConnection):
 
     DefaultRegionName = 'us-east-1'
     DefaultRegionEndpoint = 'rds.us-east-1.amazonaws.com'
-    APIVersion = '2011-04-01'
+    APIVersion = '2012-09-17'
 
     def __init__(self, aws_access_key_id=None, aws_secret_access_key=None,
                  is_secure=True, port=None, proxy=None, proxy_port=None,
