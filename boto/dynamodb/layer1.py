6c6e291eca2cf63afd9011f81e0dba3df7145fd7
David Swift <dswift@pccowboy.com>
2012-10-03 18:39:02 -0700

    oops - missed one

--- boto/dynamodb/layer1.py
+++ boto/dynamodb/layer1.py
@@ -95,7 +95,7 @@ class Layer1(AWSAuthConnection):
                                    aws_access_key_id,
                                    aws_secret_access_key,
                                    is_secure, port, proxy, proxy_port,
-                                   debug=debug, security_token,
+                                   debug, security_token,
                                    validate_certs)
         self.throughput_exceeded_events = 0
 
