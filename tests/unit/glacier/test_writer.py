612d6fdae133e234ef5f0e8890365e7f6cd24deb
Robie Basak <robie@justgohome.co.uk>
2012-10-11 21:14:00 +0100

    Factor out check_mock_vault_calls

--- tests/unit/glacier/test_writer.py
+++ tests/unit/glacier/test_writer.py
@@ -8,6 +8,7 @@ from mock import (
     patch,
     sentinel,
 )
+from nose.tools import assert_equal
 
 from boto.glacier.layer1 import Layer1
 from boto.glacier.vault import Vault
@@ -77,6 +78,18 @@ def calculate_mock_vault_calls(data, part_size, chunk_size):
     return upload_part_calls, data_tree_hashes
 
 
+def check_mock_vault_calls(vault, upload_part_calls, data_tree_hashes,
+                           data_len):
+    vault.layer1.upload_part.assert_has_calls(
+        upload_part_calls, any_order=True)
+    assert_equal(
+        len(upload_part_calls), vault.layer1.upload_part.call_count)
+
+    data_tree_hash = bytes_to_hex(tree_hash(data_tree_hashes))
+    vault.layer1.complete_multipart_upload.assert_called_once_with(
+        sentinel.vault_name, sentinel.upload_id, data_tree_hash, data_len)
+
+
 class TestWriter(unittest.TestCase):
     def setUp(self):
         super(TestWriter, self).setUp()
@@ -95,15 +108,8 @@ class TestWriter(unittest.TestCase):
         data = ''.join(write_list)
         upload_part_calls, data_tree_hashes = calculate_mock_vault_calls(
             data, self.part_size, self.chunk_size)
-
-        self.vault.layer1.upload_part.assert_has_calls(
-            upload_part_calls, any_order=True)
-        self.assertEquals(
-            len(upload_part_calls), self.vault.layer1.upload_part.call_count)
-
-        data_tree_hash = bytes_to_hex(tree_hash(data_tree_hashes))
-        self.vault.layer1.complete_multipart_upload.assert_called_once_with(
-            sentinel.vault_name, sentinel.upload_id, data_tree_hash, len(data))
+        check_mock_vault_calls(
+            self.vault, upload_part_calls, data_tree_hashes, len(data))
 
     def test_single_byte_write(self):
         self.check_write(['1'])
