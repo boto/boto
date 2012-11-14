boto/boto

commit e2a38bd985c13160ff581a627424f6f1ea535120

Add support for the Reserved Instance Marketplace

boto/ec2/reservedinstance.py
@@ -14,18 +14,22 @@
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
 # OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABIL-
 # ITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
-# SHALL THE AUTHOR BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+# SHALL THE AUTHOR BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
 # WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 # IN THE SOFTWARE.
 
+from boto.resultset import ResultSet
 from boto.ec2.ec2object import EC2Object
 
+
 class ReservedInstancesOffering(EC2Object):
-    
+
     def __init__(self, connection=None, id=None, instance_type=None,
                  availability_zone=None, duration=None, fixed_price=None,
-                 usage_price=None, description=None):
+                 usage_price=None, description=None, instance_tenancy=None,
+                 currency_code=None, offering_type=None,
+                 recurring_charges=None, pricing_details=None):
         EC2Object.__init__(self, connection)
         self.id = id
         self.instance_type = instance_type
@@ -34,11 +38,22 @@ class ReservedInstancesOffering(EC2Object):
         self.fixed_price = fixed_price
         self.usage_price = usage_price
         self.description = description
+        self.instance_tenancy = instance_tenancy
+        self.currency_code = currency_code
+        self.offering_type = offering_type
+        self.recurring_charges = recurring_charges
+        self.pricing_details = pricing_details
 
     def __repr__(self):
         return 'ReservedInstanceOffering:%s' % self.id
 
     def startElement(self, name, attrs, connection):
+        if name == 'recurringCharges':
+            self.recurring_charges = ResultSet([('item', RecurringCharge)])
+            return self.recurring_charges
+        elif name == 'pricingDetailsSet':
+            self.pricing_details = ResultSet([('item', PricingDetail)])
+            return self.pricing_details
         return None
 
     def endElement(self, name, value, connection):
@@ -49,15 +64,21 @@ class ReservedInstancesOffering(EC2Object):
         elif name == 'availabilityZone':
             self.availability_zone = value
         elif name == 'duration':
-            self.duration = value
+            self.duration = int(value)
         elif name == 'fixedPrice':
             self.fixed_price = value
         elif name == 'usagePrice':
             self.usage_price = value
         elif name == 'productDescription':
             self.description = value
-        else:
-            setattr(self, name, value)
+        elif name == 'instanceTenancy':
+            self.instance_tenancy = value
+        elif name == 'currencyCode':
+            self.currency_code = value
+        elif name == 'offeringType':
+            self.offering_type = value
+        elif name == 'marketplace':
+            self.marketplace = True if value == 'true' else False
 
     def describe(self):
         print 'ID=%s' % self.id
@@ -70,6 +91,31 @@ class ReservedInstancesOffering(EC2Object):
 
     def purchase(self, instance_count=1):
         return self.connection.purchase_reserved_instance_offering(self.id, instance_count)
+
+
+class RecurringCharge(object):
+    def __init__(self, connection=None, frequency=None, amount=None):
+        self.frequency = frequency
+        self.amount = amount
+
+    def startElement(self, name, attrs, connection):
+        return None
+
+    def endElement(self, name, value, connection):
+        setattr(self, name, value)
+
+
+class PricingDetail(object):
+    def __init__(self, connection=None, price=None, count=None):
+        self.price = price
+        self.count = count
+
+    def startElement(self, name, attrs, connection):
+        return None
+
+    def endElement(self, name, value, connection):
+        setattr(self, name, value)
+
 
 class ReservedInstance(ReservedInstancesOffering):
 
@@ -95,3 +141,84 @@ class ReservedInstance(ReservedInstancesOffering):
             self.state = value
         else:
             ReservedInstancesOffering.endElement(self, name, value, connection)
+
+
+class ReservedInstanceListing(EC2Object):
+    def __init__(self, connection=None, listing_id=None, id=None,
+                 create_date=None, update_date=None,
+                 status=None, status_message=None, client_token=None):
+        self.connection = connection
+        self.listing_id = listing_id
+        self.id = id
+        self.create_date = create_date
+        self.update_date = update_date
+        self.status = status
+        self.status_message = status_message
+        self.client_token = client_token
+
+    def startElement(self, name, attrs, connection):
+        if name == 'instanceCounts':
+            self.instance_counts = ResultSet([('item', InstanceCount)])
+            return self.instance_counts
+        elif name == 'priceSchedules':
+            self.price_schedules = ResultSet([('item', PriceSchedule)])
+            return self.price_schedules
+        return None
+
+    def endElement(self, name, value, connection):
+        if name == 'reservedInstancesListingId':
+            self.listing_id = value
+        elif name == 'reservedInstancesId':
+            self.id = value
+        elif name == 'createDate':
+            self.create_date = value
+        elif name == 'updateDate':
+            self.update_date = value
+        elif name == 'status':
+            self.status = value
+        elif name == 'statusMessage':
+            self.status_message = value
+        else:
+            setattr(self, name, value)
+
+
+class InstanceCount(object):
+    def __init__(self, connection=None, state=None, instance_count=None):
+        self.state = state
+        self.instance_count = instance_count
+
+    def startElement(self, name, attrs, connection):
+        return None
+
+    def endElement(self, name, value, connection):
+        if name == 'state':
+            self.state = value
+        elif name == 'instanceCount':
+            self.instance_count = int(value)
+        else:
+            setattr(self, name, value)
+
+
+class PriceSchedule(object):
+    def __init__(self, connection=None, term=None, price=None,
+                 currency_code=None, active=None):
+        self.connection = connection
+        self.term = term
+        self.price = price
+        self.currency_code = currency_code
+        self.active = active
+
+    def startElement(self, name, attrs, connection):
+        return None
+
+    def endElement(self, name, value, connection):
+        if name == 'term':
+            self.term = int(value)
+        elif name == 'price':
+            self.price = value
+        elif name == 'currencyCode':
+            self.currency_code = value
+        elif name == 'active':
+            self.active = True if value == 'true' else False
+        else:
+            setattr(self, name, value)

boto/ec2/connection.py
@@ -47,6 +47,7 @@ from boto.ec2.regioninfo import RegionInfo
 from boto.ec2.instanceinfo import InstanceInfo
 from boto.ec2.reservedinstance import ReservedInstancesOffering
 from boto.ec2.reservedinstance import ReservedInstance
+from boto.ec2.reservedinstance import ReservedInstanceListing
 from boto.ec2.spotinstancerequest import SpotInstanceRequest
 from boto.ec2.spotpricehistory import SpotPriceHistory
 from boto.ec2.spotdatafeedsubscription import SpotDatafeedSubscription
@@ -63,7 +64,7 @@ from boto.exception import EC2ResponseError
 
 class EC2Connection(AWSQueryConnection):
 
-    APIVersion = boto.config.get('Boto', 'ec2_version', '2012-07-20')
+    APIVersion = boto.config.get('Boto', 'ec2_version', '2012-08-15')
     DefaultRegionName = boto.config.get('Boto', 'ec2_region_name', 'us-east-1')
     DefaultRegionEndpoint = boto.config.get('Boto', 'ec2_region_endpoint',
                                             'ec2.us-east-1.amazonaws.com')
@@ -2661,17 +2662,26 @@ class EC2Connection(AWSQueryConnection):
     # Reservation methods
     #
 
-    def get_all_reserved_instances_offerings(self, reserved_instances_id=None,
+    def get_all_reserved_instances_offerings(self,
+                                             reserved_instances_offering_ids=None,
                                              instance_type=None,
                                              availability_zone=None,
                                              product_description=None,
-                                             filters=None):
+                                             filters=None,
+                                             instance_tenancy=None,
+                                             offering_type=None,
+                                             include_marketplace=None,
+                                             min_duration=None,
+                                             max_duration=None,
+                                             max_instance_count=None,
+                                             next_token=None,
+                                             max_results=None):
         """
         Describes Reserved Instance offerings that are available for purchase.
 
-        :type reserved_instances_id: str
-        :param reserved_instances_id: Displays Reserved Instances with the
-                                      specified offering IDs.
+        :type reserved_instances_offering_ids: list
+        :param reserved_instances_id: One or more Reserved Instances
+            offering IDs.
 
         :type instance_type: str
         :param instance_type: Displays Reserved Instances of the specified
@@ -2695,12 +2705,49 @@ class EC2Connection(AWSQueryConnection):
                         being performed.  Check the EC2 API guide
                         for details.
 
+        :type instance_tenancy: string
+        :param instance_tenancy: The tenancy of the Reserved Instance offering.
+            A Reserved Instance with tenancy of dedicated will run on
+            single-tenant hardware and can only be launched within a VPC.
+
+        :type offering_type: string
+        :param offering_type: The Reserved Instance offering type.
+            Valid Values:
+                * Heavy Utilization
+                * Medium Utilization
+                * Light Utilization
+
+        :type include_marketplace: bool
+        :param include_marketplace: Include Marketplace offerings in the
+            response.
+
+        :type min_duration: int :param min_duration: Minimum duration (in
+            seconds) to filter when searching for offerings.
+
+        :type max_duration: int
+        :param max_duration: Maximum duration (in seconds) to filter when
+            searching for offerings.
+
+        :type max_instance_count: int
+        :param max_instance_count: Maximum number of instances to filter when
+            searching for offerings.
+
+        :type next_token: string
+        :param next_token: Token to use when requesting the next paginated set
+            of offerings.
+
+        :type max_results: int
+        :param max_results: Maximum number of offerings to return per call.
+
         :rtype: list
-        :return: A list of :class:`boto.ec2.reservedinstance.ReservedInstancesOffering`
+        :return: A list of
+            :class:`boto.ec2.reservedinstance.ReservedInstancesOffering`.
+
         """
         params = {}
-        if reserved_instances_id:
-            params['ReservedInstancesId'] = reserved_instances_id
+        if reserved_instances_offering_ids is not None:
+            self.build_list_params(params, reserved_instances_offering_ids,
+                                   'ReservedInstancesOfferingId')
         if instance_type:
             params['InstanceType'] = instance_type
         if availability_zone:
@@ -2709,6 +2756,25 @@ class EC2Connection(AWSQueryConnection):
             params['ProductDescription'] = product_description
         if filters:
             self.build_filter_params(params, filters)
+        if instance_tenancy is not None:
+            params['InstanceTenancy'] = instance_tenancy
+        if offering_type is not None:
+            params['OfferingType'] = offering_type
+        if include_marketplace is not None:
+            if include_marketplace:
+                params['IncludeMarketplace'] = 'true'
+            else:
+                params['IncludeMarketplace'] = 'false'
+        if min_duration is not None:
+            params['MinDuration'] = str(min_duration)
+        if max_duration is not None:
+            params['MaxDuration'] = str(max_duration)
+        if max_instance_count is not None:
+            params['MaxInstanceCount'] = str(max_instance_count)
+        if next_token is not None:
+            params['NextToken'] = next_token
+        if max_results is not None:
+            params['MaxResults'] = str(max_results)
 
         return self.get_list('DescribeReservedInstancesOfferings',
                              params, [('item', ReservedInstancesOffering)],
@@ -2748,7 +2814,7 @@ class EC2Connection(AWSQueryConnection):
 
     def purchase_reserved_instance_offering(self,
                                             reserved_instances_offering_id,
-                                            instance_count=1):
+                                            instance_count=1, limit_price=None):
         """
         Purchase a Reserved Instance for use with your account.
         ** CAUTION **
@@ -2763,14 +2829,106 @@ class EC2Connection(AWSQueryConnection):
         :param instance_count: The number of Reserved Instances to purchase.
                                Default value is 1.
 
+        :type limit_price: tuple
+        :param instance_count: Limit the price on the total order.
+                               Must be a tuple of (amount, currency_code), for example:
+                                   (100.0, 'USD').
+
         :rtype: :class:`boto.ec2.reservedinstance.ReservedInstance`
         :return: The newly created Reserved Instance
         """
         params = {
             'ReservedInstancesOfferingId': reserved_instances_offering_id,
             'InstanceCount': instance_count}
+        if limit_price is not None:
+            params['LimitPrice.Amount'] = str(limit_price[0])
+            params['LimitPrice.CurrencyCode'] = str(limit_price[1])
         return self.get_object('PurchaseReservedInstancesOffering', params,
                                ReservedInstance, verb='POST')
+
+    def create_reserved_instances_listing(self, reserved_instances_id, instance_count,
+                                          price_schedules, client_token):
+        """Creates a new listing for Reserved Instances.
+
+        Creates a new listing for Amazon EC2 Reserved Instances that will be
+        sold in the Reserved Instance Marketplace. You can submit one Reserved
+        Instance listing at a time.
+
+        The Reserved Instance Marketplace matches sellers who want to resell
+        Reserved Instance capacity that they no longer need with buyers who
+        want to purchase additional capacity. Reserved Instances bought and
+        sold through the Reserved Instance Marketplace work like any other
+        Reserved Instances.
+
+        If you want to sell your Reserved Instances, you must first register as
+        a Seller in the Reserved Instance Marketplace. After completing the
+        registration process, you can create a Reserved Instance Marketplace
+        listing of some or all of your Reserved Instances, and specify the
+        upfront price you want to receive for them. Your Reserved Instance
+        listings then become available for purchase.
+
+        :type reserved_instances_id: string
+        :param reserved_instances_id: The ID of the Reserved Instance that
+            will be listed.
+
+        :type instance_count: int
+        :param instance_count: The number of instances that are a part of a
+            Reserved Instance account that will be listed in the Reserved
+            Instance Marketplace. This number should be less than or equal to
+            the instance count associated with the Reserved Instance ID
+            specified in this call.
+
+        :type price_schedules: List of tuples
+        :param price_schedules: A list specifying the price of the Reserved
+            Instance for each month remaining in the Reserved Instance term.
+            Each tuple contains two elements, the price and the term.  For
+            example, for an instance that 11 months remaining in its term,
+            we can have a price schedule with an upfront price of $2.50.
+            At 8 months remaining we can drop the price down to $2.00.
+            This would be expressed as::
+
+                price_schedules=[('2.50', 11), ('2.00', 8)]
+
+        :type client_token: string
+        :param client_token: Unique, case-sensitive identifier you provide
+            to ensure idempotency of the request.  Maximum 64 ASCII characters.
+
+        :rtype: list
+        :return: A list of
+            :class:`boto.ec2.reservedinstance.ReservedInstanceListing`
+
+        """
+        params = {
+            'ReservedInstancesId': reserved_instances_id,
+            'InstanceCount': str(instance_count),
+            'ClientToken': client_token,
+        }
+        for i, schedule in enumerate(price_schedules):
+            price, term = schedule
+            params['PriceSchedules.%s.Price' % i] = str(price)
+            params['PriceSchedules.%s.Term' % i] = str(term)
+        return self.get_list('CreateReservedInstancesListing',
+                             params, [('item', ReservedInstanceListing)], verb='POST')
+
+    def cancel_reserved_instances_listing(
+            self, reserved_instances_listing_ids=None):
+        """Cancels the specified Reserved Instance listing.
+
+        :type reserved_instances_listing_ids: List of strings
+        :param reserved_instances_listing_ids: The ID of the
+            Reserved Instance listing to be cancelled.
+
+        :rtype: list
+        :return: A list of
+            :class:`boto.ec2.reservedinstance.ReservedInstanceListing`
+
+        """
+        params = {}
+        if reserved_instances_listing_ids is not None:
+            self.build_list_params(params, reserved_instances_listing_ids,
+                                   'ReservedInstancesListingId')
+        return self.get_list('CancelReservedInstancesListing',
+                             params, [('item', ReservedInstanceListing)], verb='POST')
 
     #
     # Monitoring

commit 68b38665fee804c85de8e3ac47ebfb4fad238d95

Fix reserved instance missing 'start' attribute.
Bug introduced with e2a38bd985c13160.  start field was lost with
the removal of the blanket setattr in the parent object
(ReservedInstancesOffering)

boto/ec2/reservedinstance.py
@@ -128,6 +128,7 @@ class ReservedInstance(ReservedInstancesOffering):
                                            usage_price, description)
         self.instance_count = instance_count
         self.state = state
+        self.start = None
 
     def __repr__(self):
         return 'ReservedInstance:%s' % self.id
@@ -139,6 +140,8 @@ class ReservedInstance(ReservedInstancesOffering):
             self.instance_count = int(value)
         elif name == 'state':
             self.state = value
+        elif name == 'start':
+            self.start = value
         else:
             ReservedInstancesOffering.endElement(self, name, value, connection)
 