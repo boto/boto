boto/boto

commit f3bcdb404eb6dbc338021f46543eb639f6340b66

Fixing the fix for #283.

boto/ec2/elb/__init__.py
@@ -1,4 +1,6 @@
-# Copyright (c) 2006-2011 Mitch Garnaat http://garnaat.org/
+# Copyright (c) 2006-2012 Mitch Garnaat http://garnaat.org/
+# Copyright (c) 2012 Amazon.com, Inc. or its affiliates.
+# All Rights Reserved
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the

boto/ec2/elb/listelement.py
@@ -1,4 +1,6 @@
-# Copyright (c) 2006-2009 Mitch Garnaat http://garnaat.org/
+# Copyright (c) 2006-2012 Mitch Garnaat http://garnaat.org/
+# Copyright (c) 2012 Amazon.com, Inc. or its affiliates.
+# All Rights Reserved
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the
@@ -25,6 +27,10 @@ class ListElement(list):
     A :py:class:`list` subclass that has some additional methods
     for interacting with Amazon's XML API.
     """
+
+    def __init__(self, parent=None):
+        self.parent = parent
+        list.__init__(self)
 
     def startElement(self, name, attrs, connection):
         pass

boto/ec2/elb/healthcheck.py
@@ -1,4 +1,6 @@
-# Copyright (c) 2006-2009 Mitch Garnaat http://garnaat.org/
+# Copyright (c) 2006-2012 Mitch Garnaat http://garnaat.org/
+# Copyright (c) 2012 Amazon.com, Inc. or its affiliates.
+# All Rights Reserved
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the
@@ -18,6 +20,7 @@
 # WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 # IN THE SOFTWARE.
+
 
 class HealthCheck(object):
     """
@@ -77,11 +80,10 @@ class HealthCheck(object):
         if not self.access_point:
             return
 
-        new_hc = self.connection.configure_health_check(self.access_point, self)
+        new_hc = self.connection.configure_health_check(self.access_point,
+                                                        self)
         self.interval = new_hc.interval
         self.target = new_hc.target
         self.healthy_threshold = new_hc.healthy_threshold
         self.unhealthy_threshold = new_hc.unhealthy_threshold
         self.timeout = new_hc.timeout
-
-

boto/ec2/elb/instancestate.py
@@ -60,6 +60,3 @@ class InstanceState(object):
             self.reason_code = value
         else:
             setattr(self, name, value)
-
-
-

boto/ec2/elb/listener.py
@@ -1,4 +1,6 @@
-# Copyright (c) 2006-2009 Mitch Garnaat http://garnaat.org/
+# Copyright (c) 2006-2012 Mitch Garnaat http://garnaat.org/
+# Copyright (c) 2012 Amazon.com, Inc. or its affiliates.
+# All Rights Reserved
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the
@@ -20,6 +22,7 @@
 # IN THE SOFTWARE.
 
 from boto.ec2.elb.listelement import ListElement
+
 
 class Listener(object):
     """
@@ -70,7 +73,3 @@ class Listener(object):
         if key == 2:
             return self.protocol
         raise KeyError
-
-
-
-

commit 2604f7481d28b563ea3f585d1d205f1da9c687a0

Fixing a regression introduced with f3bcdb4.

boto/connection.py
@@ -976,7 +976,7 @@ class AWSQueryConnection(AWSAuthConnection):
     def build_list_params(self, params, items, label):
         # Convert comma- or whitespace-delimited string to array
         if isinstance(items, basestring):
-            items = re.findall('[^,\s]+', items)
+            items = [items]
         for i in range(1, len(items) + 1):
             params['%s.%d' % (label, i)] = items[i - 1]
 

boto/ec2/elb/__init__.py
@@ -27,7 +27,7 @@ load balancing service from AWS.
 """
 from boto.connection import AWSQueryConnection
 from boto.ec2.instanceinfo import InstanceInfo
-from boto.ec2.elb.loadbalancer import LoadBalancer
+from boto.ec2.elb.loadbalancer import LoadBalancer, LoadBalancerZones
 from boto.ec2.elb.instancestate import InstanceState
 from boto.ec2.elb.healthcheck import HealthCheck
 from boto.ec2.elb.listelement import ListElement
@@ -293,8 +293,9 @@ class ELBConnection(AWSQueryConnection):
         params = {'LoadBalancerName': load_balancer_name}
         self.build_list_params(params, zones_to_add,
                                'AvailabilityZones.member.%d')
-        return self.get_object('EnableAvailabilityZonesForLoadBalancer',
-                               params, ListElement)
+        obj = self.get_object('EnableAvailabilityZonesForLoadBalancer',
+                               params, LoadBalancerZones)
+        return obj.zones
 
     def disable_availability_zones(self, load_balancer_name, zones_to_remove):
         """
@@ -317,8 +318,9 @@ class ELBConnection(AWSQueryConnection):
         params = {'LoadBalancerName': load_balancer_name}
         self.build_list_params(params, zones_to_remove,
                                'AvailabilityZones.member.%d')
-        return self.get_object('DisableAvailabilityZonesForLoadBalancer',
-                               params, ListElement)
+        obj = self.get_object('DisableAvailabilityZonesForLoadBalancer',
+                               params, LoadBalancerZones)
+        return obj.zones
 
     def register_instances(self, load_balancer_name, instances):
         """

boto/ec2/elb/listelement.py
@@ -28,10 +28,6 @@ class ListElement(list):
     for interacting with Amazon's XML API.
     """
 
-    def __init__(self, parent=None):
-        self.parent = parent
-        list.__init__(self)
-
     def startElement(self, name, attrs, connection):
         pass
 

boto/ec2/elb/loadbalancer.py
@@ -29,6 +29,20 @@ from boto.ec2.instanceinfo import InstanceInfo
 from boto.resultset import ResultSet
 
 
+class LoadBalancerZones(object):
+    """
+    Used to collect the zones for a Load Balancer when enable_zones
+    or disable_zones are called.
+    """
+    def __init__(self, connection=None):
+        self.connection = connection
+        self.zones = ListElement()
+
+    def startElement(self, name, attrs, connection):
+        if name == 'AvailabilityZones':
+            return self.zones
+
+
 class LoadBalancer(object):
     """
     Represents an EC2 Load Balancer.