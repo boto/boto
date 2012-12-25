9c6a04ad61d955ab49626bb271f04d5839e7bff7
Abhinav Upadhyay <er.abhinav.upadhyay@gmail.com>
2012-12-25 23:59:30 +0530

    Conver tabs to spaces and fix indentation errors.

--- tests/mturk/_init_environment.py
+++ tests/mturk/_init_environment.py
@@ -11,18 +11,18 @@ SetHostMTurkConnection = None
 def config_environment():
     global SetHostMTurkConnection
     try:
-            local = os.path.join(os.path.dirname(__file__), 'local.py')
-            execfile(local)
+        local = os.path.join(os.path.dirname(__file__), 'local.py')
+        execfile(local)
     except:
-            pass
+        pass
 
     if live_connection:
-            #TODO: you must set the auth credentials to something valid
-            from boto.mturk.connection import MTurkConnection
+        #TODO: you must set the auth credentials to something valid
+        from boto.mturk.connection import MTurkConnection
     else:
-            # Here the credentials must be set, but it doesn't matter what
-            #  they're set to.
-            os.environ.setdefault('AWS_ACCESS_KEY_ID', 'foo')
-            os.environ.setdefault('AWS_SECRET_ACCESS_KEY', 'bar')
-            from mocks import MTurkConnection
+        # Here the credentials must be set, but it doesn't matter what
+        #  they're set to.
+        os.environ.setdefault('AWS_ACCESS_KEY_ID', 'foo')
+        os.environ.setdefault('AWS_SECRET_ACCESS_KEY', 'bar')
+        from mocks import MTurkConnection
     SetHostMTurkConnection = functools.partial(MTurkConnection, host=mturk_host)
