7f01b57372a0f7e4733f06315c6f032aa205e01f
Abhinav Upadhyay <er.abhinav.upadhyay@gmail.com>
2012-12-26 00:28:34 +0530

    Convert tabs to spaces.

--- tests/mturk/test_disable_hit.py
+++ tests/mturk/test_disable_hit.py
@@ -4,8 +4,8 @@ from common import MTurkCommon
 from boto.mturk.connection import MTurkRequestError
 
 class TestDisableHITs(MTurkCommon):
-	def test_disable_invalid_hit(self):
-		self.assertRaises(MTurkRequestError, self.conn.disable_hit, 'foo')
+    def test_disable_invalid_hit(self):
+        self.assertRaises(MTurkRequestError, self.conn.disable_hit, 'foo')
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
