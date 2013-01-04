dc39e0296eaf612a34637afff8be70168f381286
Mike Schwartz <mfschwartz@google.com>
2013-01-03 17:54:45 -0800

    Changed boto auth plugin framework to allow multiple auth plugins supporting requested capability

--- boto/exception.py
+++ boto/exception.py
@@ -409,17 +409,6 @@ class NoAuthHandlerFound(Exception):
     """Is raised when no auth handlers were found ready to authenticate."""
     pass
 
-class TooManyAuthHandlerReadyToAuthenticate(Exception):
-    """Is raised when there are more than one auth handler ready.
-
-    In normal situation there should only be one auth handler that is ready to
-    authenticate. In case where more than one auth handler is ready to
-    authenticate, we raise this exception, to prevent unpredictable behavior
-    when multiple auth handlers can handle a particular case and the one chosen
-    depends on the order they were checked.
-    """
-    pass
-
 # Enum class for resumable upload failure disposition.
 class ResumableTransferDisposition(object):
     # START_OVER means an attempt to resume an existing transfer failed,
