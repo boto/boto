b6b2b3eca995e2eb7f4d527825520584da25c2de
Pascal Hakim <pasc@redellipse.net>
2013-02-02 19:15:47 -0500

    Re-implemented cloudsearch connection tests using AWSMockServiceTestCase

--- tests/unit/cloudsearch/test_connection.py
+++ tests/unit/cloudsearch/test_connection.py
@@ -1,24 +1,17 @@
 #!/usr/bin env python
 
-from tests.unit import unittest
-from httpretty import HTTPretty
-import urlparse
+from tests.unit import AWSMockServiceTestCase
 
-import boto.cloudsearch
 from boto.cloudsearch.domain import Domain
+from boto.cloudsearch.layer1 import Layer1
 
-class CloudSearchConnectionTest(unittest.TestCase):
-    def setUp(self):
-        HTTPretty.enable()
-        HTTPretty.register_uri(HTTPretty.POST, "https://cloudsearch.us-east-1.amazonaws.com/",
-            body=self.response,
-            content_type="text/xml")
+import json
 
-    def tearDown(self):
-        HTTPretty.disable()
+class TestCloudSearchCreateDomain(AWSMockServiceTestCase):
+    connection_class = Layer1
 
-class CloudSearchConnectionCreationTest(CloudSearchConnectionTest):
-    response = """
+    def default_body(self):
+        return """
 <CreateDomainResponse xmlns="http://cloudsearch.amazonaws.com/doc/2011-02-01">
   <CreateDomainResult>
     <DomainStatus>
@@ -47,21 +40,25 @@ class CloudSearchConnectionCreationTest(CloudSearchConnectionTest):
 </CreateDomainResponse>
 """
 
-    def test_cloudsearch_connection(self):
-        """Check that the correct arguments are sent to AWS when creating a cloudsearch connection"""
-        conn = boto.cloudsearch.connect_to_region("us-east-1", aws_access_key_id='key_id', aws_secret_access_key='access_key')
-        domain = Domain(conn, conn.create_domain('demo'))
+    def test_create_domain(self):
+        self.set_http_response(status_code=200)
+        api_response = self.service_connection.create_domain('demo')
 
-        args = urlparse.parse_qs(HTTPretty.last_request.body)
-        
-        self.assertEqual(args['AWSAccessKeyId'], ['key_id'])
-        self.assertEqual(args['Action'], ['CreateDomain'])
-        self.assertEqual(args['DomainName'], ['demo'])
+        self.assert_request_parameters({
+            'Action': 'CreateDomain',
+            'DomainName': 'demo',
+            'AWSAccessKeyId': 'aws_access_key_id',
+            'SignatureMethod': 'HmacSHA256',
+            'SignatureVersion': 2,
+            'Version': '2011-02-01',
+        }, ignore_params_values=['Timestamp'])
 
     def test_cloudsearch_connect_result_endpoints(self):
         """Check that endpoints & ARNs are correctly returned from AWS"""
-        conn = boto.cloudsearch.connect_to_region("us-east-1", aws_access_key_id='key_id', aws_secret_access_key='access_key')
-        domain = Domain(conn, conn.create_domain('demo'))
+
+        self.set_http_response(status_code=200)
+        api_response = self.service_connection.create_domain('demo')
+        domain = Domain(self, api_response)
 
         self.assertEqual(domain.doc_service_arn, "arn:aws:cs:us-east-1:1234567890:doc/demo")
         self.assertEqual(domain.doc_service_endpoint, "doc-demo-userdomain.us-east-1.cloudsearch.amazonaws.com")
@@ -70,8 +67,9 @@ class CloudSearchConnectionCreationTest(CloudSearchConnectionTest):
 
     def test_cloudsearch_connect_result_statuses(self):
         """Check that domain statuses are correctly returned from AWS"""
-        conn = boto.cloudsearch.connect_to_region("us-east-1", aws_access_key_id='key_id', aws_secret_access_key='access_key')
-        domain = Domain(conn, conn.create_domain('demo'))
+        self.set_http_response(status_code=200)
+        api_response = self.service_connection.create_domain('demo')
+        domain = Domain(self, api_response)
 
         self.assertEqual(domain.created, True)
         self.assertEqual(domain.processing, False)
@@ -80,35 +78,37 @@ class CloudSearchConnectionCreationTest(CloudSearchConnectionTest):
 
     def test_cloudsearch_connect_result_details(self):
         """Check that the domain information is correctly returned from AWS"""
-        conn = boto.cloudsearch.connect_to_region("us-east-1", aws_access_key_id='key_id', aws_secret_access_key='access_key')
-        domain = Domain(conn, conn.create_domain('demo'))
+        self.set_http_response(status_code=200)
+        api_response = self.service_connection.create_domain('demo')
+        domain = Domain(self, api_response)
 
         self.assertEqual(domain.id, "1234567890/demo")
         self.assertEqual(domain.name, "demo")
 
-    def test_cloudsearch_connect_to_invalid_region(self):
-        conn = boto.cloudsearch.connect_to_region("missing-region", aws_access_key_id='key_id', aws_secret_access_key='access_key')
-
-        self.assertEqual(conn, None)
-
     def test_cloudsearch_documentservice_creation(self):
-        conn = boto.cloudsearch.connect_to_region("us-east-1", aws_access_key_id='key_id', aws_secret_access_key='access_key')
-        domain = Domain(conn, conn.create_domain('demo'))
+        self.set_http_response(status_code=200)
+        api_response = self.service_connection.create_domain('demo')
+        domain = Domain(self, api_response)
 
         document = domain.get_document_service()
 
         self.assertEqual(document.endpoint, "doc-demo-userdomain.us-east-1.cloudsearch.amazonaws.com")
 
     def test_cloudsearch_searchservice_creation(self):
-        conn = boto.cloudsearch.connect_to_region("us-east-1", aws_access_key_id='key_id', aws_secret_access_key='access_key')
-        domain = Domain(conn, conn.create_domain('demo'))
+        self.set_http_response(status_code=200)
+        api_response = self.service_connection.create_domain('demo')
+        domain = Domain(self, api_response)
 
         search = domain.get_search_service()
 
         self.assertEqual(search.endpoint, "search-demo-userdomain.us-east-1.cloudsearch.amazonaws.com")
 
-class CloudSearchConnectionDeletionTest(CloudSearchConnectionTest):
-    response = """
+
+class CloudSearchConnectionDeletionTest(AWSMockServiceTestCase):
+    connection_class = Layer1
+
+    def default_body(self):
+        return """
 <DeleteDomainResponse xmlns="http://cloudsearch.amazonaws.com/doc/2011-02-01">
   <DeleteDomainResult>
     <DomainStatus>
@@ -139,17 +139,24 @@ class CloudSearchConnectionDeletionTest(CloudSearchConnectionTest):
 
     def test_cloudsearch_deletion(self):
         """Check that the correct arguments are sent to AWS when creating a cloudsearch connection"""
-        conn = boto.cloudsearch.connect_to_region("us-east-1", aws_access_key_id='key_id', aws_secret_access_key='access_key')
-        conn.delete_domain('demo')
+        self.set_http_response(status_code=200)
+        api_response = self.service_connection.delete_domain('demo')
 
-        args = urlparse.parse_qs(HTTPretty.last_request.body)
-        
-        self.assertEqual(args['AWSAccessKeyId'], ['key_id'])
-        self.assertEqual(args['Action'], ['DeleteDomain'])
-        self.assertEqual(args['DomainName'], ['demo'])
+        self.assert_request_parameters({
+            'Action': 'DeleteDomain',
+            'DomainName': 'demo',
+            'AWSAccessKeyId': 'aws_access_key_id',
+            'SignatureMethod': 'HmacSHA256',
+            'SignatureVersion': 2,
+            'Version': '2011-02-01',
+        }, ignore_params_values=['Timestamp'])
 
-class CloudSearchConnectionIndexDocumentTest(CloudSearchConnectionTest):
-    response = """
+
+class CloudSearchConnectionIndexDocumentTest(AWSMockServiceTestCase):
+    connection_class = Layer1
+
+    def default_body(self):
+        return """
 <IndexDocumentsResponse xmlns="http://cloudsearch.amazonaws.com/doc/2011-02-01">
   <IndexDocumentsResult>
     <FieldNames>
@@ -186,19 +193,22 @@ class CloudSearchConnectionIndexDocumentTest(CloudSearchConnectionTest):
 
     def test_cloudsearch_index_documents(self):
         """Check that the correct arguments are sent to AWS when indexing a domain"""
-        conn = boto.cloudsearch.connect_to_region("us-east-1", aws_access_key_id='key_id', aws_secret_access_key='access_key')
-        conn.index_documents('demo')
+        self.set_http_response(status_code=200)
+        api_response = self.service_connection.index_documents('demo')
 
-        args = urlparse.parse_qs(HTTPretty.last_request.body)
-        
-        self.assertEqual(args['AWSAccessKeyId'], ['key_id'])
-        self.assertEqual(args['Action'], ['IndexDocuments'])
-        self.assertEqual(args['DomainName'], ['demo'])
+        self.assert_request_parameters({
+            'Action': 'IndexDocuments',
+            'DomainName': 'demo',
+            'AWSAccessKeyId': 'aws_access_key_id',
+            'SignatureMethod': 'HmacSHA256',
+            'SignatureVersion': 2,
+            'Version': '2011-02-01',
+        }, ignore_params_values=['Timestamp'])
 
     def test_cloudsearch_index_documents_resp(self):
         """Check that the AWS response is being parsed correctly when indexing a domain"""
-        conn = boto.cloudsearch.connect_to_region("us-east-1", aws_access_key_id='key_id', aws_secret_access_key='access_key')
-        results = conn.index_documents('demo')
+        self.set_http_response(status_code=200)
+        api_response = self.service_connection.index_documents('demo')
 
-        self.assertEqual(results, ['average_score', 'brand_id', 'colors', 'context', 'context_owner', 'created_at', 'creator_id', 'description', 'file_size', 'format', 'has_logo', 'has_messaging', 'height', 'image_id', 'ingested_from', 'is_advertising', 'is_photo', 'is_reviewed', 'modified_at', 'subject_date', 'tags', 'title', 'width'])
+        self.assertEqual(api_response, ['average_score', 'brand_id', 'colors', 'context', 'context_owner', 'created_at', 'creator_id', 'description', 'file_size', 'format', 'has_logo', 'has_messaging', 'height', 'image_id', 'ingested_from', 'is_advertising', 'is_photo', 'is_reviewed', 'modified_at', 'subject_date', 'tags', 'title', 'width'])
 
