f4b1452cc69dab18abb378e0481a42935d8fdab3
Toby Burress <kurin@delete.org>
2013-02-13 22:25:08 -0500

    Remove a hack.
    So in 2.5 and below, apparently "host" had to be amended to "host:port"
    for compatibility with httplib.  The code would check to see if we
    were running 2.6 and 2.7, which in 3.3 we *aren't*, but I guess it
    never got the memo.
    
    2.5 and older aren't supported now anyway, so remove the check entirely.

--- boto/connection.py
+++ boto/connection.py
@@ -632,21 +632,7 @@ class AWSAuthConnection(object):
     def server_name(self, port=None):
         if not port:
             port = self.port
-        if port == 80:
-            signature_host = self.host
-        else:
-            # This unfortunate little hack can be attributed to
-            # a difference in the 2.6 version of httplib.  In old
-            # versions, it would append ":443" to the hostname sent
-            # in the Host header and so we needed to make sure we
-            # did the same when calculating the V2 signature.  In 2.6
-            # (and higher!)
-            # it no longer does that.  Hence, this kludge.
-            if ((ON_APP_ENGINE and sys.version[:3] == '2.5') or
-                    sys.version[:3] in ('2.6', '2.7')) and port == 443:
-                signature_host = self.host
-            else:
-                signature_host = '%s:%d' % (self.host, port)
+        signature_host = self.host
         return signature_host
 
     def handle_proxy(self, proxy, proxy_port, proxy_user, proxy_pass):
