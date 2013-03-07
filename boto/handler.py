625930e6aceaf327c7b8874dfdaf1db13936a08d
James Saryerwinnie <js@jamesls.com>
2013-03-07 11:25:21 -0800

    Remove trailing space

--- boto/handler.py
+++ boto/handler.py
@@ -44,12 +44,13 @@ class XmlHandler(xml.sax.ContentHandler):
     def characters(self, content):
         self.current_text += content
 
+
 class XmlHandlerWrapper(object):
     def __init__(self, root_node, connection):
         self.handler = XmlHandler(root_node, connection)
         self.parser = xml.sax.make_parser()
         self.parser.setContentHandler(self.handler)
         self.parser.setFeature(xml.sax.handler.feature_external_ges, 0)
-    
+
     def parseString(self, content):
         return self.parser.parse(StringIO.StringIO(content))
