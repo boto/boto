e94bc5b5086ce4c5064eb7303c04b8cceae4ef46
James Saryerwinnie <js@jamesls.com>
2013-03-10 19:56:12 -0700

    Update ec2 with new operations

--- boto/ec2/image.py
+++ boto/ec2/image.py
@@ -348,3 +348,16 @@ class ImageAttribute:
             self.ramdisk = value
         else:
             setattr(self, name, value)
+
+
+class CopyImage(object):
+    def __init__(self, parent=None):
+        self._parent = parent
+        self.image_id = None
+
+    def startElement(self, name, attrs, connection):
+        pass
+
+    def endElement(self, name, value, connection):
+        if name == 'imageId':
+            self.image_id = value
