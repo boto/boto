146fd1a7450df338b6a743c18483b5652a6d82be
haseebalam <in.haseeb@aol.in>
2013-03-30 16:20:34 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the
 # "Software"), to deal in the Software without restriction, including
-# without limitation the rights to use, copy, modify, merge, publish, dis-
+# without limitation the rights to use, reuse, copy, modify, merge, publish, dis-
 # tribute, sublicense, and/or sell copies of the Software, and to permit
 # persons to whom the Software is furnished to do so, subject to the fol-
 # lowing conditions:
