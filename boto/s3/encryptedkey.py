4188ab5dd6452dc74644c8ab7b7b2189f54c9c0c
Cody Hanson <codyhanson50@gmail.com>
2013-04-03 17:33:40 -0600

    Started dev on the EncryptedKey extension to Key

---
+++ boto/s3/encryptedkey.py
@@ -0,0 +1,26 @@
+
+import Crypto
+
+class EncryptedKey(boto.s3.Key):
+    """
+    Extends the Key class to perform local AES Encryption
+
+
+
+
+    """
+   def __init__(self, bucket=None, name=None, encryptionKey, encryptionKeyName):
+
+
+
+
+    def set_contents_from_filename:
+
+
+    def get_contents_to_filename:
+
+
+    def set_contents_from_string:
+
+
+    def get_contents_as_string:
