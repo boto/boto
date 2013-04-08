0be71d5a2985f71b1e387ff96805d440e069f43e
ukhersu <sukrit.khera@pearson.com>
2013-04-08 02:24:05 -0700

    pep8 standards

--- tests/unit/vpc/test_vpc.py
+++ tests/unit/vpc/test_vpc.py
@@ -13,8 +13,8 @@ class TestVPCConnection(unittest.TestCase):
     def setUp(self):
         unittest.TestCase.setUp(self)
         self.vpc_connection = VPCConnection(
-                                aws_access_key_id='aws_access_key_id',
-                                aws_secret_access_key='aws_secret_access_key')
+            aws_access_key_id='aws_access_key_id',
+            aws_secret_access_key='aws_secret_access_key')
 
     def test_detach_internet_gateway(self):
         """
@@ -26,13 +26,13 @@ class TestVPCConnection(unittest.TestCase):
 
         def get_status(status, params):
             if status == "DetachInternetGateway" and \
-            params["InternetGatewayId"] == internet_gateway_id and \
-            params["VpcId"] == vpc_id:
+                params["InternetGatewayId"] == internet_gateway_id and \
+                    params["VpcId"] == vpc_id:
                 return True
             else:
                 return False
 
         self.vpc_connection.get_status = get_status
         status = self.vpc_connection.detach_internet_gateway(
-                                    internet_gateway_id, vpc_id)
+            internet_gateway_id, vpc_id)
         self.assertEquals(True, status)
