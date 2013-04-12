1d9d2939f511586c1e0f9ccd9f32be4bb5b96bf7
James Saryerwinnie <js@jamesls.com>
2013-04-12 16:09:58 -0700

    Fix #1146: return response from custom url opener
    The previous fix for this (70e9ec) created a proxy opener
    and retrieved metadata results, but then called urlopen(...)
    and returned those results.  The end result was that you
    were getting the proxied result, which is not what we want.
    
    As a side effect of this bug, we were making twice as many
    calls to the metadata service than we needed, once using
    the custom opener we create, and once using urllib2.urlopen.
    
    I've also added a unit test for this fix.  It's admittedly a bit
    contrived, but it did fail before implementing the fix, and it
    does pass now with the fix in place.

--- tests/unit/utils/test_utils.py
+++ tests/unit/utils/test_utils.py
@@ -23,9 +23,12 @@ import unittest
 import hashlib
 import hmac
 
+import mock
+
 from boto.utils import Password
 from boto.utils import pythonize_name
 from boto.utils import _build_instance_metadata_url
+from boto.utils import retry_url
 
 
 class TestPassword(unittest.TestCase):
@@ -152,6 +155,35 @@ class TestBuildInstanceMetadataURL(unittest.TestCase):
             ),
             'http://10.0.1.5/2013-03-22/user-data/'
         )
+
+
+class TestRetryURL(unittest.TestCase):
+    def setUp(self):
+        self.urlopen_patch = mock.patch('urllib2.urlopen')
+        self.opener_patch = mock.patch('urllib2.build_opener')
+        self.urlopen = self.urlopen_patch.start()
+        self.opener = self.opener_patch.start()
+
+    def tearDown(self):
+        self.urlopen_patch.stop()
+        self.opener_patch.stop()
+
+    def set_normal_response(self, response):
+        fake_response = mock.Mock()
+        fake_response.read.return_value = response
+        self.urlopen.return_value = fake_response
+
+    def set_no_proxy_allowed_response(self, response):
+        fake_response = mock.Mock()
+        fake_response.read.return_value = response
+        self.opener.return_value.open.return_value = fake_response
+
+    def test_retry_url_uses_proxy(self):
+        self.set_normal_response('normal response')
+        self.set_no_proxy_allowed_response('no proxy response')
+
+        response = retry_url('http://10.10.10.10/foo', num_retries=1)
+        self.assertEqual(response, 'no proxy response')
 
 
 if __name__ == '__main__':
