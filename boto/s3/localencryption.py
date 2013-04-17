3db82b1b20901b0362e8a1367328f9c5c63e021f
Cody Hanson <codyhanson50@gmail.com>
2013-04-16 23:27:43 -0600

    Need to get the MD5 to recompute after we encrypt

--- boto/s3/localencryption.py
+++ boto/s3/localencryption.py
@@ -1,5 +1,3 @@
-
-import boto.s3.key
 from Crypto.Cipher import AES
 from Crypto import Random
 import base64
@@ -45,28 +43,3 @@ class AESCipher:
         return fp
 
 
-class EncryptedKey(boto.s3.key.Key):
-    """
-    Extends the Key class to perform local AES Encryption on files and strings
-
-    Also can perform a stream cipher on stream based data.(maybe?)
-
-    Design goal to not break any functionality of the boto.s3.key.Key class.
-    Acts as an encryption and decryption wrapper class around the core S3 functionality
-
-    """
-    def __init__(self, encryptionKey, bucket=None, name=None):
-        self.encryptionKey = encryptionKey
-        self.aes = AESCipher(hashlib.sha256(encryptionKey).digest())
-        super(EncryptedKey,self).__init__(bucket,name)
-        super(EncryptedKey,self).send_file() = self.send_file
-
-
-    def send_file(self,fp, headers=None, cb=None, num_cb=10, 
-        query_args=None, chunked_transfer=False, size=None):
-        super(EncryptedKey,self).send_file(fp,headers,cb,num_cb,query_args,chunked_transfer,size)
-        return self.aes.encryptFP(fp)
-
-    def get_file(self,fp, headers=None, cb=None, num_cb=10, torrent=None,version_id=None,override_num_retries=None,response_headers=None ):
-        super(EncryptedKey,self).get_file(fp,headers,cb,num_cb, torrent, version_id, override_num_retries, response_headers)
-        return self.aes.decryptFP(fp)
--- test.py
+++ test.py
@@ -1,18 +1,28 @@
 #! /usr/bin/env python
 
 from boto.s3.connection import S3Connection
-from boto.s3.encryptedkey import EncryptedKey
+from boto.s3.key import Key
 from boto.exception import S3ResponseError
 
+import sys
+
 conn = S3Connection()
+
+if (conn == None):
+    print "no connection"
+    sys.exit(1)
 bucket = conn.get_bucket("testingboto")
 
 if (bucket == None):
     print "no bucket"
 
+print bucket
+
 string = "this is a test string"
 
-ekey = EncryptedKey("password",bucket,"cipherString")
+print "creating encryptedKey"
+key = Key(bucket,"cipherString2","password")
 
-ekey.set_contents_from_string(string)
+print "setting contents"
+key.set_contents_from_string(string)
 print "done"
