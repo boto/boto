1d33ddcb83d117297d46d1489fe274b26e9c254f
Cody Hanson <codyhanson50@gmail.com>
2013-04-18 08:20:27 -0600

    Set/Get String is working. Finally figured out a good inheritance scheme, only overriding set/get from filepointer. Need to start writing tests. One question: preserve offset of filepointer?

--- boto/s3/encryptedkey.py
+++ boto/s3/encryptedkey.py
@@ -6,7 +6,6 @@ import base64
 import hashlib
 
 
-#blocksize and padding functions
 class AESCipher:
     def __init__( self, key ):
         self.key = key 
@@ -44,15 +43,18 @@ class AESCipher:
         fp.seek(0)
         return fp
 
-
-#class StreamCipher:
-
 class EncryptedKey(boto.s3.key.Key):
     """
     Extends the Key class to perform local AES Encryption on files and strings
 
     Design goal to not break any functionality of the boto.s3.key.Key class.
     Acts as an encryption and decryption wrapper class around the core S3 functionality
+
+    By overriding set_contents_from_file and get_contents_from_file,
+    we can protect many differenent parts of the api, including
+    getter and setter functions for strings, filenames, and filepointers.
+
+    raw calling of 'key.send_file' and 'key.get_file' will not be protected however.
 
     """
     def __init__(self, encryptionKey, bucket=None, name=None):
@@ -61,47 +63,11 @@ class EncryptedKey(boto.s3.key.Key):
         super(EncryptedKey,self).__init__(bucket,name)
 
 
-    #data retrieval functions
-
     def set_contents_from_file(self,fp, headers=None, replace=True, 
         cb=None, num_cb=10, policy=None, md5=None, reduced_redundancy=False, 
         query_args=None, encrypt_key=False, size=None, rewind=False):
         fp = self.aes.encryptFP(fp)
-        r = super(EncryptedKey,self).set_contents_from_file(fp,headers,replace,cb,num_cb,policy,md5,reduced_redundancy,query_args,encrypt_key,size,rewind)
-        print r
-
-    def set_contents_from_filename(self, filename, headers=None, 
-        replace=True, cb=None, num_cb=10, policy=None, md5=None, 
-        reduced_redundancy=False, encrypt_key=False):
-        fp = open(filename)
-        fp = self.aes.encryptFP(fp)
-        self.set_contents_from_file(fp,headers,replace,cb,num_cb,policy,md5,reduced_redundancy,encrypt_key)
-
-
-
-    def set_contents_from_string(self,s, headers=None, replace=True, 
-        cb=None, num_cb=10, policy=None, md5=None, reduced_redundancy=False, 
-        encrypt_key=False):
-        ciphertext = self.aes.encrypt(s)
-        print ciphertext
-        super(EncryptedKey,self).set_contents_from_string(ciphertext,headers,replace,cb,num_cb,policy,md5,reduced_redundancy,encrypt_key)
-
-
-    # Implement?
-    # throw exception?
-    def send_file(self,fp, headers=None, cb=None, num_cb=10, 
-        query_args=None, chunked_transfer=False, size=None):
-        pass
-
-
-    def set_contents_from_stream(self, fp, headers=None, replace=True, 
-        cb=None, num_cb=10, policy=None, reduced_redundancy=False, 
-        query_args=None, size=None):
-        pass
-
-
-
-    #data upload functions
+        return super(EncryptedKey,self).set_contents_from_file(fp,headers,replace,cb,num_cb,policy,md5,reduced_redundancy,query_args,encrypt_key,size,rewind)
 
 
     def get_contents_to_file(self,fp, headers=None, cb=None, 
@@ -111,24 +77,5 @@ class EncryptedKey(boto.s3.key.Key):
         #make sure we are at the beginning of the file
         #or is the offset important?
         fp.seek(0)
-        fp = decryptFP(fp)
+        fp = self.aes.decryptFP(fp)
 
-    def get_contents_to_filename(self,filename, headers=None, cb=None, 
-        num_cb=10, torrent=False, version_id=None, res_download_handler=None,
-        response_headers=None):
-        fp = open(filename)
-        self.get_contents_to_file(fp,headers,cb,num_cb,torrent,version_id,res_download_handler,response_headers)
-
-
-    #implement this one?
-    # throw exception?
-    def get_file(self,filename, headers=None, replace=True,
-        cb=None, num_cb=10, policy=None, md5=None,
-        reduced_redundancy=False, encrypt_key=False):
-        pass
-
-    def get_contents_as_string(self,headers=None, cb=None, num_cb=10, 
-        torrent=False, version_id=None, response_headers=None):
-        ciphercontent = super(EncryptedKey,self).get_contents_as_string(headers,cb,num_cb,torrent,version_id,response_headers)
-        plaincontent = self.aes.decrypt(ciphercontent)
-        return plaincontent
--- test.py
+++ test.py
@@ -2,10 +2,11 @@
 
 from boto.s3.connection import S3Connection
 from boto.s3.encryptedkey import EncryptedKey
+from boto.s3.key import Key
 from boto.exception import S3ResponseError
 
 conn = S3Connection()
-bucket = conn.get_bucket("testingboto")
+bucket = conn.get_bucket("testingboto2")
 
 if (bucket == None):
     print "no bucket"
@@ -15,4 +16,9 @@ string = "this is a test string2"
 ekey = EncryptedKey("password",bucket,"cipherString2.txt")
 
 ekey.set_contents_from_string(string)
-print "done"
+
+print "done putting, now getting."
+
+ekey = EncryptedKey("password",bucket,"cipherString2.txt")
+print ekey.get_contents_as_string()
+
