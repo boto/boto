0e261dcb09398e6eab1e60fefd2f4f14c9526cf9
xbe <xbe@machine>
2013-05-03 01:12:57 -0700

    Allow Route 53 to work with zones that have health checks.

--- boto/route53/connection.py
+++ boto/route53/connection.py
@@ -51,10 +51,10 @@ class Route53Connection(AWSAuthConnection):
     DefaultHost = 'route53.amazonaws.com'
     """The default Route53 API endpoint to connect to."""
 
-    Version = '2012-02-29'
+    Version = '2012-12-12'
     """Route53 API version."""
 
-    XMLNameSpace = 'https://route53.amazonaws.com/doc/2012-02-29/'
+    XMLNameSpace = 'https://route53.amazonaws.com/doc/2012-12-12/'
     """XML schema for this Route53 API version."""
 
     def __init__(self, aws_access_key_id=None, aws_secret_access_key=None,
