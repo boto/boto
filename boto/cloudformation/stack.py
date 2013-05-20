a25c30a3095ec65017937f3a86fb9266466bb0b2
Daniel Lindsley <daniel@toastdriven.com>
2013-05-20 16:20:47 -0700

    Added tests for the cloudformation millisecond bug.

--- boto/cloudformation/stack.py
+++ boto/cloudformation/stack.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-import re
 
 from boto.resultset import ResultSet
 
@@ -42,11 +41,10 @@ class Stack(object):
 
     def endElement(self, name, value, connection):
         if name == 'CreationTime':
-            p = re.compile('\d{4,4}-\d{2,2}-\d{2,2}T\d{2,2}:\d{2,2}:\d{2,2}\.\d{1,6}Z')
-            if p.match(value):
-                self.creation_time = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%fZ')
-            else:
+            try:
                 self.creation_time = datetime.strptime(value, '%Y-%m-%dT%H:%M:%SZ')
+            except ValueError:
+                self.creation_time = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%fZ')
         elif name == "Description":
             self.description = value
         elif name == "DisableRollback":
@@ -217,7 +215,7 @@ class Tag(dict):
             self._current_value = value
         else:
             setattr(self, name, value)
-        
+
         if self._current_key and self._current_value:
             self[self._current_key] = self._current_value
             self._current_key = None
@@ -356,11 +354,10 @@ class StackEvent(object):
         elif name == "StackName":
             self.stack_name = value
         elif name == "Timestamp":
-            p = re.compile('\d{4,4}-\d{2,2}-\d{2,2}T\d{2,2}:\d{2,2}:\d{2,2}\.\d{1,6}Z')
-            if p.match(value):
-                self.timestamp = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%fZ')
-            else:
+            try:
                 self.timestamp = datetime.strptime(value, '%Y-%m-%dT%H:%M:%SZ')
+            except ValueError:
+                self.timestamp = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%fZ')
         else:
             setattr(self, name, value)
 
