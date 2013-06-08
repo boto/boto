be07f7a3304ceb7b85c2b5e4231f54c3914ea6c5
Daniel Lindsley <daniel@toastdriven.com>
2013-06-07 21:17:01 -0700

    Added a failing testcase for trailing path slashes in SigV4.

--- tests/unit/auth/test_sigv4.py
+++ tests/unit/auth/test_sigv4.py
@@ -73,6 +73,16 @@ class TestSigV4Handler(unittest.TestCase):
         # This should be both normalized & urlencoded.
         self.assertEqual(canonical_uri, 'x/x%20.html')
 
+        auth = HmacAuthV4Handler('glacier.us-east-1.amazonaws.com',
+                                 Mock(), self.provider)
+        request = HTTPRequest(
+            'GET', 'https', 'glacier.us-east-1.amazonaws.com', 443,
+            'x/./././x/html/', None, {},
+            {'x-amz-glacier-version': '2012-06-01'}, '')
+        canonical_uri = auth.canonical_uri(request)
+        # This should be both normalized & urlencoded.
+        self.assertEqual(canonical_uri, 'x/x/html/')
+
     def test_headers_to_sign(self):
         auth = HmacAuthV4Handler('glacier.us-east-1.amazonaws.com',
                                  Mock(), self.provider)
