0dbbff6fa6cf1986289200c82f4345ef43c7ce89
Adrian <adrian@startgridnetwork.com>
2013-06-13 18:29:38 -0700

    Added a try block around json.loads call, and code to look for 403 errors

--- boto/cloudsearch/search.py
+++ boto/cloudsearch/search.py
@@ -289,7 +289,19 @@ class SearchConnection(object):
         params = query.to_params()
 
         r = requests.get(url, params=params)
-        data = json.loads(r.content)
+        try:
+            data = json.loads(r.content)
+        except json.JSONDecodeError,e:
+            if r.status_code == 403:
+                msg = ''
+                import re
+                g = re.search('<html><body><h1>403 Forbidden</h1>([^<]+)<', r.content)
+                try:
+                    msg = ': %s' % (g.groups()[0].strip())
+                except AttributeError:
+                    pass
+                raise SearchServiceException('Authentication error from Amazon%s' % msg)
+            raise SearchServiceException("Got non-json response from Amazon")
         data['query'] = query
         data['search_service'] = self
 
