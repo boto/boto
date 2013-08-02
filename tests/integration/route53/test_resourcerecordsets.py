276df7eb8f0e41a1bcc048ae6d9f08c4618066a1
James Legg <james.legg.conversocial.com>
2013-08-02 18:34:03 +0100

    test for ResourceRecordSet miscounting problem

--- tests/integration/route53/test_resourcerecordsets.py
+++ tests/integration/route53/test_resourcerecordsets.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2013 Amazon.com, Inc. or its affiliates.  All Rights Reserved
+#ovei Copyright (c) 2013 Amazon.com, Inc. or its affiliates.  All Rights Reserved
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the
@@ -48,6 +48,43 @@ class TestRoute53ResourceRecordSets(unittest.TestCase):
         deleted.add_value('192.168.0.25')
         rrs.commit()
 
+    def test_record_count(self):
+        rrs = ResourceRecordSets(self.conn, self.zone.id)
+        hosts = 230
+        hosts = 2
+        for hostid in range(hosts):
+            rec = "test" + str(hostid) + ".example.com"
+            created = rrs.add_change("CREATE", rec , "A")
+            ip = '192.168.0.' + str(hostid)
+            created.add_value(ip)
+
+        rrs.commit()
+
+        all_records = self.conn.get_all_rrsets(self.zone.id)
+
+        #first time around was always fine
+        i = 0
+        for rset in all_records:
+            i += 1
+
+        #second time was a failure
+        i = 0
+        for rset in all_records:
+            i += 1
+
+        #cleanup indivual records
+        rrs = ResourceRecordSets(self.conn, self.zone.id)
+        for hostid in range(hosts):
+            rec = "test" + str(hostid) + ".example.com"
+            deleted = rrs.add_change("DELETE", rec , "A")
+            ip = '192.168.0.' + str(hostid)
+            deleted.add_value(ip)
+
+        rrs.commit()
+        
+        #2nd count should match the number of hosts plus NS/SOA records
+        records = hosts + 2
+        self.assertEqual(i,records)
 
 if __name__ == '__main__':
     unittest.main()
