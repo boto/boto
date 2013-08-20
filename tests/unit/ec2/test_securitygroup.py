e63aae8fef179a3d9a4231c5961bd6349cf21b61
Daniel G. Taylor <danielgtaylor@gmail.com>
2013-08-20 11:42:45 -0700

    Add unit tests for new security group filtering behavior

---
+++ tests/unit/ec2/test_securitygroup.py
@@ -0,0 +1,184 @@
+#!/usr/bin/env python
+
+from tests.unit import unittest
+from tests.unit import AWSMockServiceTestCase
+
+import mock
+
+from boto.ec2.connection import EC2Connection
+
+DESCRIBE_SECURITY_GROUP = r"""<?xml version="1.0" encoding="UTF-8"?>
+<DescribeSecurityGroupsResponse xmlns="http://ec2.amazonaws.com/doc/2013-06-15/">
+   <requestId>59dbff89-35bd-4eac-99ed-be587EXAMPLE</requestId>
+   <securityGroupInfo>
+      <item>
+         <ownerId>111122223333</ownerId>
+         <groupId>sg-1a2b3c4d</groupId>
+         <groupName>WebServers</groupName>
+         <groupDescription>Web Servers</groupDescription>
+         <vpcId/>
+         <ipPermissions>
+            <item>
+               <ipProtocol>tcp</ipProtocol>
+               <fromPort>80</fromPort>
+               <toPort>80</toPort>
+               <groups/>
+               <ipRanges>
+                  <item>
+                     <cidrIp>0.0.0.0/0</cidrIp>
+                  </item>
+               </ipRanges>
+            </item>
+         </ipPermissions>
+         <ipPermissionsEgress/>
+      </item>
+      <item>
+         <ownerId>111122223333</ownerId>
+         <groupId>sg-2a2b3c4d</groupId>
+         <groupName>RangedPortsBySource</groupName>
+         <groupDescription>Group A</groupDescription>
+         <ipPermissions>
+            <item>
+               <ipProtocol>tcp</ipProtocol>
+               <fromPort>6000</fromPort>
+               <toPort>7000</toPort>
+               <groups>
+                  <item>
+                     <userId>111122223333</userId>
+                     <groupId>sg-3a2b3c4d</groupId>
+                     <groupName>Group B</groupName>
+                  </item>
+               </groups>
+               <ipRanges/>
+            </item>
+         </ipPermissions>
+         <ipPermissionsEgress/>
+      </item>
+   </securityGroupInfo>
+</DescribeSecurityGroupsResponse>"""
+
+DESCRIBE_INSTANCES = r"""<?xml version="1.0" encoding="UTF-8"?>
+<DescribeInstancesResponse xmlns="http://ec2.amazonaws.com/doc/2012-10-01/">
+    <requestId>c6132c74-b524-4884-87f5-0f4bde4a9760</requestId>
+    <reservationSet>
+        <item>
+            <reservationId>r-72ef4a0a</reservationId>
+            <ownerId>184906166255</ownerId>
+            <groupSet/>
+            <instancesSet>
+                <item>
+                    <instanceId>i-instance</instanceId>
+                    <imageId>ami-1624987f</imageId>
+                    <instanceState>
+                        <code>16</code>
+                        <name>running</name>
+                    </instanceState>
+                    <privateDnsName/>
+                    <dnsName/>
+                    <reason/>
+                    <keyName>mykeypair</keyName>
+                    <amiLaunchIndex>0</amiLaunchIndex>
+                    <productCodes/>
+                    <instanceType>m1.small</instanceType>
+                    <launchTime>2012-12-14T23:48:37.000Z</launchTime>
+                    <placement>
+                        <availabilityZone>us-east-1d</availabilityZone>
+                        <groupName/>
+                        <tenancy>default</tenancy>
+                    </placement>
+                    <kernelId>aki-88aa75e1</kernelId>
+                    <monitoring>
+                        <state>disabled</state>
+                    </monitoring>
+                    <subnetId>subnet-0dc60667</subnetId>
+                    <vpcId>vpc-id</vpcId>
+                    <privateIpAddress>10.0.0.67</privateIpAddress>
+                    <sourceDestCheck>true</sourceDestCheck>
+                    <groupSet>
+                        <item>
+                            <groupId>sg-1a2b3c4d</groupId>
+                            <groupName>WebServerSG</groupName>
+                        </item>
+                    </groupSet>
+                    <architecture>x86_64</architecture>
+                    <rootDeviceType>ebs</rootDeviceType>
+                    <rootDeviceName>/dev/sda1</rootDeviceName>
+                    <blockDeviceMapping>
+                        <item>
+                            <deviceName>/dev/sda1</deviceName>
+                            <ebs>
+                                <volumeId>vol-id</volumeId>
+                                <status>attached</status>
+                                <attachTime>2012-12-14T23:48:43.000Z</attachTime>
+                                <deleteOnTermination>true</deleteOnTermination>
+                            </ebs>
+                        </item>
+                    </blockDeviceMapping>
+                    <virtualizationType>paravirtual</virtualizationType>
+                    <clientToken>foo</clientToken>
+                    <tagSet>
+                        <item>
+                            <key>Name</key>
+                            <value/>
+                        </item>
+                    </tagSet>
+                    <hypervisor>xen</hypervisor>
+                    <networkInterfaceSet>
+                        <item>
+                            <networkInterfaceId>eni-id</networkInterfaceId>
+                            <subnetId>subnet-id</subnetId>
+                            <vpcId>vpc-id</vpcId>
+                            <description>Primary network interface</description>
+                            <ownerId>ownerid</ownerId>
+                            <status>in-use</status>
+                            <privateIpAddress>10.0.0.67</privateIpAddress>
+                            <sourceDestCheck>true</sourceDestCheck>
+                            <groupSet>
+                                <item>
+                                    <groupId>sg-id</groupId>
+                                    <groupName>WebServerSG</groupName>
+                                </item>
+                            </groupSet>
+                            <attachment>
+                                <attachmentId>eni-attach-id</attachmentId>
+                                <deviceIndex>0</deviceIndex>
+                                <status>attached</status>
+                                <attachTime>2012-12-14T23:48:37.000Z</attachTime>
+                                <deleteOnTermination>true</deleteOnTermination>
+                            </attachment>
+                            <privateIpAddressesSet>
+                                <item>
+                                    <privateIpAddress>10.0.0.67</privateIpAddress>
+                                    <primary>true</primary>
+                                </item>
+                                <item>
+                                    <privateIpAddress>10.0.0.54</privateIpAddress>
+                                    <primary>false</primary>
+                                </item>
+                                <item>
+                                    <privateIpAddress>10.0.0.55</privateIpAddress>
+                                    <primary>false</primary>
+                                </item>
+                            </privateIpAddressesSet>
+                        </item>
+                    </networkInterfaceSet>
+                    <ebsOptimized>false</ebsOptimized>
+                </item>
+            </instancesSet>
+        </item>
+    </reservationSet>
+</DescribeInstancesResponse>
+"""
+
+class TestDescribeSecurityGroups(AWSMockServiceTestCase):
+    connection_class = EC2Connection
+
+    def test_get_instances(self):
+        self.set_http_response(status_code=200, body=DESCRIBE_SECURITY_GROUP)
+        groups = self.service_connection.get_all_security_groups()
+
+        self.set_http_response(status_code=200, body=DESCRIBE_INSTANCES)
+        instances = groups[0].instances()
+
+        self.assertEqual(1, len(instances))
+        self.assertEqual(groups[0].id, instances[0].groups[0].id)
