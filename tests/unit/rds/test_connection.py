ed4fd8c8e4d5ba86035de30ff329cbfc33aae38f
Eugene Brevdo <ebrevdo@svbio.com>
2013-08-23 12:52:55 -0700

    Revert "Merge pull request #1683 from danielgtaylor/rds-vpc-sg"
    This reverts commit 1b8d700f54a5247ff942d67cf5c436ffa5c4a301, reversing
    changes made to b22091fa21d4925e54ed0146d2f205ceb36995e4.
    
    dialt0ne's version of vpc + security groups for RDS is more complete.

--- tests/unit/rds/test_connection.py
+++ tests/unit/rds/test_connection.py
@@ -24,7 +24,6 @@
 from tests.unit import unittest
 from tests.unit import AWSMockServiceTestCase
 
-from boto.ec2.securitygroup import SecurityGroup
 from boto.rds import RDSConnection
 from boto.rds.parametergroup import ParameterGroup
 
@@ -386,66 +385,6 @@ class TestRDSConnectionRestoreDBInstanceFromPointInTime(AWSMockServiceTestCase):
             'TargetDBInstanceIdentifier': 'restored-db',
             'UseLatestRestorableTime': 'true',
             'DBSubnetGroupName': 'dbsubnetgroup',
-        }, ignore_params_values=['Version'])
-
-    def test_create_db_instance_vpc_sg_str(self):
-        self.set_http_response(status_code=200)
-        db = self.service_connection.create_dbinstance(
-            'SimCoProd01',
-            10,
-            'db.m1.large',
-            'master',
-            'Password01',
-            param_group='default.mysql5.1',
-            db_subnet_group_name='dbSubnetgroup01',
-            vpc_security_groups=['sg-1', 'sg-2'])
-
-        self.assert_request_parameters({
-            'Action': 'CreateDBInstance',
-            'AllocatedStorage': 10,
-            'AutoMinorVersionUpgrade': 'true',
-            'DBInstanceClass': 'db.m1.large',
-            'DBInstanceIdentifier': 'SimCoProd01',
-            'DBParameterGroupName': 'default.mysql5.1',
-            'DBSubnetGroupName': 'dbSubnetgroup01',
-            'Engine': 'MySQL5.1',
-            'MasterUsername': 'master',
-            'MasterUserPassword': 'Password01',
-            'Port': 3306,
-            'VpcSecurityGroupIds.member.1': 'sg-1',
-            'VpcSecurityGroupIds.member.2': 'sg-2'
-        }, ignore_params_values=['Version'])
-
-    def test_create_db_instance_vpc_sg_obj(self):
-        self.set_http_response(status_code=200)
-
-        sg1 = SecurityGroup(name='sg-1')
-        sg2 = SecurityGroup(name='sg-2')
-
-        db = self.service_connection.create_dbinstance(
-            'SimCoProd01',
-            10,
-            'db.m1.large',
-            'master',
-            'Password01',
-            param_group='default.mysql5.1',
-            db_subnet_group_name='dbSubnetgroup01',
-            vpc_security_groups=[sg1, sg2])
-
-        self.assert_request_parameters({
-            'Action': 'CreateDBInstance',
-            'AllocatedStorage': 10,
-            'AutoMinorVersionUpgrade': 'true',
-            'DBInstanceClass': 'db.m1.large',
-            'DBInstanceIdentifier': 'SimCoProd01',
-            'DBParameterGroupName': 'default.mysql5.1',
-            'DBSubnetGroupName': 'dbSubnetgroup01',
-            'Engine': 'MySQL5.1',
-            'MasterUsername': 'master',
-            'MasterUserPassword': 'Password01',
-            'Port': 3306,
-            'VpcSecurityGroupIds.member.1': 'sg-1',
-            'VpcSecurityGroupIds.member.2': 'sg-2'
         }, ignore_params_values=['Version'])
 
 
