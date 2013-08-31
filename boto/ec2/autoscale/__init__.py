37a81ccaf6fb3292fc8c47ec30d4962b2eb4d8ac
Francis Tseng <ftzeng@gmail.com>
2013-08-31 00:41:50 -0400

    autoscale launch configuration boolean and user data fix

--- boto/ec2/autoscale/__init__.py
+++ boto/ec2/autoscale/__init__.py
@@ -218,7 +218,7 @@ class AutoScaleConnection(AWSQueryConnection):
         if launch_config.key_name:
             params['KeyName'] = launch_config.key_name
         if launch_config.user_data:
-            params['UserData'] = base64.b64encode(launch_config.user_data)
+            params['UserData'] = base64.b64encode(launch_config.user_data).decode('utf-8')
         if launch_config.kernel_id:
             params['KernelId'] = launch_config.kernel_id
         if launch_config.ramdisk_id:
@@ -238,9 +238,9 @@ class AutoScaleConnection(AWSQueryConnection):
         if launch_config.instance_profile_name is not None:
             params['IamInstanceProfile'] = launch_config.instance_profile_name
         if launch_config.ebs_optimized:
-            params['EbsOptimized'] = True
+            params['EbsOptimized'] = 'true'
         else:
-            params['EbsOptimized'] = False
+            params['EbsOptimized'] = 'false'
         return self.get_object('CreateLaunchConfiguration', params,
                                Request, verb='POST')
 
