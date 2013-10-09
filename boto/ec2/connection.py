f2e40595fb5bbbd7e91cd10b9900993c4ef45f51
Daniel G. Taylor <danielgtaylor@gmail.com>
2013-10-09 14:17:43 -0700

    Bump EC2 version number

--- boto/ec2/connection.py
+++ boto/ec2/connection.py
@@ -69,7 +69,7 @@ from boto.exception import EC2ResponseError
 
 class EC2Connection(AWSQueryConnection):
 
-    APIVersion = boto.config.get('Boto', 'ec2_version', '2013-07-15')
+    APIVersion = boto.config.get('Boto', 'ec2_version', '2013-10-01')
     DefaultRegionName = boto.config.get('Boto', 'ec2_region_name', 'us-east-1')
     DefaultRegionEndpoint = boto.config.get('Boto', 'ec2_region_endpoint',
                                             'ec2.us-east-1.amazonaws.com')
