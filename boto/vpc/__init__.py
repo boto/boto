56e74c64ac6aaa36bfb21a4e99283df70bf1f970
Michael Barrientos <mike@syapse.com>
2013-10-26 10:14:13 -0700

    Add detach_vpn_gateway, add VPN gateway tests

--- boto/vpc/__init__.py
+++ boto/vpc/__init__.py
@@ -979,7 +979,7 @@ class VPCConnection(EC2Connection):
         :rtype: The newly created VpnGateway
         :return: A :class:`boto.vpc.vpngateway.VpnGateway` object
         """
-        params = {'Type' : type}
+        params = {'Type': type}
         if availability_zone:
             params['AvailabilityZone'] = availability_zone
         if dry_run:
@@ -1021,10 +1021,32 @@ class VPCConnection(EC2Connection):
         :return: a :class:`boto.vpc.vpngateway.Attachment`
         """
         params = {'VpnGatewayId': vpn_gateway_id,
-                  'VpcId' : vpc_id}
+                  'VpcId': vpc_id}
         if dry_run:
             params['DryRun'] = 'true'
         return self.get_object('AttachVpnGateway', params, Attachment)
+
+    def detach_vpn_gateway(self, vpn_gateway_id, vpc_id, dry_run=False):
+        """
+        Detaches a VPN gateway from a VPC.
+
+        :type vpn_gateway_id: str
+        :param vpn_gateway_id: The ID of the vpn_gateway to detach
+
+        :type vpc_id: str
+        :param vpc_id: The ID of the VPC you want to detach the gateway from.
+
+        :type dry_run: bool
+        :param dry_run: Set to True if the operation should not actually run.
+
+        :rtype: bool
+        :return: True if successful
+        """
+        params = {'VpnGatewayId': vpn_gateway_id,
+                  'VpcId': vpc_id}
+        if dry_run:
+            params['DryRun'] = 'true'
+        return self.get_status('DetachVpnGateway', params)
 
     # Subnets
 
