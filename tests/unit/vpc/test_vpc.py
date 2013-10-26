52630cc97847a50bb24fb159514cbc802e8d62e9
Michael Barrientos <mike@syapse.com>
2013-10-26 13:22:44 -0700

    VPC RouteTable fixes and tests, pep8
    * Return newAssociationId in replace_route_table_assocation
    * Allow create_route to take network interface_id

--- tests/unit/vpc/test_vpc.py
+++ tests/unit/vpc/test_vpc.py
@@ -39,30 +39,6 @@ class TestDescribeVPCs(AWSMockServiceTestCase):
         self.assertEqual(vpc.instance_tenancy, 'default')
 
 
-class TestReplaceRouteTableAssociation(AWSMockServiceTestCase):
-
-    connection_class = VPCConnection
-
-    def default_body(self):
-        return """
-            <ReplaceRouteTableAssociationResponse xmlns="http://ec2.amazonaws.com/doc/2013-10-01/">
-               <requestId>59dbff89-35bd-4eac-99ed-be587EXAMPLE</requestId>
-               <newAssociationId>rtbassoc-faad4893</newAssociationId>
-            </ReplaceRouteTableAssociationResponse>
-        """
-
-    def test_replace_route_table_association(self):
-        self.set_http_response(status_code=200)
-        self.service_connection.replace_route_table_assocation('rtbassoc-faad4893', 'rtb-f9ad4890')
-        self.assert_request_parameters({
-            'Action': 'ReplaceRouteTableAssociation',
-            'AssociationId': 'rtbassoc-faad4893',
-            'RouteTableId': 'rtb-f9ad4890'},
-            ignore_params_values=['AWSAccessKeyId', 'SignatureMethod',
-                                  'SignatureVersion', 'Timestamp',
-                                  'Version'])
-
-
 class TestCreateVpc(AWSMockServiceTestCase):
 
     connection_class = VPCConnection
