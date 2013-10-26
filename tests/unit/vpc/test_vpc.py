0ef7f8108356f523030ac169eb4a993bf74a7477
Michael Barrientos <mike@syapse.com>
2013-10-26 12:37:38 -0700

    DeleteVpc and ModifyVpcAttribute tests, PEP8

--- tests/unit/vpc/test_vpc.py
+++ tests/unit/vpc/test_vpc.py
@@ -2,7 +2,7 @@
 from tests.unit import unittest
 from tests.unit import AWSMockServiceTestCase
 
-from boto.vpc import VPCConnection
+from boto.vpc import VPCConnection, VPC
 
 
 DESCRIBE_VPCS = r'''<?xml version="1.0" encoding="UTF-8"?>
@@ -63,7 +63,7 @@ class TestReplaceRouteTableAssociation(AWSMockServiceTestCase):
                                   'Version'])
 
 
-class TestCreateVPCs(AWSMockServiceTestCase):
+class TestCreateVpc(AWSMockServiceTestCase):
 
     connection_class = VPCConnection
 
@@ -84,7 +84,7 @@ class TestCreateVPCs(AWSMockServiceTestCase):
 
     def test_create_vpc(self):
         self.set_http_response(status_code=200)
-        self.service_connection.create_vpc('10.0.0.0/16', 'default')
+        api_response = self.service_connection.create_vpc('10.0.0.0/16', 'default')
         self.assert_request_parameters({
             'Action': 'CreateVpc',
             'InstanceTenancy': 'default',
@@ -92,7 +92,75 @@ class TestCreateVPCs(AWSMockServiceTestCase):
             ignore_params_values=['AWSAccessKeyId', 'SignatureMethod',
                                   'SignatureVersion', 'Timestamp',
                                   'Version'])
+        self.assertIsInstance(api_response, VPC)
+        self.assertEquals(api_response.id, 'vpc-1a2b3c4d')
+        self.assertEquals(api_response.state, 'pending')
+        self.assertEquals(api_response.cidr_block, '10.0.0.0/16')
+        self.assertEquals(api_response.dhcp_options_id, 'dopt-1a2b3c4d2')
+        self.assertEquals(api_response.instance_tenancy, 'default')
 
+
+class TestDeleteVpc(AWSMockServiceTestCase):
+
+    connection_class = VPCConnection
+
+    def default_body(self):
+        return """
+            <DeleteVpcResponse xmlns="http://ec2.amazonaws.com/doc/2013-10-01/">
+               <requestId>7a62c49f-347e-4fc4-9331-6e8eEXAMPLE</requestId>
+               <return>true</return>
+            </DeleteVpcResponse>
+        """
+
+    def test_delete_vpc(self):
+        self.set_http_response(status_code=200)
+        api_response = self.service_connection.delete_vpc('vpc-1a2b3c4d')
+        self.assert_request_parameters({
+            'Action': 'DeleteVpc',
+            'VpcId': 'vpc-1a2b3c4d'},
+            ignore_params_values=['AWSAccessKeyId', 'SignatureMethod',
+                                  'SignatureVersion', 'Timestamp',
+                                  'Version'])
+        self.assertEquals(api_response, True)
+
+
+class TestModifyVpcAttribute(AWSMockServiceTestCase):
+
+    connection_class = VPCConnection
+
+    def default_body(self):
+        return """
+            <ModifyVpcAttributeResponse xmlns="http://ec2.amazonaws.com/doc/2013-10-01/">
+               <requestId>7a62c49f-347e-4fc4-9331-6e8eEXAMPLE</requestId>
+               <return>true</return>
+            </ModifyVpcAttributeResponse>
+        """
+
+    def test_modify_vpc_attribute_dns_support(self):
+        self.set_http_response(status_code=200)
+        api_response = self.service_connection.modify_vpc_attribute(
+            'vpc-1a2b3c4d', enable_dns_support=True)
+        self.assert_request_parameters({
+            'Action': 'ModifyVpcAttribute',
+            'VpcId': 'vpc-1a2b3c4d',
+            'EnableDnsSupport.Value': 'true'},
+            ignore_params_values=['AWSAccessKeyId', 'SignatureMethod',
+                                  'SignatureVersion', 'Timestamp',
+                                  'Version'])
+        self.assertEquals(api_response, True)
+
+    def test_modify_vpc_attribute_dns_hostnames(self):
+        self.set_http_response(status_code=200)
+        api_response = self.service_connection.modify_vpc_attribute(
+            'vpc-1a2b3c4d', enable_dns_hostnames=True)
+        self.assert_request_parameters({
+            'Action': 'ModifyVpcAttribute',
+            'VpcId': 'vpc-1a2b3c4d',
+            'EnableDnsHostnames.Value': 'true'},
+            ignore_params_values=['AWSAccessKeyId', 'SignatureMethod',
+                                  'SignatureVersion', 'Timestamp',
+                                  'Version'])
+        self.assertEquals(api_response, True)
 
 if __name__ == '__main__':
     unittest.main()
