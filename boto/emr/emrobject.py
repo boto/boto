28a19d3f5acaded7ae23b6baec49a3387a536bab
Daniel G. Taylor <danielgtaylor@gmail.com>
2013-11-01 11:49:01 -0700

    Modify EMR add_jobflow_steps to return a JobFlowStepList with a list of step ids, part of the latest EMR API updates.

--- boto/emr/emrobject.py
+++ boto/emr/emrobject.py
@@ -60,6 +60,23 @@ class Arg(EmrObject):
         self.value = value
 
 
+class StepId(Arg):
+    pass
+
+
+class JobFlowStepList(EmrObject):
+    def __ini__(self, connection=None):
+        self.connection = connection
+        self.stepids = None
+
+    def startElement(self, name, attrs, connection):
+        if name == 'StepIds':
+            self.stepids = ResultSet([('member', StepId)])
+            return self.stepids
+        else:
+            return None
+
+
 class BootstrapAction(EmrObject):
     Fields = set([
         'Args',
