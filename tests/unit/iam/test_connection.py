fbc26e4e705eb19ae6a57691db399f4e17706624
Daniel G. Taylor <danielgtaylor@gmail.com>
2013-11-21 13:38:49 -0800

    Add a comment about empty response bodies, change argument order in update_saml_provider to match the rest of the code.

--- tests/unit/iam/test_connection.py
+++ tests/unit/iam/test_connection.py
@@ -137,7 +137,7 @@ class TestUpdateSamlProvider(AWSMockServiceTestCase):
 
     def test_update_saml_provider(self):
         self.set_http_response(status_code=200)
-        response = self.service_connection.update_saml_provider('doc', 'arn')
+        response = self.service_connection.update_saml_provider('arn', 'doc')
 
         self.assert_request_parameters(
             {
