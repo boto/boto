2ef66c9cbaaad6c596dc8b5d91bb66fe4aa8ae9d
Daniel G. Taylor <danielgtaylor@gmail.com>
2013-12-20 15:32:09 -0800

    Fix #1898 by doing an initial call to `datetime.strptime`
    This commit works around an upstream threading bug
    http://bugs.python.org/issue7980 which affects Mac OS X. It includes a
    test which is skipped by default because it will not always fail.

--- boto/__init__.py
+++ boto/__init__.py
@@ -27,6 +27,7 @@
 from boto.pyami.config import Config, BotoConfigLocations
 from boto.storage_uri import BucketStorageUri, FileStorageUri
 import boto.plugin
+import datetime
 import os
 import platform
 import re
@@ -38,6 +39,9 @@ from boto.exception import InvalidUriError
 
 __version__ = '2.21.0'
 Version = __version__  # for backware compatibility
+
+# http://bugs.python.org/issue7980
+datetime.datetime.strptime('', '')
 
 UserAgent = 'Boto/%s Python/%s %s/%s' % (
     __version__,
