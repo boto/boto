30ec4daa8bff5bbe425013ef827ca752bbcc1d5e
Daniel G. Taylor <danielgtaylor@gmail.com>
2013-12-27 10:36:06 -0800

    Add unit test for unicode calling format

--- tests/unit/s3/test_connection.py
+++ tests/unit/s3/test_connection.py
@@ -46,5 +46,37 @@ class TestSignatureAlteration(AWSMockServiceTestCase):
         )
 
 
+class TestUnicodeCallingFormat(AWSMockServiceTestCase):
+    connection_class = S3Connection
+
+    def default_body(self):
+        return """<?xml version="1.0" encoding="UTF-8"?>
+<ListAllMyBucketsResult xmlns="http://doc.s3.amazonaws.com/2006-03-01">
+  <Owner>
+    <ID>bcaf1ffd86f461ca5fb16fd081034f</ID>
+    <DisplayName>webfile</DisplayName>
+  </Owner>
+  <Buckets>
+    <Bucket>
+      <Name>quotes</Name>
+      <CreationDate>2006-02-03T16:45:09.000Z</CreationDate>
+    </Bucket>
+    <Bucket>
+      <Name>samples</Name>
+      <CreationDate>2006-02-03T16:41:58.000Z</CreationDate>
+    </Bucket>
+  </Buckets>
+</ListAllMyBucketsResult>"""
+
+    def create_service_connection(self, **kwargs):
+        kwargs['calling_format'] = u'boto.s3.connection.OrdinaryCallingFormat'
+        return super(TestUnicodeCallingFormat,
+                     self).create_service_connection(**kwargs)
+
+    def test_unicode_calling_format(self):
+        self.set_http_response(status_code=200)
+        self.service_connection.get_all_buckets()
+
+
 if __name__ == "__main__":
     unittest.main()
