8be3aaecd1bb50eec016b1000f53a1f2dc4f9fa6
Pascal Hakim <pasc@redellipse.net>
2014-01-25 12:19:27 +1100

    Updated unit tests for #2028, #2029

--- tests/unit/ec2/autoscale/test_group.py
+++ tests/unit/ec2/autoscale/test_group.py
@@ -279,6 +279,21 @@ class TestLaunchConfigurationDescribe(AWSMockServiceTestCase):
         self.assertEqual(response[0].instance_monitoring.enabled, 'true')
         self.assertEqual(response[0].ebs_optimized, False)
 
+        self.assert_request_parameters({
+            'Action': 'DescribeLaunchConfigurations',
+        }, ignore_params_values=['Version'])
+
+    def test_get_all_configuration_limited(self):
+        self.set_http_response(status_code=200)
+
+        response = self.service_connection.get_all_launch_configurations(max_records=10, names=["my-test1", "my-test2"])
+        self.assert_request_parameters({
+            'Action': 'DescribeLaunchConfigurations',
+            'MaxRecords': 10,
+            'LaunchConfigurationNames.member.1': 'my-test1',
+            'LaunchConfigurationNames.member.2': 'my-test2'
+        }, ignore_params_values=['Version'])
+
 class TestLaunchConfiguration(AWSMockServiceTestCase):
     connection_class = AutoScaleConnection
 
