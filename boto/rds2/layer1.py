fd36a34eddfb5247adcd8aabe7ce63a439428ce5
Daniel Lindsley <daniel@toastdriven.com>
2014-02-12 14:45:48 -0800

    And fixed the copyright.

--- boto/rds2/layer1.py
+++ boto/rds2/layer1.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2013 Amazon.com, Inc. or its affiliates.  All Rights Reserved
+# Copyright (c) 2014 Amazon.com, Inc. or its affiliates.  All Rights Reserved
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the
