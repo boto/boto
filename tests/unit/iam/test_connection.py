b52767f3fb8b4fa85bd49934c80303d900797bc0
Daniel Lindsley <daniel@toastdriven.com>
2014-03-07 16:46:52 -0800

    Fixed default IAM policy documents in cn-north-1.

--- tests/unit/iam/test_connection.py
+++ tests/unit/iam/test_connection.py
@@ -164,3 +164,75 @@ class TestDeleteSamlProvider(AWSMockServiceTestCase):
                 'SAMLProviderArn': 'arn'
             },
             ignore_params_values=['Version'])
+
+
+class TestCreateRole(AWSMockServiceTestCase):
+    connection_class = IAMConnection
+
+    def default_body(self):
+        return """
+          <CreateRoleResponse xmlns="https://iam.amazonaws.com/doc/2010-05-08/">
+            <CreateRoleResult>
+              <Role>
+                <Path>/application_abc/component_xyz/</Path>
+                <Arn>arn:aws:iam::123456789012:role/application_abc/component_xyz/S3Access</Arn>
+                <RoleName>S3Access</RoleName>
+                <AssumeRolePolicyDocument>{"Version":"2012-10-17","Statement":[{"Effect":"Allow","Principal":{"Service":["ec2.amazonaws.com"]},"Action":["sts:AssumeRole"]}]}</AssumeRolePolicyDocument>
+                <CreateDate>2012-05-08T23:34:01.495Z</CreateDate>
+                <RoleId>AROADBQP57FF2AEXAMPLE</RoleId>
+              </Role>
+            </CreateRoleResult>
+            <ResponseMetadata>
+              <RequestId>4a93ceee-9966-11e1-b624-b1aEXAMPLE7c</RequestId>
+            </ResponseMetadata>
+          </CreateRoleResponse>
+        """
+
+    def test_create_role_default(self):
+        self.set_http_response(status_code=200)
+        response = self.service_connection.create_role('a_name')
+
+        self.assert_request_parameters(
+            {'Action': 'CreateRole',
+             'AssumeRolePolicyDocument': '{"Statement": [{"Action": ["sts:AssumeRole"], "Effect": "Allow", "Principal": {"Service": ["ec2.amazonaws.com"]}}]}',
+             'RoleName': 'a_name'},
+            ignore_params_values=['Version'])
+
+    def test_create_role_default_cn_north(self):
+        self.set_http_response(status_code=200)
+        self.service_connection.host = 'iam.cn-north-1.amazonaws.com.cn'
+        response = self.service_connection.create_role('a_name')
+
+        self.assert_request_parameters(
+            {'Action': 'CreateRole',
+             'AssumeRolePolicyDocument': '{"Statement": [{"Action": ["sts:AssumeRole"], "Effect": "Allow", "Principal": {"Service": ["ec2.amazonaws.com.cn"]}}]}',
+             'RoleName': 'a_name'},
+            ignore_params_values=['Version'])
+
+    def test_create_role_string_policy(self):
+        self.set_http_response(status_code=200)
+        response = self.service_connection.create_role(
+            'a_name',
+            # Historical usage.
+            assume_role_policy_document='{"hello": "policy"}'
+        )
+
+        self.assert_request_parameters(
+            {'Action': 'CreateRole',
+             'AssumeRolePolicyDocument': '{"hello": "policy"}',
+             'RoleName': 'a_name'},
+            ignore_params_values=['Version'])
+
+    def test_create_role_data_policy(self):
+        self.set_http_response(status_code=200)
+        response = self.service_connection.create_role(
+            'a_name',
+            # With plain data, we should dump it for them.
+            assume_role_policy_document={"hello": "policy"}
+        )
+
+        self.assert_request_parameters(
+            {'Action': 'CreateRole',
+             'AssumeRolePolicyDocument': '{"hello": "policy"}',
+             'RoleName': 'a_name'},
+            ignore_params_values=['Version'])
