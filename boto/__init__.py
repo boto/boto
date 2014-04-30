5ce56bc6a7da47d062f78e69ee3126cbe1d57a3b
Kevin Deldycke <kdeldycke@ocs.online.net>
2014-04-30 15:58:19 +0200

    Fix access to package resource.

--- boto/__init__.py
+++ boto/__init__.py
@@ -29,6 +29,7 @@ from boto.storage_uri import BucketStorageUri, FileStorageUri
 import boto.plugin
 import datetime
 import os
+import pkg_resources
 import platform
 import re
 import sys
@@ -58,7 +59,7 @@ TOO_LONG_DNS_NAME_COMP = re.compile(r'[-_a-z0-9]{64}')
 GENERATION_RE = re.compile(r'(?P<versionless_uri_str>.+)'
                            r'#(?P<generation>[0-9]+)$')
 VERSION_RE = re.compile('(?P<versionless_uri_str>.+)#(?P<version_id>.+)$')
-ENDPOINTS_PATH = os.path.join(os.path.dirname(__file__), 'endpoints.json')
+ENDPOINTS_PATH = pkg_resources.resource_filename('boto', 'endpoints.json')
 
 
 def init_logging():
