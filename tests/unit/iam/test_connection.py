ce7f9c0bb2045af78d9c232ff3301e77203abb82
Andrew Miller <info@akmiller.co.uk>
2014-07-01 22:13:17 +0100

    Update test_connection.py
    Add unit tests for list_saml_providers

--- tests/unit/iam/test_connection.py
+++ tests/unit/iam/test_connection.py
@@ -89,6 +89,13 @@ class TestListSamlProviders(AWSMockServiceTestCase):
         self.assert_request_parameters(
             {'Action': 'ListSAMLProviders'},
             ignore_params_values=['Version'])
+        self.assertEqual(response.saml_provider_list, [
+            {'arn':'arn:aws:iam::123456789012:instance-profile/application_abc/component_xyz/Database',
+             'valid_until':'2032-05-09T16:27:11Z',
+             'create_date':'2012-05-09T16:27:03Z'},
+            {'arn':'arn:aws:iam::123456789012:instance-profile/application_abc/component_xyz/Webserver',
+             'valid_until':'2015-03-11T13:11:02Z',
+             'create_date':'2012-05-09T16:27:11Z'}])
 
 
 class TestGetSamlProvider(AWSMockServiceTestCase):
