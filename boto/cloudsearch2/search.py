e9de1610c2e7960b1dc25804e3735256b7a09e22
Yuval Oren <yuval@n3twork.com>
2014-07-16 18:57:48 -0700

    Fixed search options. The parameter is not 'options' but 'q.options', as specified in https://docs.aws.amazon.com/cloudsearch/latest/developerguide/search-api.html#search-request-parameters

--- boto/cloudsearch2/search.py
+++ boto/cloudsearch2/search.py
@@ -137,7 +137,7 @@ class Query(object):
                 params['highlight.%s' % k] = v
 
         if self.options:
-            params['options'] = self.options
+            params['q.options'] = self.options
 
         if self.return_fields:
             params['return'] = ','.join(self.return_fields)
