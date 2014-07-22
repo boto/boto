a45c186a3b78a151bb98cf4eddddadb5d0ddb854
Petr Novodvorskiy <nidd@skytap.com>
2014-07-22 13:20:44 -0700

    Update copyright for VPC peering support.

--- boto/vpc/vpc_peering_connection.py
+++ boto/vpc/vpc_peering_connection.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2009-2010 Mitch Garnaat http://garnaat.org/
+# Copyright (c) 2014 Skytap http://skytap.com/
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the
