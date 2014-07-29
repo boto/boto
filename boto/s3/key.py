boto/boto

commit f3e5d11d16ef405910dbcc502ef73c808f95dc7f

Fetch key storage class on-demand.

boto/s3/key.py
@@ -116,7 +116,7 @@ class Key(object):
         self.is_latest = False
         self.last_modified = None
         self.owner = None
-        self.storage_class = 'STANDARD'
+        self._storage_class = None
         self.path = None
         self.resp = None
         self.mode = None
@@ -186,6 +186,23 @@ class Key(object):
             del self.local_hashes['md5']
 
     base64md5 = property(_get_base64md5, _set_base64md5);
+
+    def _get_storage_class(self):
+        if self._storage_class is None and self.bucket:
+            # Attempt to fetch storage class
+            list_items = list(self.bucket.list(self.name.encode('utf-8')))
+            if len(list_items):
+                self._storage_class = list_items[0].storage_class
+            else:
+                # Key is not yet saved? Just use default...
+                self._storage_class = 'STANDARD'
+
+        return self._storage_class
+
+    def _set_storage_class(self, value):
+        self._storage_class = value
+
+    storage_class = property(_get_storage_class, _set_storage_class)
 
     def get_md5_from_hexdigest(self, md5_hexdigest):
         """

tests/unit/s3/test_key.py
@@ -92,6 +92,7 @@ class TestS3KeyRetries(AWSMockServiceTestCase):
         self.set_http_response(status_code=500)
         b = Bucket(self.service_connection, 'mybucket')
         k = b.new_key('test_failure')
+        k.storage_class = 'STANDARD'
         fail_file = StringIO('This will attempt to retry.')
 
         with self.assertRaises(BotoServerError):
@@ -103,6 +104,7 @@ class TestS3KeyRetries(AWSMockServiceTestCase):
         self.set_http_response(status_code=400, body=weird_timeout_body)
         b = Bucket(self.service_connection, 'mybucket')
         k = b.new_key('test_failure')
+        k.storage_class = 'STANDARD'
         fail_file = StringIO('This will pretend to be chunk-able.')
 
         k.should_retry = counter(k.should_retry)
@@ -119,6 +121,7 @@ class TestS3KeyRetries(AWSMockServiceTestCase):
         self.set_http_response(status_code=502, body=weird_timeout_body)
         b = Bucket(self.service_connection, 'mybucket')
         k = b.new_key('test_failure')
+        k.storage_class = 'STANDARD'
         fail_file = StringIO('This will pretend to be chunk-able.')
 
         k.should_retry = counter(k.should_retry)
@@ -135,6 +138,7 @@ class TestS3KeyRetries(AWSMockServiceTestCase):
         self.set_http_response(status_code=504, body=weird_timeout_body)
         b = Bucket(self.service_connection, 'mybucket')
         k = b.new_key('test_failure')
+        k.storage_class = 'STANDARD'
         fail_file = StringIO('This will pretend to be chunk-able.')
 
         k.should_retry = counter(k.should_retry)

commit b49b7ba28e985f5bc0c5a8d2cae7314db192a041

Prevent an infinite loop.
Fixes the issue observed in the Google Storage integration tests
from #2462. The problem was introduced in f3e5d11. This only uses
the internal cached value instead of potentially triggering infinite
list bucket calls.

boto/s3/key.py
@@ -191,8 +191,9 @@ class Key(object):
         if self._storage_class is None and self.bucket:
             # Attempt to fetch storage class
             list_items = list(self.bucket.list(self.name.encode('utf-8')))
-            if len(list_items):
-                self._storage_class = list_items[0].storage_class
+            if len(list_items) and getattr(list_items[0], '_storage_class',
+                                           None):
+                self._storage_class = list_items[0]._storage_class
             else:
                 # Key is not yet saved? Just use default...
                 self._storage_class = 'STANDARD'