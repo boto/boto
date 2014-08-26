a544fb7d53aa09489cf237ffdce76c595e2732ba
Konstantinos Koukopoulos <koukopoulos@gmail.com>
2014-08-26 17:44:51 +0300

    remove forgotten pdb invocation.

--- boto/s3/bucketlistresultset.py
+++ boto/s3/bucketlistresultset.py
@@ -19,7 +19,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 # IN THE SOFTWARE.
 
-from boto.compat import urllib, six
+import urllib
 
 def bucket_lister(bucket, prefix='', delimiter='', marker='', headers=None,
                   encoding_type=None):
@@ -37,9 +37,9 @@ def bucket_lister(bucket, prefix='', delimiter='', marker='', headers=None,
         if k:
             marker = rs.next_marker or k.name
         if marker and encoding_type == "url":
-            if isinstance(marker, six.text_type):
+            if isinstance(marker, unicode):
                 marker = marker.encode('utf-8')
-            marker = urllib.parse.unquote(marker)
+            marker = urllib.unquote(marker)
         more_results= rs.is_truncated
 
 class BucketListResultSet(object):
