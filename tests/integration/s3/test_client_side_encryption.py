3b297a1914cbf0287cb0b430c56734e062410fd4
Nicolas Trésegnie <nicolas.tresegnie@kazoup.com>
2014-10-29 13:40:43 +0000

    Test multipart upload with client-side encryption

--- tests/integration/s3/test_client_side_encryption.py
+++ tests/integration/s3/test_client_side_encryption.py
@@ -1,8 +1,13 @@
 """
 Some unit tests for the S3 Client Side Encryption
 """
+from distutils.spawn import find_executable
+from io import StringIO
+import json
 import os
+import random
 import shutil
+import string
 import subprocess
 import unittest
 import time
@@ -11,6 +16,9 @@ import binascii
 import urllib2
 from StringIO import StringIO
 from pip.vendor.distlib.version import NormalizedVersion
+from unittest2 import skip
+from boto.exception import BotoClientError
+from multiprocessing import Process
 
 from boto.s3.connection import S3Connection
 from boto.s3.multipart import MultiPartUpload
@@ -25,12 +33,21 @@ try:
 except ImportError:
     py4j = None
 
+try:
+    import memcache
+except ImportError:
+    memcache = None
 
-class S3ClientSideEncryptionTest(unittest.TestCase):
+
+class _TestWithS3BucketMixin(unittest.TestCase):
     s3 = True
 
+    conn = None
+    bucket_name = None
+    bucket = None
+
     def setUp(self):
-        super(S3ClientSideEncryptionTest, self).setUp()
+        super(_TestWithS3BucketMixin, self).setUp()
         self.conn = S3Connection()
         self.bucket_name = 'client-side-encryption-%d' % int(time.time())
         self.bucket = self.conn.create_bucket(self.bucket_name)
@@ -38,8 +55,16 @@ class S3ClientSideEncryptionTest(unittest.TestCase):
     def tearDown(self):
         for key in self.bucket:
             key.delete()
+        for multipart_upload in self.bucket.list_multipart_uploads():
+            self.bucket.cancel_multipart_upload(
+                multipart_upload.key_name,
+                multipart_upload.id,
+            )
         self.bucket.delete()
-        super(S3ClientSideEncryptionTest, self).tearDown()
+        super(_TestWithS3BucketMixin, self).tearDown()
+
+
+class S3ClientSideEncryptionTest(_TestWithS3BucketMixin, unittest.TestCase):
 
     def test_client_side_encryption(self):
         print('--- running {} tests ---'.format(self.__class__.__name__))
@@ -160,20 +185,226 @@ class S3ClientSideEncryptionTest(unittest.TestCase):
 
         print('--- {} tests completed ---'.format(self.__class__.__name__))
 
-    def test_client_side_encryption_multipart(self):
-        print('--- running {} multipart tests ---'.format(self.__class__.__name__))
 
-        connection = S3Connection(client_side_encryption_key=os.urandom(256 / 8))
+class S3ClientSideEncryptionMultipartTest(_TestWithS3BucketMixin, unittest.TestCase):
+
+    def test_client_side_encryption_multipart(self):
+
+        threshold = 5 * 2**20
+        values = string.letters + string.digits + string.punctuation
+        content_below_threshold = ''.join(random.choice(values) for _ in xrange(threshold - threshold/2))
+        content_above_threshold = ''.join(random.choice(values) for _ in xrange(threshold + threshold/2))
+
+        all_parts = [
+            [''],  # Edge case
+            [content_below_threshold],  # Final part will be detected during upload
+            [content_above_threshold],  # Final part won't be detected during upload
+            [content_above_threshold, content_below_threshold],  # Final part will be detected during upload
+            [content_above_threshold, content_above_threshold],  # Final part won't be detected during upload
+        ]
+
+        for i, parts in enumerate(all_parts):
+            print('Testing with content {}...'.format(i))
+            content = ''.join(parts)
+
+            connection = S3Connection(
+                client_side_encryption_key=os.urandom(256 / 8)
+            )
+            bucket = connection.get_bucket(self.bucket_name)
+
+            # Test the presence of the metadata
+            key_name = 'file-%d' % int(time.time())
+            multipart_upload = bucket.initiate_multipart_upload(key_name)
+            metadata_key = '{}-{}-metadata'.format(bucket.name, multipart_upload.id)
+            self.assertIn(metadata_key, connection.client_side_encryption_registry)
+
+            # Check the content of the metadata
+            encryption_metadata = json.loads(connection.client_side_encryption_registry[metadata_key])
+            self.assertIn('x-amz-iv', encryption_metadata)
+            self.assertIn('x-amz-key', encryption_metadata)
+            self.assertIn('x-amz-matdesc', encryption_metadata)
+
+            # Check that first iv is present
+            # This is also available in the metadata but "Special cases aren't special enough to break the rules."
+            part_0_key = '{}-{}-0'.format(bucket.name, multipart_upload.id)
+            self.assertIn(part_0_key, connection.client_side_encryption_registry)
+
+            # Upload the content
+            for j, part in enumerate(parts):
+                print('\tUploading part {}...'.format(j+1))
+                fp = StringIO()
+                fp.write(part.decode('utf-8'))
+                fp.seek(0)
+                multipart_upload.upload_part_from_file(fp, j+1)
+            print('\tCompleting upload...')
+            multipart_upload.complete_upload()
+
+            print('\tTesting content...')
+            key = bucket.get_key(key_name)
+            key_content = key.get_contents_as_string()
+            self.assertEqual(key_content, content, "The uploaded content is not equal to the original content.")
+
+            registry_key_prefix = '{}-{}'.format(bucket.name, multipart_upload.id)
+            for registry_key in connection.client_side_encryption_registry:
+                self.assertNotIn(registry_key_prefix, registry_key, "The client-side encryption registry still contains "
+                                                                    "data about the upload after its completion.")
+
+    def test_client_side_encryption_multipart_copy(self):
+        connection = S3Connection(
+            client_side_encryption_key=os.urandom(256 / 8)
+        )
         bucket = connection.get_bucket(self.bucket_name)
 
-        multipart_upload = MultiPartUpload(bucket)  # does not create an upload, so there's nothing to clean
-        self.assertRaises(ValueError, multipart_upload.upload_part_from_file, None, None)
+        multipart_upload = MultiPartUpload(bucket)  # does not actually create an upload
         self.assertRaises(ValueError, multipart_upload.copy_part_from_key, None, None, None)
 
-        print('--- tests {} multipart tests completed ---'.format(self.__class__.__name__))
+    def test_client_side_encryption_multipart_errors(self):
+
+        connection = S3Connection(
+            client_side_encryption_key=os.urandom(256 / 8)
+        )
+        bucket = connection.get_bucket(self.bucket_name)
+
+        threshold = 5 * 2**20
+        values = string.letters + string.digits + string.punctuation
+        content_below_threshold = ''.join(random.choice(values) for _ in xrange(threshold - threshold/2))
+        content_above_threshold = ''.join(random.choice(values) for _ in xrange(threshold + threshold/2))
+
+        all_parts = [
+            ([1, 2], [content_above_threshold + '0', content_below_threshold]),  # First part not multiple of 16
+            ([1, 2], [content_below_threshold, content_below_threshold]),  # First part too short
+            ([2, 1], [content_above_threshold, content_above_threshold]),  # Wrong order
+        ]
+
+        def upload_wrong_parts(part_numbers, parts):
+            key_name = 'file-%d' % int(time.time())
+            multipart_upload = bucket.initiate_multipart_upload(key_name)
+
+            for j, part in zip(part_numbers, parts):
+                fp = StringIO()
+                fp.write(part.decode('utf-8'))
+                fp.seek(0)
+                multipart_upload.upload_part_from_file(fp, j+1)
+            multipart_upload.complete_upload()
+
+        for i, (part_numbers, parts) in enumerate(all_parts):
+            print('Testing with content {}...'.format(i))
+            self.assertRaises(BotoClientError, lambda: upload_wrong_parts(part_numbers, parts))
 
 
-class _TestWithJavaS3GatewayMixin(object):
+class MemcachedDictionary(object):
+
+    def __init__(self, memcache_client):
+        self.memcache_client = memcache_client
+
+    def __getitem__(self, key):
+        return self.memcache_client.get(key)
+
+    def get(self, key, default):
+        if not key in self:
+            return default
+        return self[key]
+
+    def __setitem__(self, key, value):
+        self.memcache_client.set(key, value, time=60*60*24)
+
+    def __delitem__(self, key):
+        self.memcache_client.delete(key)
+
+    def keys(self):
+        # This will prevent the deletion of the data of completed uploads, but should it be enough for the tests
+        return []
+
+    def __contains__(self, key):
+        return self.memcache_client.get(key) is not None
+
+
+class S3ClientSideEncryptionMultipartMemcachedTest(_TestWithS3BucketMixin, unittest.TestCase):
+
+    memcache_process = None
+
+    @staticmethod
+    def _get_memcache_client():
+        return memcache.Client(['127.0.0.1:51211'])
+
+    @staticmethod
+    def _kill_memcached():
+        import psutil
+        for process in psutil.process_iter():
+            if '51211' in process.cmdline():
+                print('Stopping memcached...')
+                process.kill()
+                time.sleep(1)
+
+    def setUp(self):
+        self._kill_memcached()
+        self.memcache_process = subprocess.Popen(['memcached', '-p', '51211'])
+        time.sleep(3)
+
+        super(S3ClientSideEncryptionMultipartMemcachedTest, self).setUp()
+
+    def tearDown(self):
+        self._kill_memcached()
+        self.memcache_process.kill()
+        super(S3ClientSideEncryptionMultipartMemcachedTest, self).tearDown()
+
+    @staticmethod
+    def _upload_part(cls, client_side_encryption_key, bucket_name, key_name, multipart_upload_id, part_number, part):
+        connection = S3Connection(
+            client_side_encryption_key=client_side_encryption_key,
+            client_side_encryption_registry=MemcachedDictionary(cls._get_memcache_client()),
+        )
+        bucket = connection.get_bucket(bucket_name)
+
+        multipart_upload = MultiPartUpload(bucket)
+        multipart_upload.key_name = key_name
+        multipart_upload.id = multipart_upload_id
+
+        fp = StringIO()
+        fp.write(part.decode('utf-8'))
+        fp.seek(0)
+        multipart_upload.upload_part_from_file(fp, part_number)
+
+    @unittest.skipIf(not psutil, "Please install psutil (python package) to run this test")
+    @unittest.skipIf(not memcache, "Please install python-memcached (python package) to run this test")
+    @unittest.skipIf(not find_executable('memcached'), "Please install memcached (program) to run this test")
+    def test_with_memcached_using_multiprocessing(self):
+
+        print('Creating content...')
+        threshold = 5 * 2**20
+        values = string.letters + string.digits + string.punctuation
+        content_above_threshold = ''.join(random.choice(values) for _ in xrange(threshold + threshold/2))
+        parts = [content_above_threshold, content_above_threshold]
+        content = ''.join(parts)
+
+        print('Creating upload...')
+        connection = S3Connection(
+            client_side_encryption_key=os.urandom(256 / 8),
+            client_side_encryption_registry=MemcachedDictionary(self._get_memcache_client()),
+        )
+        bucket = connection.get_bucket(self.bucket_name)
+        key_name = 'file-%d' % int(time.time())
+        multipart_upload = bucket.initiate_multipart_upload(key_name)
+
+        for j, part in enumerate(parts):
+            print('Uploading part {} in a subprocess...'.format(j+1))
+            process = Process(
+                target=S3ClientSideEncryptionMultipartMemcachedTest._upload_part,
+                args=(S3ClientSideEncryptionMultipartMemcachedTest, connection.client_side_encryption_key,
+                      self.bucket_name, key_name, multipart_upload.id, j+1, part))
+            process.start()
+            process.join()
+
+        print('Completing upload...')
+        multipart_upload.complete_upload()
+
+        print('Testing content...')
+        key = bucket.get_key(key_name)
+        key_content = key.get_contents_as_string()
+        self.assertEqual(key_content, content, "The uploaded content is not equal to the original content.")
+
+
+class _TestWithJavaS3GatewayMixin(unittest.TestCase):
     jars = None
     gateway = None
 
@@ -276,20 +507,8 @@ class _TestWithJavaS3GatewayMixin(object):
         super(_TestWithJavaS3GatewayMixin, self).tearDown()
 
 
-class S3ClientSideEncryptionTestJavaImplementationCompatibility(_TestWithJavaS3GatewayMixin, unittest.TestCase):
-    s3 = True
-
-    def setUp(self):
-        super(S3ClientSideEncryptionTestJavaImplementationCompatibility, self).setUp()
-        self.conn = S3Connection()
-        self.bucket_name = 'client-side-encryption-%d' % int(time.time())
-        self.bucket = self.conn.create_bucket(self.bucket_name)
-
-    def tearDown(self):
-        for key in self.bucket:
-            key.delete()
-        self.bucket.delete()
-        super(S3ClientSideEncryptionTestJavaImplementationCompatibility, self).setUp()
+class S3ClientSideEncryptionTestJavaImplementationCompatibility(_TestWithS3BucketMixin, _TestWithJavaS3GatewayMixin,
+                                                                unittest.TestCase):
 
     @unittest.skipIf(not psutil or not py4j, "Please install psutil and py4j to run this test")
     def test_client_side_encryption_java_compatibility(self):
