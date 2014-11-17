edf76cfc9113f4f6b53db2a67f07c20bfcf74bac
Susan Goldblatt <susan@oysterbooks.com>
2014-11-17 15:24:35 -0500

    re-adding assert

--- tests/mturk/create_hit_external.py
+++ tests/mturk/create_hit_external.py
@@ -15,6 +15,7 @@ class Test(unittest.TestCase):
                 conn = SetHostMTurkConnection()
                 keywords=['boto', 'test', 'doctest']
                 create_hit_rs = conn.create_hit(question=q, lifetime=60*65, max_assignments=2, title="Boto External Question Test", keywords=keywords, reward = 0.05, duration=60*6, approval_delay=60*60, annotation='An annotation from boto external question test', unique_request_token='test' + str(datetime.datetime.now()), response_groups=['Minimal', 'HITDetail', 'HITQuestion', 'HITAssignmentSummary',])                assert(create_hit_rs.status == True)
+                assert(create_hit_rs.status == True)
 
 if __name__ == "__main__":
         unittest.main()
