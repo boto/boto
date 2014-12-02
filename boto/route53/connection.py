0c7fc6dd3ce91d4a37327188630868e99922cc2b
Zihao Yu <zihao.yu@hbo.com>
2014-12-02 14:47:46 -0500

    add get_checker_ip_ranges method

--- boto/route53/connection.py
+++ boto/route53/connection.py
@@ -306,6 +306,23 @@ class Route53Connection(AWSAuthConnection):
         h.parse(body)
         return e
 
+    def get_checker_ip_ranges(self):
+        """
+        Return a list of Route53 healthcheck IP ranges
+        """
+        uri = '/%s/checkeripranges' % self.Version
+        response = self.make_request('GET', uri)
+        body = response.read()
+        boto.log.debug(body)
+        if response.status >= 300:
+            raise exception.DNSServerError(response.status,
+                                           response.reason,
+                                           body)
+        e = boto.jsonresponse.Element(list_marker='CheckerIpRanges', item_marker=('member',))
+        h = boto.jsonresponse.XmlHandler(e, None)
+        h.parse(body)
+        return e
+
     def delete_health_check(self, health_check_id):
         """
         Delete a health check
