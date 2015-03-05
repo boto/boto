e098a50d224bc4cb64fd6ceddff2846029bacd52
kyleknap <kyleknap@amazon.com>
2015-03-05 09:23:19 -0800

    Add more regions for configservice

--- tests/integration/configservice/test_configservice.py
+++ tests/integration/configservice/test_configservice.py
@@ -33,7 +33,12 @@ class TestConfigService(unittest.TestCase):
         response = self.configservice.describe_configuration_recorders()
         self.assertIn('ConfigurationRecorders', response)
 
-    def test_handle_(self):
+    def test_handle_no_such_configuration_recorder(self):
         with self.assertRaises(NoSuchConfigurationRecorderException):
             self.configservice.describe_configuration_recorders(
                 configuration_recorder_names=['non-existant-recorder'])
+
+    def test_connect_to_non_us_east_1(self):
+        self.configservice = boto.configservice.connect_to_region('us-west-2')
+        response = self.configservice.describe_configuration_recorders()
+        self.assertIn('ConfigurationRecorders', response)
