6e9f8a7fffad1bced211904d9119ccb8edbbf3ab
Doug Ellwanger <doug@nunahealth.com>
2015-03-18 16:02:04 -0700

    Fix KMS in Python 3

--- tests/unit/kms/test_kms.py
+++ tests/unit/kms/test_kms.py
@@ -39,7 +39,7 @@ class TestKinesis(AWSMockServiceTestCase):
         self.set_http_response(status_code=200)
         data = b'\x00\x01\x02\x03\x04\x05'
         self.service_connection.encrypt(key_id='foo', plaintext=data)
-        body = json.loads(self.actual_request.body)
+        body = json.loads(self.actual_request.body.decode('utf-8'))
         self.assertEqual(body['Plaintext'], 'AAECAwQF')
 
     def test_non_binary_input_for_blobs_fails(self):
