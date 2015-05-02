0f4ed7e4c0dbc48502f13c7b7a7a88397b273b3e
Paulo Miguel Almeida <paulo.ubuntu@gmail.com>
2015-05-01 23:26:49 -0300

    Added documentation for boto.sqs.queue.Queue.set_attribute set_attribute method  and boto.sqs.connection.SQSConnection.set_queue_attribute

--- boto/sqs/connection.py
+++ boto/sqs/connection.py
@@ -158,6 +158,65 @@ class SQSConnection(AWSQueryConnection):
                                Attributes, queue.id)
 
     def set_queue_attribute(self, queue, attribute, value):
+        """
+        Set a new value for an attribute of a Queue.
+
+        :type queue: A Queue object
+        :param queue: The SQS queue to get attributes for
+
+        :type attribute: String
+        :param attribute: The name of the attribute you want to set.
+
+        :param value: The new value for the attribute.
+
+            For DelaySeconds the value must be an integer number of
+            seconds from 0 to 900 (15 minutes).
+            >>> queue.set_attribute('DelaySeconds', 900)
+
+            For MaximumMessageSize the value must be an integer number of
+            bytes from 1024 (1 KiB) to 262144 (256 KiB).
+            >>> queue.set_attribute('MaximumMessageSize', 262144)
+
+            For MessageRetentionPeriod the value must be an integer number of
+            seconds from 60 (1 minute) to 1209600 (14 days).
+            >>> queue.set_attribute('MessageRetentionPeriod', 1209600)
+
+            For Policy the value must be an string that contains JSON formatted
+            parameters and values.
+            >>> queue.set_attribute('Policy', json.dumps({
+            ...     'Version': '2008-10-17',
+            ...     'Id': '/123456789012/testQueue/SQSDefaultPolicy',
+            ...     'Statement': [
+            ...        {
+            ...            'Sid': 'Queue1ReceiveMessage',
+            ...            'Effect': 'Allow',
+            ...            'Principal': {
+            ...                'AWS': '*'
+            ...            },
+            ...            'Action': 'SQS:ReceiveMessage',
+            ...            'Resource': 'arn:aws:aws:sqs:us-east-1:123456789012:testQueue'
+            ...        }
+            ...    ]
+            ... }))
+
+            For ReceiveMessageWaitTimeSeconds the value must be an integer number of
+            seconds from 0 to 20.
+            >>> queue.set_attribute('ReceiveMessageWaitTimeSeconds', 20)
+
+            For VisibilityTimeout the value must be an integer number of
+            seconds from 0 to 43200 (12 hours).
+            >>> queue.set_attribute('VisibilityTimeout', 43200)
+
+            For RedrivePolicy the value must be an string that contains JSON formatted
+            parameters and values. You can set maxReceiveCount to a value between 1 and 1000.
+            The deadLetterTargetArn value is the Amazon Resource Name (ARN) of the queue that
+            will receive the dead letter messages.
+            >>> queue.set_attribute('RedrivePolicy', json.dumps({
+            ...    'maxReceiveCount': 5,
+            ...    'deadLetterTargetArn': "arn:aws:aws:sqs:us-east-1:123456789012:testDeadLetterQueue"
+            ... }))
+        """
+
         params = {'Attribute.Name' : attribute, 'Attribute.Value' : value}
         return self.get_status('SetQueueAttributes', params, queue.id)
 
