74b650da900bb627908f64a257a439ef43f34c65
7heo <7heo@mail.com>
2015-05-07 21:34:01 +0200

    Move get_location to connection.py
    Having get_location in bucket.py made no sense, since the location needs
    to be known for the connection to be established.

--- boto/s3/connection.py
+++ boto/s3/connection.py
@@ -221,6 +221,27 @@ class S3Connection(AWSAuthConnection):
         """
         self.bucket_class = bucket_class
 
+    def get_bucket_location(self, bucket_name):
+        """
+        Returns the LocationConstraint for the bucket.
+
+        :rtype: str
+        :return: The LocationConstraint for the bucket or the empty
+            string if no constraint was specified when bucket was created.
+        """
+        response = self.make_request('GET', bucket_name, query_args='location')
+        body = response.read()
+        if response.status == 200:
+            rs = ResultSet(self)
+            h = handler.XmlHandler(rs, self)
+            if not isinstance(body, bytes):
+                body = body.encode('utf-8')
+            xml.sax.parseString(body, h)
+            return rs.LocationConstraint
+        else:
+            raise self.connection.provider.storage_response_error(
+                response.status, response.reason, body)
+
     def build_post_policy(self, expiration_time, conditions):
         """
         Taken from the AWS book Python examples and modified for use with boto
