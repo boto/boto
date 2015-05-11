0df32db05cef2037e4d8ca2a68e13cd38c74720c
7heo <7heo@mail.com>
2015-05-11 10:37:26 +0200

    OrdinaryCallingFormat: Use location in server name
    Also moved the Location class to a different file, since it has to be
    imported from both connection.py and bucket.py.

---
+++ boto/s3/location.py
@@ -0,0 +1,30 @@
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish, dis-
+# tribute, sublicense, and/or sell copies of the Software, and to permit
+# persons to whom the Software is furnished to do so, subject to the fol-
+# lowing conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABIL-
+# ITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+# SHALL THE AUTHOR BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+
+class Location(object):
+
+    DEFAULT = ''  # US Classic Region
+    EU = 'EU'
+    USWest = 'us-west-1'
+    USWest2 = 'us-west-2'
+    SAEast = 'sa-east-1'
+    APNortheast = 'ap-northeast-1'
+    APSoutheast = 'ap-southeast-1'
+    APSoutheast2 = 'ap-southeast-2'
+    CNNorth1 = 'cn-north-1'
