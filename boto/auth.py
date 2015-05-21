0f230f0cf95b6061a005fcaed22ccef797ebd67e
Juan A. Suarez Romero <jasuarez@igalia.com>
2015-05-21 13:38:40 +0200

    Do not include port in host if it's already there
    Using SigV4, if port is not an standard port it is already included in
    the request host. So do not include the port twice.
    
    Fixes [boto/boto#2623].

--- boto/auth.py
+++ boto/auth.py
@@ -590,7 +590,7 @@ class S3HmacAuthV4Handler(HmacAuthV4Handler, AuthHandler):
     def host_header(self, host, http_request):
         port = http_request.port
         secure = http_request.protocol == 'https'
-        if ((port == 80 and not secure) or (port == 443 and secure)):
+        if ((port == 80 and not secure) or (port == 443 and secure) or (":" in http_request.host)):
             return http_request.host
         return '%s:%s' % (http_request.host, port)
 
