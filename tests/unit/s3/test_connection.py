6f4f407020fe47b06695bfdc420ee420bf1b49f6
Stefan Foulis <stefan@foulis.ch>
2016-01-20 18:23:14 +0100

    add tests for query_auth setting

--- tests/unit/s3/test_connection.py
+++ tests/unit/s3/test_connection.py
@@ -50,6 +50,30 @@ class TestSignatureAlteration(AWSMockServiceTestCase):
         )
 
 
+class TestPresigned(MockServiceWithConfigTestCase):
+    connection_class = S3Connection
+
+    def test_presign_respect_query_auth(self):
+        self.config = {
+            's3': {
+                'use-sigv4': False,
+            }
+        }
+
+        conn = self.connection_class(
+            aws_access_key_id='less',
+            aws_secret_access_key='more',
+            host='s3.amazonaws.com'
+        )
+
+        url_enabled = conn.generate_url(86400, 'GET', bucket='examplebucket',
+                                      key='test.txt', query_auth=True)
+
+        url_disabled = conn.generate_url(86400, 'GET', bucket='examplebucket',
+                                      key='test.txt', query_auth=False)
+        self.assertIn('Signature=', url_enabled)
+        self.assertNotIn('Signature=', url_disabled)
+
 class TestSigV4HostError(MockServiceWithConfigTestCase):
     connection_class = S3Connection
 
@@ -134,6 +158,27 @@ class TestSigV4Presigned(MockServiceWithConfigTestCase):
 
         self.assertIn('VersionId=2', url)
         self.assertIn('X-Amz-Security-Token=token', url)
+
+    def test_sigv4_presign_respect_query_auth(self):
+        self.config = {
+            's3': {
+                'use-sigv4': True,
+            }
+        }
+
+        conn = self.connection_class(
+            aws_access_key_id='less',
+            aws_secret_access_key='more',
+            host='s3.amazonaws.com'
+        )
+
+        url_enabled = conn.generate_url(86400, 'GET', bucket='examplebucket',
+                                      key='test.txt', query_auth=True)
+
+        url_disabled = conn.generate_url(86400, 'GET', bucket='examplebucket',
+                                      key='test.txt', query_auth=False)
+        self.assertIn('Signature=', url_enabled)
+        self.assertNotIn('Signature=', url_disabled)
 
     def test_sigv4_presign_headers(self):
         self.config = {
