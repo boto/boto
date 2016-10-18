dc85cd08bae8af7e772bdc054cc1303d9c81f184
Noadmin <2011.development@gmail.com>
2016-10-18 07:15:47 -0700

    Add S3 Geo replication header support for Amazon S3 and DDN WASS

--- boto/utils.py
+++ boto/utils.py
@@ -79,7 +79,7 @@ qsa_of_interest = ['acl', 'cors', 'defaultObjectAcl', 'location', 'logging',
                    'response-content-language', 'response-expires',
                    'response-cache-control', 'response-content-disposition',
                    'response-content-encoding', 'delete', 'lifecycle',
-                   'tagging', 'restore',
+                   'tagging', 'restore', 'replication',
                    # storageClass is a QSA for buckets in Google Cloud Storage.
                    # (StorageClass is associated to individual keys in S3, but
                    # having it listed here should cause no problems because
