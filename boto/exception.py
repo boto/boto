d86db65beb2eb2be3663168491eb40478483ff6c
JordonPhillips <phjordon@amazon.com>
2016-12-08 10:51:54 -0800

    Backport EndpointResolver

--- boto/exception.py
+++ boto/exception.py
@@ -583,3 +583,18 @@ class InvalidInstanceMetadataError(Exception):
     def __init__(self, msg):
         final_msg = msg + '\n' + self.MSG
         super(InvalidInstanceMetadataError, self).__init__(final_msg)
+
+
+class BaseEndpointResolverError(Exception):
+    """Base error for endpoint resolving errors.
+
+    Should never be raised directly, but clients can catch
+    this exception if they want to generically handle any errors
+    during the endpoint resolution process.
+
+    """
+
+
+class NoRegionError(BaseEndpointResolverError):
+    """No region was specified."""
+    fmt = 'You must specify a region.'
