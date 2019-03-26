ec6c69a89842933e27e27be90c7add2911bdb1c0
Cat Lee Ball 🎷🐛 <cball@google.com>
2019-03-26 15:35:25 -0700

    Expanded get_utf8_value logic

--- boto/utils.py
+++ boto/utils.py
@@ -860,10 +860,10 @@ def notify(subject, body=None, html_body=None, to_string=None,
 
 
 def get_utf8_value(value):
-    if isinstance(value, bytes):
-        value.decode('utf-8')
+    if six.PY3:
+        if isinstance(value, bytes):
+            value = value.decode('utf-8')
         return value
-
     if not isinstance(value, six.string_types):
         value = six.text_type(value)
 
